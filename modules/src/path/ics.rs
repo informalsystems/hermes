172220ea84b1a980d482b9ad8a7cd4647da90db9
Romain Ruetschi <romain@informal.systems>
2020-03-11 10:23:16 +0100

    Extract IBC queries paths into conditional modules (#24)
    * Extract IBC queries paths into conditional modules
    
    Since these paths currently differ between the Go implementation
    and the ICS, we need to be able to switch them up at compile-time
    for when we will need to test against the Go implementation.
    
    * Revert tendermint dependency to master

---
+++ modules/src/path/ics.rs
@@ -0,0 +1,9 @@
+use super::{ConnectionPath, ConsensusStatePath};
+
+pub fn connection_path(_path: &ConnectionPath) -> String {
+    todo!()
+}
+
+pub fn consensus_state_path(_path: &ConsensusStatePath) -> String {
+    todo!()
+}
