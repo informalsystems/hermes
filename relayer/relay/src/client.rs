e75b5a18b2e3670206b9b60de4cd3de4f76057c1
Romain Ruetschi <romain@informal.systems>
2020-03-17 12:59:02 +0100

    Document the relayer::client::Client struct

--- relayer/relay/src/client.rs
+++ relayer/relay/src/client.rs
@@ -16,20 +16,33 @@ pub use trust_options::TrustOptions;
 pub mod rpc_requester;
 pub use rpc_requester::RpcRequester;
 
+/// Defines a client from the point of view of the relayer.
+///
+/// This is basically a wrapper around the facilities provided
+/// by the light client implementation in the `tendermint-rs` crate,
+/// where verified headers are stored in a trusted store.
 pub struct Client<Chain, Store>
 where
     Chain: chain::Chain,
     Store: store::Store<Chain>,
 {
+    /// The chain this client is for
     chain: Chain,
+
+    /// The trusted store where to store verified headers
     trusted_store: Store,
+
+    /// The trusting period configured for this chain
     trusting_period: Duration,
+
+    /// The trust threshold configured for this chain
     trust_threshold: TrustThresholdFraction,
 
     // FIXME: Use a custom type, because TrustedState states
     // it holds a header at height H and validator set at height H + 1,
     // whereas this trusted state holds both header and validator set at
     // same height H
+    /// The last trusted state verified by this client
     last_trusted_state: Option<TrustedState<Chain::Commit, Chain::Header>>,
 }
 
@@ -38,6 +51,12 @@ where
     Chain: chain::Chain,
     Store: store::Store<Chain>,
 {
+    /// Creates a new `Client` for the given `chain`, storing headers
+    /// in the given `trusted_store`, and verifying them with the
+    /// given `trust_options`.
+    ///
+    /// This method is async because it needs to pull the latest header
+    /// from the chain, verify it, and store it.
     pub async fn new(
         chain: Chain,
         trusted_store: Store,
@@ -45,31 +64,50 @@ where
     ) -> Result<Self, error::Error> {
         let mut client = Self::new_from_trusted_store(chain, trusted_store, &trust_options)?;
 
+        // If we managed to pull and verify a header from the chain already
         if let Some(ref trusted_state) = client.last_trusted_state {
+            // Check that this header can be trusted with the given trust options
             client
                 .check_trusted_header(trusted_state.last_header(), &trust_options)
                 .await?;
 
+            // If the last header we trust is below the given trusted height, we need
+            // to fetch and verify the header at the given trusted height instead.
             if trusted_state.last_header().header().height() < trust_options.height {
                 client.init_with_trust_options(trust_options).await?;
             }
         } else {
+            // Otherwise, init the client with the given trusted height, etc.
             client.init_with_trust_options(trust_options).await?;
         }
 
+        // Perform an update already, to make sure the client is up-to-date.
+        // TODO: Should we leave this up to the responsibility of the caller?
         let _ = client.update(SystemTime::now()).await?;
 
         Ok(client)
     }
 
+    /// The last state (if any) trusted by this client
     pub fn last_trusted_state(&self) -> Option<&TrustedState<Chain::Commit, Chain::Header>> {
         self.last_trusted_state.as_ref()
     }
 
+    /// The chain for which this client is configured
     pub fn chain(&self) -> &Chain {
         &self.chain
     }
 
+    /// Fetch and verify the latest header from the chain
+    ///
+    /// If the fetched header is higher than the previous trusted state,
+    /// and it verifies then we succeed and return it wrapped in `Some(_)`.
+    ///
+    /// If it is higher but does not verify we fail with an error.
+    ///
+    /// If it is lower we succeed but return `None`.
+    ///
+    /// If there is no trusted state yet we fail with an error.
     pub async fn update(
         &mut self,
         now: SystemTime,
@@ -105,7 +143,18 @@ where
         }
     }
 
-    // TODO: Cleanup signature
+    /// Verify the given signed header and validator set against the
+    /// trusted store.
+    ///
+    /// If the given header is already in the store, but it does not match
+    /// the stored hash, we fail with an error.
+    ///
+    /// Otherwise, if we already have a trusted state, we then pull the header right after
+    /// the header we were given to verify, fetch its validator set,
+    /// and attempt to verify the new header against its next validator set.
+    /// If that succeeds we update the trusted store and our last trusted state.
+    ///
+    /// If there is no current trusted state, we fail with an error.
     async fn verify_header(
         &mut self,
         new_header: &SignedHeader<Chain::Commit, Chain::Header>,
@@ -116,8 +165,11 @@ where
             .trusted_store
             .get(StoreHeight::Given(new_header.header().height()));
 
+        // If the given header height is already in the store
         if let Ok(state) = in_store {
             let stored_header = state.last_header().header();
+
+            // ... but it does not match the stored hash, then we fail
             if stored_header.hash() != new_header.header().hash() {
                 fail!(
                     error::Kind::LightClient,
@@ -128,6 +180,9 @@ where
             }
         }
 
+        // If we already have a trusted state, we then pull the header right after
+        // the header we were given to verify, fetch its validator set,
+        // and attempt to verify the new header against its next validator set.
         if let Some(ref last_trusted_state) = self.last_trusted_state {
             let next_height = new_header
                 .header()
@@ -169,6 +224,8 @@ where
         }
     }
 
+    /// Create a new client with the given trusted store and trust options,
+    /// and try to restore the last trusted state from the trusted store.
     fn new_from_trusted_store(
         chain: Chain,
         trusted_store: Store,
@@ -187,6 +244,8 @@ where
         Ok(client)
     }
 
+    /// Restore the last trusted state from the state, by asking for
+    /// its last stored height, without any verification.
     fn restore_trusted_state(&mut self) -> Result<(), error::Error> {
         if let Some(last_height) = self.trusted_store.last_height() {
             let last_trusted_state = self
@@ -199,6 +258,12 @@ where
         Ok(())
     }
 
+    /// Check that the given trusted state corresponding to the given
+    /// trust options is valid.
+    ///
+    /// TODO: Improve doc
+    /// TODO: Impement rollback
+    /// TODO: Implement cleanup
     async fn check_trusted_header(
         &self,
         trusted_header: &SignedHeader<Chain::Commit, Chain::Header>,
@@ -229,6 +294,18 @@ where
         Ok(())
     }
 
+    /// Init this client with the given trust options.
+    ///
+    /// This pulls the header and validator set at the height specified in
+    /// the trust options, and checks their hashses against the hashes
+    /// specified in the trust options.
+    ///
+    /// Then validate the commit against its validator set.
+    ///
+    /// Then verify that +1/3 of the validator set signed the commit.
+    ///
+    /// If everything succeeds, add the header to the trusted store and
+    /// update the last trusted state with it.
     async fn init_with_trust_options(
         &mut self,
         trust_options: TrustOptions,
@@ -290,6 +367,12 @@ where
         Ok(())
     }
 
+    /// Update the last trusted state with the given state,
+    /// and add it to the trusted store.
+    ///
+    /// This method only verifies that the validators hashes match.
+    ///
+    /// TODO: Pruning
     fn update_trusted_state(
         &mut self,
         state: TrustedState<Chain::Commit, Chain::Header>,
