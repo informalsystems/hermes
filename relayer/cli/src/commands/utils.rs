b1dd6eebf4efa9d930babdb955baa41162cbe615
Anca Zamfir <zamfiranca@gmail.com>
2020-04-09 13:34:01 +0200

    add utils

---
+++ relayer/cli/src/commands/utils.rs
@@ -0,0 +1,11 @@
+use core::future::Future;
+
+/// block on future
+pub fn block_on<F: Future>(future: F) -> F::Output {
+    tokio::runtime::Builder::new()
+        .basic_scheduler()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(future)
+}
