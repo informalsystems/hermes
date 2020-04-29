0fa2fb957664d223a4f11d813fa4f7965afcbf6f
Zaki Manian <zaki@manian.org>
2020-04-29 12:12:27 -0700

    Rename the events field to data and make field public

--- modules/src/events.rs
+++ modules/src/events.rs
@@ -172,14 +172,14 @@ macro_rules! make_event {
     ($a:ident, $b:literal, $c:literal) => {
         #[derive(Debug, Deserialize, Serialize, Clone)]
         pub struct $a {
-            events: std::collections::HashMap<String, Vec<String>>,
+            pub data: std::collections::HashMap<String, Vec<String>>,
         }
         impl TryFrom<&Event> for $a {
             type Error = &'static str;
             fn try_from(event: &Event) -> Result<Self, Self::Error> {
                 match event {
                     Event::JsonRPCTransctionResult { ref data } => Ok($a {
-                        events: data.extract_events($b, $c)?.clone(),
+                        data: data.extract_events($b, $c)?.clone(),
                     }),
                     Event::GenericJSONEvent { .. } => {
                         Err("Expected JSON representing a $a, got wrong type")?
