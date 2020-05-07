b64f8d9e7df3e8e0bc43e67d569f09b5c64b9829
Anca Zamfir <ancazamfir@users.noreply.github.com>
2020-05-07 18:02:59 +0200

    Switch to ICS paths (#62)
    * Switch to ICS paths
    
    * Change "connection" to "connections" in path
    
    * Fix clippy warnings
    
    * Point tendermint-rs to master branch
    
    * Update RUSTFLAGS for nightly coverage tests
    
    Co-authored-by: Romain Ruetschi <romain@informal.systems>

--- modules/src/path/ics.rs
+++ modules/src/path/ics.rs
@@ -1,13 +1,13 @@
 use super::{ClientStatePath, ConnectionPath, ConsensusStatePath};
 
-pub fn connection_path(_path: &ConnectionPath) -> String {
-    todo!()
+pub fn connection_path(path: &ConnectionPath) -> String {
+    format!("connections/{}", path.connection_id)
 }
 
-pub fn consensus_state_path(_path: &ConsensusStatePath) -> String {
-    todo!()
+pub fn consensus_state_path(path: &ConsensusStatePath) -> String {
+    format!("clients/{}/consensusState/{}", path.client_id, path.height)
 }
 
-pub fn client_state_path(_path: &ClientStatePath) -> String {
-    todo!()
+pub fn client_state_path(path: &ClientStatePath) -> String {
+    format!("clients/{}/clientState", path.client_id)
 }
