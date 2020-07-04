746d8b7ab531202b37f1f4dd6583432ed9523b6e
Greg Szabo <greg@philosobear.com>
2020-07-03 22:17:22 -0400

    Relayer GitHub Actions configuration and testing

--- ci/build-chains.sh
+++ ci/build-chains.sh
@@ -18,25 +18,25 @@ echo "*** Build binary"
 GOOS=linux make build
 
 echo "*** Create config using the built binary"
-docker run -it --rm -v $(pwd)/build:/root:z alpine /root/gaiad testnet -o /root/chain1 --v 1 --chain-id chain1 --keyring-backend test
-sed -i.bak -e 's/^index_all_keys[[:space:]]*=.*/index_all_keys = true/'   build/chain1/node0/gaiad/config/config.toml
-sed -i.bak -e 's/^timeout_commit[[:space:]]*=.*/timeout_commit = "1s"/'   build/chain1/node0/gaiad/config/config.toml
-sed -i.bak -e 's/^timeout_propose[[:space:]]*=.*/timeout_propose = "1s"/' build/chain1/node0/gaiad/config/config.toml
+docker run -it --rm -v $(pwd)/build:/root:z alpine /root/gaiad testnet -o /root/chain_a --v 1 --chain-id chain_A --keyring-backend test
+sed -i.bak -e 's/^index_all_keys[[:space:]]*=.*/index_all_keys = true/'   build/chain_a/node0/gaiad/config/config.toml
+sed -i.bak -e 's/^timeout_commit[[:space:]]*=.*/timeout_commit = "1s"/'   build/chain_a/node0/gaiad/config/config.toml
+sed -i.bak -e 's/^timeout_propose[[:space:]]*=.*/timeout_propose = "1s"/' build/chain_a/node0/gaiad/config/config.toml
 
-docker run -it --rm -v $(pwd)/build:/root:z alpine /root/gaiad testnet -o /root/chain2 --v 1 --chain-id chain2 --keyring-backend test
-sed -i.bak -e 's/^index_all_keys[[:space:]]*=.*/index_all_keys = true/'   build/chain2/node0/gaiad/config/config.toml
-sed -i.bak -e 's/^timeout_commit[[:space:]]*=.*/timeout_commit = "1s"/'   build/chain2/node0/gaiad/config/config.toml
-sed -i.bak -e 's/^timeout_propose[[:space:]]*=.*/timeout_propose = "1s"/' build/chain2/node0/gaiad/config/config.toml
+docker run -it --rm -v $(pwd)/build:/root:z alpine /root/gaiad testnet -o /root/chain_b --v 1 --chain-id chain_B --keyring-backend test
+sed -i.bak -e 's/^index_all_keys[[:space:]]*=.*/index_all_keys = true/'   build/chain_b/node0/gaiad/config/config.toml
+sed -i.bak -e 's/^timeout_commit[[:space:]]*=.*/timeout_commit = "1s"/'   build/chain_b/node0/gaiad/config/config.toml
+sed -i.bak -e 's/^timeout_propose[[:space:]]*=.*/timeout_propose = "1s"/' build/chain_b/node0/gaiad/config/config.toml
 
 echo "*** Create Docker image and upload to Docker Hub"
 cd ..
-docker build -t informaldev/chain1 -f chain1.Dockerfile .
-docker build -t informaldev/chain2 -f chain2.Dockerfile .
-docker push informaldev/chain1
-docker push informaldev/chain2
+docker build -t informaldev/chain_a -f chain_a.Dockerfile .
+docker build -t informaldev/chain_b -f chain_b.Dockerfile .
+docker push informaldev/chain_a
+docker push informaldev/chain_b
 
 # Get details from the config files
-echo SECRET1=$(jq -r .secret gaia/build/chain1/node0/gaiacli/key_seed.json)
-echo SECRET2=$(jq -r .secret gaia/build/chain2/node0/gaiacli/key_seed.json)
-echo NODE1ID=$(jq -r .app_state.genutil.gentxs[0].value.memo  gaia/build/chain1/node0/gaiad/config/genesis.json)
-echo NODE2ID=$(jq -r .app_state.genutil.gentxs[0].value.memo  gaia/build/chain2/node0/gaiad/config/genesis.json)
+echo SECRET_A=$(jq -r .secret gaia/build/chain_a/node0/gaiacli/key_seed.json)
+echo SECRET_B=$(jq -r .secret gaia/build/chain_b/node0/gaiacli/key_seed.json)
+echo NODEID_A=$(jq -r .app_state.genutil.gentxs[0].value.memo  gaia/build/chain_a/node0/gaiad/config/genesis.json)
+echo NODEID_B=$(jq -r .app_state.genutil.gentxs[0].value.memo  gaia/build/chain_b/node0/gaiad/config/genesis.json)
