e4e3d22d0d12d16a20227b79146907a183b1281d
Andy Nogueira <me@andynogueira.dev>
2020-08-11 14:33:04 -0400

    Update dependencies after changing package name #198

--- relayer-cli/src/commands/query/connection.rs
+++ relayer-cli/src/commands/query/connection.rs
@@ -3,13 +3,13 @@ use crate::prelude::*;
 use abscissa_core::{Command, Options, Runnable};
 use relayer::config::{ChainConfig, Config};
 
-use modules::ics24_host::error::ValidationError;
-use modules::ics24_host::identifier::ConnectionId;
-use modules::ics24_host::Path::Connections;
+use ibc::ics24_host::error::ValidationError;
+use ibc::ics24_host::identifier::ConnectionId;
+use ibc::ics24_host::Path::Connections;
 use relayer::chain::{Chain, CosmosSDKChain};
 use tendermint::chain::Id as ChainId;
 
-use modules::ics03_connection::connection::ConnectionEnd;
+use ibc::ics03_connection::connection::ConnectionEnd;
 
 #[derive(Clone, Command, Debug, Options)]
 pub struct QueryConnectionEndCmd {
