c8af369544f3d0d3feb5372cb731904f0fc41256
Anca Zamfir <zamfiranca@gmail.com>
2020-08-14 11:49:20 +0200

    Remove from_str() for State, change msg order to i32

--- modules/src/ics03_connection/connection.rs
+++ modules/src/ics03_connection/connection.rs
@@ -5,7 +5,6 @@ use crate::try_from_raw::TryFromRaw;
 use serde_derive::{Deserialize, Serialize};
 
 // Import proto declarations.
-use anomaly::fail;
 use ibc_proto::connection::ConnectionEnd as RawConnectionEnd;
 use ibc_proto::connection::Counterparty as RawCounterparty;
 use std::convert::TryFrom;
@@ -192,76 +191,6 @@ impl State {
             2 => Self::TryOpen,
             3 => Self::Open,
             _ => Self::Uninitialized,
-        }
-    }
-}
-
-impl std::str::FromStr for State {
-    type Err = Error;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "UNINITIALIZED" => Ok(Self::Uninitialized),
-            "INIT" => Ok(Self::Init),
-            "TRYOPEN" => Ok(Self::TryOpen),
-            "OPEN" => Ok(Self::Open),
-            _ => fail!(Kind::UnknownState, s),
-        }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use std::str::FromStr;
-
-    #[test]
-    fn parse_connection_state() {
-        use super::State;
-
-        struct Test {
-            state: &'static str,
-            want_res: State,
-            want_err: bool,
-        }
-
-        let tests: Vec<Test> = vec![
-            Test {
-                state: "UNINITIALIZED",
-                want_res: State::Uninitialized,
-                want_err: false,
-            },
-            Test {
-                state: "INIT",
-                want_res: State::Init,
-                want_err: false,
-            },
-            Test {
-                state: "TRYOPEN",
-                want_res: State::TryOpen,
-                want_err: false,
-            },
-            Test {
-                state: "OPEN",
-                want_res: State::Open,
-                want_err: false,
-            },
-            Test {
-                state: "INVALID_STATE",
-                want_res: State::Open,
-                want_err: true,
-            },
-        ]
-        .into_iter()
-        .collect();
-
-        for test in tests {
-            match State::from_str(test.state) {
-                Ok(res) => {
-                    assert!(!test.want_err);
-                    assert_eq!(test.want_res, res);
-                }
-                Err(_) => assert!(test.want_err, "parse failed"),
-            }
         }
     }
 }
