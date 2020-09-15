dc752e3fb3b31ed799013538ca0c968d3660d776
Romain Ruetschi <romain@informal.systems>
2020-09-15 18:00:37 +0200

    Stylistic fixes

--- modules/src/mock_client/header.rs
+++ modules/src/mock_client/header.rs
@@ -1,9 +1,9 @@
+use crate::ics02_client::client_def::AnyHeader;
+use crate::ics02_client::client_type::ClientType;
+use crate::ics02_client::error::{self, Error};
 use crate::ics02_client::header::Header;
-use crate::ics02_client::{client_type::ClientType, error::Kind};
-use crate::{
-    ics02_client::{client_def::AnyHeader, error::Error},
-    try_from_raw::TryFromRaw,
-};
+use crate::try_from_raw::TryFromRaw;
+
 use serde_derive::{Deserialize, Serialize};
 use tendermint::block::Height;
 
@@ -37,7 +37,7 @@ impl TryFromRaw for MockHeader {
     type RawType = ibc_proto::ibc::mock::Header;
 
     fn try_from(raw: Self::RawType) -> Result<Self, Self::Error> {
-        let proto_height = raw.height.ok_or_else(|| Kind::InvalidRawHeader)?;
+        let proto_height = raw.height.ok_or_else(|| error::Kind::InvalidRawHeader)?;
         Ok(Self(decode_height(proto_height)))
     }
 }
