6ab8611cf6c34485a8a9cae46196afc9c60a2a97
Vitor Enes <vitorenesduarte@gmail.com>
2021-02-03 10:21:33 +0100

    Buffered file reads

--- modules/tests/modelator.rs
+++ modules/tests/modelator.rs
@@ -2,6 +2,7 @@ use eyre::{eyre, Context, Result};
 use serde::de::DeserializeOwned;
 use std::fmt::Debug;
 use std::fs::File;
+use std::io::BufReader;
 use std::path::Path;
 
 pub trait TestExecutor<S> {
@@ -16,13 +17,18 @@ where
     S: DeserializeOwned + Debug + Clone,
     P: AsRef<Path>,
 {
-    let reader = File::open(path.as_ref())
+    // open test file
+    let file = File::open(path.as_ref())
         .wrap_err_with(|| format!("test file {:?} not found.", path.as_ref()))?;
+    let reader = BufReader::new(file);
+
+    // parse test file
     let states: Vec<S> = serde_json::de::from_reader(reader)
         .wrap_err_with(|| format!("test file {:?} could not be deserialized", path.as_ref()))?;
 
     let mut states = states.into_iter();
 
+    // check the initial state
     if let Some(state) = states.next() {
         if !executor.check_initial_state(state.clone()) {
             return Err(eyre!("check failed on initial state:\n{:#?}", state));
@@ -32,6 +38,7 @@ where
         return Ok(());
     }
 
+    // check all the remaining states
     for state in states {
         if !executor.check_next_state(state.clone()) {
             return Err(eyre!(
