bb1062af5284070d597db5998b21cdeeedff9228
Vitor Enes <vitorenesduarte@gmail.com>
2021-02-12 11:48:15 +0100

    Remove model_based test from the executor

--- modules/tests/executor/mod.rs
+++ modules/tests/executor/mod.rs
@@ -250,20 +250,3 @@ impl modelator::TestExecutor<Step> for IBCTestExecutor {
         outcome_matches && self.check_chain_heights(step.chains)
     }
 }
-
-const TESTS_DIR: &str = "tests/support/model_based/tests";
-
-#[test]
-fn model_based() {
-    let tests = vec!["ICS02UpdateOKTest", "ICS02HeaderVerificationFailureTest"];
-
-    for test in tests {
-        let test = format!("{}/{}.json", TESTS_DIR, test);
-        let executor = IBCTestExecutor::new();
-        // we should be able to just return the `Result` once the following issue
-        // is fixed: https://github.com/rust-lang/rust/issues/43301
-        if let Err(e) = modelator::test(&test, executor) {
-            panic!("{:?}", e);
-        }
-    }
-}
