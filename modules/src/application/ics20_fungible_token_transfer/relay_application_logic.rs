9abebb5aa483f96bf09f78fe5741fead32de7153
cezarad <9439384+cezarad@users.noreply.github.com>
2021-03-03 07:17:09 +0100

    remove dispatch in relay_application_logic

--- modules/src/application/ics20_fungible_token_transfer/relay_application_logic.rs
+++ modules/src/application/ics20_fungible_token_transfer/relay_application_logic.rs
@@ -1,24 +1,3 @@
 //! This module implements the processing logic for ICS20 (token transfer) message.
 
-use crate::application::ics20_fungible_token_transfer::context::Ics20Context;
-use crate::handler::HandlerOutput;
-use crate::ics04_channel::packet::PacketResult;
-
-use super::error::{Error, Kind};
-use super::msgs::transfer::MsgTransfer;
-use super::relay_application_logic;
-
 pub mod send_transfer;
-
-/// Entry point for processing a transfer message in ICS20 token transfer
-pub fn dispatch<Ctx>(ctx: &mut Ctx, msg: MsgTransfer) -> Result<HandlerOutput<PacketResult>, Error>
-where
-    Ctx: Ics20Context,
-{
-    //TODO: application logic
-
-    let handler_output = relay_application_logic::send_transfer::send_transfer(ctx, msg)
-        .map_err(|e| Kind::HandlerRaisedError.context(e))?;
-
-    Ok(handler_output)
-}
