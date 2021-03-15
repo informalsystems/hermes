77691624ffcd0121924bf55ea83a28fdeeb5e557
Adi Seredinschi <adi@informal.systems>
2021-03-15 16:08:52 +0100

    Added Protobuf impl for MsgUpgradeAnyClient

--- modules/src/ics02_client/msgs/upgrade_client.rs
+++ modules/src/ics02_client/msgs/upgrade_client.rs
@@ -1,10 +1,16 @@
 //! Definition of domain type msg `MsgUpgradeAnyClient`.
 
+use std::convert::TryFrom;
+use std::str::FromStr;
+
+use tendermint_proto::Protobuf;
+
 use ibc_proto::ibc::core::client::v1::MsgUpgradeClient as RawMsgUpgradeClient;
 use ibc_proto::ibc::core::commitment::v1::MerkleProof;
 
 use crate::ics02_client::client_consensus::AnyConsensusState;
 use crate::ics02_client::client_state::AnyClientState;
+use crate::ics02_client::error::Kind;
 use crate::ics23_commitment::commitment::CommitmentProofBytes;
 use crate::ics24_host::identifier::ClientId;
 use crate::signer::Signer;
@@ -36,6 +42,8 @@ impl Msg for MsgUpgradeAnyClient {
     }
 }
 
+impl Protobuf<RawMsgUpgradeClient> for MsgUpgradeAnyClient {}
+
 impl From<MsgUpgradeAnyClient> for RawMsgUpgradeClient {
     fn from(dm_msg: MsgUpgradeAnyClient) -> RawMsgUpgradeClient {
         let c_bytes: CommitmentProofBytes = dm_msg.proof_upgrade_client.into();
@@ -49,5 +57,35 @@ impl From<MsgUpgradeAnyClient> for RawMsgUpgradeClient {
             proof_upgrade_consensus_state: cs_bytes.into(),
             signer: dm_msg.signer.to_string(),
         }
+    }
+}
+
+impl TryFrom<RawMsgUpgradeClient> for MsgUpgradeAnyClient {
+    type Error = Kind;
+
+    fn try_from(proto_msg: RawMsgUpgradeClient) -> Result<Self, Self::Error> {
+        let raw_client_state = proto_msg
+            .client_state
+            .ok_or(Kind::InvalidRawClientState)?;
+        let raw_consensus_state = proto_msg
+            .consensus_state
+            .ok_or(Kind::InvalidRawConsensusState)?;
+
+        let c_bytes = CommitmentProofBytes::from(proto_msg.proof_upgrade_client);
+        let cs_bytes = CommitmentProofBytes::from(proto_msg.proof_upgrade_consensus_state);
+
+        Ok(MsgUpgradeAnyClient {
+            client_id: ClientId::from_str(&proto_msg.client_id)
+                .map_err(|e| Kind::InvalidClientIdentifier(e.kind().clone()))?,
+            client_state: AnyClientState::try_from(raw_client_state)
+                .map_err(|_| Kind::InvalidRawClientState)?,
+            consensus_state: AnyConsensusState::try_from(raw_consensus_state)
+                .map_err(|_| Kind::InvalidRawConsensusState)?,
+            proof_upgrade_client: MerkleProof::try_from(c_bytes)
+                .map_err(|e| Kind::InvalidUpgradeClientProof(e.kind().clone()))?,
+            proof_upgrade_consensus_state: MerkleProof::try_from(cs_bytes)
+                .map_err(|e| Kind::InvalidUpgradeConsensusStateProof(e.kind().clone()))?,
+            signer: proto_msg.signer.into(),
+        })
     }
 }
