2e2dc0b96a57ca58b3f26f4818a6180c15c939dd
Adi Seredinschi <adi@informal.systems>
2021-04-07 18:35:15 +0200

    Tracing with custom writer to ensure stderr output. h/t @romac

--- relayer-cli/src/components.rs
+++ relayer-cli/src/components.rs
@@ -1,18 +1,24 @@
+use std::io;
+
 use abscissa_core::{Component, FrameworkError};
 use tracing_subscriber::fmt::{
     format::{Format, Json, JsonFields},
     time::SystemTime,
-    Formatter,
+    Formatter as TracingFormatter,
 };
 use tracing_subscriber::util::SubscriberInitExt;
 use tracing_subscriber::{reload::Handle, EnvFilter, FmtSubscriber};
 
 use ibc_relayer::config::GlobalConfig;
 
+/// Custom types to simplify the `Tracing` definition below
+type Formatter = TracingFormatter<JsonFields, Format<Json, SystemTime>, StdWriter>;
+type StdWriter = fn() -> io::Stderr;
+
 /// Abscissa component for initializing the `tracing` subsystem
 #[derive(Component, Debug)]
 pub struct Tracing {
-    filter_handle: Handle<EnvFilter, Formatter<JsonFields, Format<Json, SystemTime>>>,
+    filter_handle: Handle<EnvFilter, Formatter>,
 }
 
 /// A custom component for parametrizing `tracing` in the relayer. Primarily used for:
@@ -21,6 +27,7 @@ pub struct Tracing {
 ///     - enables JSON-formatted output
 impl Tracing {
     /// Creates a new [`Tracing`] component
+    #[allow(trivial_casts)]
     pub fn new(cfg: GlobalConfig) -> Result<Self, FrameworkError> {
         let filter = cfg.log_level;
         let use_color = false;
@@ -28,6 +35,7 @@ impl Tracing {
         // Construct a tracing subscriber with the supplied filter and enable reloading.
         let builder = FmtSubscriber::builder()
             .with_env_filter(filter)
+            .with_writer(std::io::stderr as StdWriter)
             .with_ansi(use_color)
             .json()
             .with_filter_reloading();
