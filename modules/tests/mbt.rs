386467538225c4472c972d06d337d921616eda11
Romain Ruetschi <romain@informal.systems>
2021-04-26 18:13:28 +0200

    Trigger CI

--- modules/tests/mbt.rs
+++ modules/tests/mbt.rs
@@ -2,7 +2,7 @@ mod runner;
 
 #[test]
 fn mbt() {
-    // we should be able to just return the `Result` once the following
+    // We should be able to just return the `Result` once the following
     // issue is fixed: https://github.com/rust-lang/rust/issues/43301
     if let Err(e) = run_tests() {
         panic!("{}", e);
@@ -10,7 +10,7 @@ fn mbt() {
 }
 
 fn run_tests() -> Result<(), Box<dyn std::error::Error>> {
-    // run the test
+    // Run the test
     let tla_tests_file = "tests/support/model_based/IBCTests.tla";
     let tla_config_file = "tests/support/model_based/IBCTests.cfg";
     let runner = runner::IbcTestRunner::new();
