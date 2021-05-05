84c02a9fab5d7f1481c762bd56bb6dc62aa82e9c
Adi Seredinschi <adi@informal.systems>
2021-05-05 10:14:03 +0200

    Simplified start multi CLI

--- relayer-cli/src/commands/start_multi.rs
+++ relayer-cli/src/commands/start_multi.rs
@@ -6,26 +6,15 @@ use crate::conclude::Output;
 use crate::prelude::*;
 
 #[derive(Clone, Command, Debug, Options)]
-pub struct StartMultiCmd {
-    #[options(help = "discover and relay on all paths for all chains in the config file")]
-    all: bool,
-}
-
-impl StartMultiCmd {
-    fn cmd(&self) -> Result<Output, BoxError> {
-        let config = app_config();
-
-        let supervisor = Supervisor::spawn(config.clone()).expect("failed to spawn supervisor");
-        supervisor.run()?;
-
-        Ok(Output::success_msg("done"))
-    }
-}
+pub struct StartMultiCmd {}
 
 impl Runnable for StartMultiCmd {
     fn run(&self) {
-        match self.cmd() {
-            Ok(output) => output.exit(),
+        let config = app_config();
+
+        let supervisor = Supervisor::spawn(config.clone()).expect("failed to spawn supervisor");
+        match supervisor.run() {
+            Ok(()) => Output::success_msg("done").exit(),
             Err(e) => Output::error(format!("{}", e)).exit(),
         }
     }
