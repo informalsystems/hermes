e935faa52f2edb37768c7a80487ad0705aa355e6
DaviRain-Su <davirain.yin@gamil.com>
2021-06-11 18:24:52 +0800

    Use displaydoc for ics24_host

--- modules/src/tx_msg.rs
+++ modules/src/tx_msg.rs
@@ -3,7 +3,7 @@ use prost_types::Any;
 use crate::ics24_host::error::ValidationError;
 
 pub trait Msg: Clone {
-    type ValidationError: std::error::Error;
+    type ValidationError: flex_error::StdErr;
     type Raw: From<Self> + prost::Message;
 
     // TODO: Clarify what is this function supposed to do & its connection to ICS26 routing mod.
