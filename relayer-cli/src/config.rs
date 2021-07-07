66bae2f75c8d401c163ee7aae5e5566f2e400f54
Shoaib Ahmed <sufialhussaini@gmail.com>
2021-07-07 14:53:00 +0530

    Validate trust-thresholds

--- relayer-cli/src/config.rs
+++ relayer-cli/src/config.rs
@@ -10,6 +10,7 @@ use thiserror::Error;
 
 use ibc::ics24_host::identifier::ChainId;
 pub use ibc_relayer::config::Config;
+use tendermint_light_client::types::TrustThreshold;
 
 /// Specifies all the possible syntactic errors
 /// that a Hermes configuration file could contain.
@@ -26,16 +27,43 @@ pub enum Error {
     /// Duplicate chains configured
     #[error("config file has duplicate entry for the chain with id {0}")]
     DuplicateChains(ChainId),
+
+    /// Invalid trust threshold
+    #[error("config file specifies an invalid trust threshold ({0}) for the chain with id {1}, caused by: {2}")]
+    InvalidTrustThreshold(TrustThreshold, ChainId, String),
 }
 
 /// Method for syntactic validation of the input
 /// configuration file.
 pub fn validate_config(config: &Config) -> Result<(), Error> {
-    // Check for duplicate chain configuration.
+    // Check for duplicate chain configuration and invalid trust-thresholds
     let mut unique_chain_ids = BTreeSet::new();
-    for chain_id in config.chains.iter().map(|c| c.id.clone()) {
-        if !unique_chain_ids.insert(chain_id.clone()) {
-            return Err(Error::DuplicateChains(chain_id));
+    for c in &config.chains {
+        if !unique_chain_ids.insert(c.id.clone()) {
+            return Err(Error::DuplicateChains(c.id.clone()));
+        }
+
+        // TODO: move below validation to tendermint::trust_threshold
+        if c.trust_threshold.denominator == 0 {
+            return Err(Error::InvalidTrustThreshold(
+                c.trust_threshold,
+                c.id.clone(),
+                "Denominator cannot be zero".to_owned(),
+            ));
+        }
+        if c.trust_threshold.numerator * 3 < c.trust_threshold.denominator {
+            return Err(Error::InvalidTrustThreshold(
+                c.trust_threshold,
+                c.id.clone(),
+                "Threshold cannot be < 1/3".to_owned(),
+            ));
+        }
+        if c.trust_threshold.numerator >= c.trust_threshold.denominator {
+            return Err(Error::InvalidTrustThreshold(
+                c.trust_threshold,
+                c.id.clone(),
+                "Threshold cannot be >= 1".to_owned(),
+            ));
         }
     }
 
