8252f129c203541d4cd2401eb6e14a28b33a5301
Shoaib Ahmed <sufialhussaini@gmail.com>
2021-07-15 16:12:05 +0530

    Add delete all option

--- relayer-cli/src/commands/keys/delete.rs
+++ relayer-cli/src/commands/keys/delete.rs
@@ -15,35 +15,49 @@ pub struct KeysDeleteCmd {
     #[options(free, required, help = "identifier of the chain")]
     chain_id: ChainId,
 
-    #[options(
-        short = "n",
-        help = "name of the key (defaults to the `key_name` defined in the config)"
-    )]
+    #[options(short = "n", help = "name of the key")]
     name: Option<String>,
+
+    #[options(short = "a", help = "delete all keys")]
+    all: bool,
 }
 
 impl KeysDeleteCmd {
-    fn options(&self, config: &Config) -> Result<KeysDeleteOptions, BoxError> {
+    fn options(&self, config: &Config) -> Result<KeysDeleteOptions<'_>, BoxError> {
         let chain_config = config
             .find_chain(&self.chain_id)
             .ok_or_else(|| format!("chain '{}' not found in configuration file", self.chain_id))?;
 
-        let name = self
-            .name
-            .clone()
-            .unwrap_or_else(|| chain_config.key_name.clone());
+        let id = match (self.all, &self.name) {
+            (true, Some(_)) => {
+                return Err("cannot set both -n/--name and -a/--all".to_owned().into());
+            }
+            (false, None) => {
+                return Err("must provide either -n/--name or -a/--all"
+                    .to_owned()
+                    .into());
+            }
+            (true, None) => KeysDeleteId::All,
+            (false, Some(ref name)) => KeysDeleteId::Named(name),
+        };
 
         Ok(KeysDeleteOptions {
             config: chain_config.clone(),
-            name,
+            id,
         })
     }
 }
 
 #[derive(Clone, Debug)]
-pub struct KeysDeleteOptions {
-    pub name: String,
-    pub config: ChainConfig,
+struct KeysDeleteOptions<'a> {
+    id: KeysDeleteId<'a>,
+    config: ChainConfig,
+}
+
+#[derive(Clone, Debug)]
+enum KeysDeleteId<'a> {
+    All,
+    Named(&'a str),
 }
 
 impl Runnable for KeysDeleteCmd {
@@ -55,19 +69,37 @@ impl Runnable for KeysDeleteCmd {
             Ok(result) => result,
         };
 
-        match delete_key(&opts.config, &opts.name) {
-            Ok(_) => Output::success_msg(format!(
-                "Removed key '{}' on chain {}",
-                opts.name, opts.config.id
-            ))
-            .exit(),
-            Err(e) => Output::error(format!("{}", e)).exit(),
-        }
+        match opts.id {
+            KeysDeleteId::All => match delete_all_keys(&opts.config) {
+                Ok(_) => {
+                    Output::success_msg(format!("Removed all keys on chain {}", opts.config.id))
+                        .exit()
+                }
+                Err(e) => Output::error(format!("{}", e)).exit(),
+            },
+            KeysDeleteId::Named(name) => match delete_key(&opts.config, name) {
+                Ok(_) => Output::success_msg(format!(
+                    "Removed key ({}) on chain {}",
+                    name, opts.config.id
+                ))
+                .exit(),
+                Err(e) => Output::error(format!("{}", e)).exit(),
+            },
+        };
     }
 }
 
 pub fn delete_key(config: &ChainConfig, key_name: &str) -> Result<(), BoxError> {
     let mut keyring = KeyRing::new(Store::Test, &config.account_prefix, &config.id)?;
     keyring.remove_key(key_name)?;
+    Ok(())
+}
+
+pub fn delete_all_keys(config: &ChainConfig) -> Result<(), BoxError> {
+    let mut keyring = KeyRing::new(Store::Test, &config.account_prefix, &config.id)?;
+    let keys = keyring.keys()?;
+    for key in keys {
+        keyring.remove_key(&key.0)?;
+    }
     Ok(())
 }
