467c02f0f1f1c408e2fd9a00bafd29414ddc2635
Romain Ruetschi <romain@informal.systems>
2021-07-21 17:52:04 +0200

    Update to tendermint-rs v0.21.0

--- modules/src/ics07_tendermint/error.rs
+++ modules/src/ics07_tendermint/error.rs
@@ -10,6 +10,9 @@ pub enum Kind {
     #[error("invalid trusting period")]
     InvalidTrustingPeriod,
 
+    #[error("invalid client state trust threshold")]
+    InvalidTrustThreshold,
+
     #[error("invalid unbonding period")]
     InvalidUnboundingPeriod,
 
