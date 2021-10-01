32a520cdd433dd27ce03c9bde0deaba2bf0e8691
Sean Chen <seanchen11235@gmail.com>
2021-10-01 11:00:06 -0500

    First editing pass of ibc/lib.rs

--- modules/src/lib.rs
+++ modules/src/lib.rs
@@ -15,19 +15,31 @@
 )]
 #![forbid(unsafe_code)]
 
-//! Implementation of the following ICS modules:
+//! This library implements the _I_nter_B_lockchain _C_ommunication (IBC) protocol in Rust. IBC is
+//! a distributed protocol that enables communication between distinct sovereign blockchains.
+//! Loose analogies may be drawn between the IBC protocol and the TCP/UDP protocols that enable
+//! communication over the internet via packet streaming. Indeed, IBC also encodes the notion of
+//! ordered and unordered packet streams.
 //!
-//! - ICS 02: Client
-//! - ICS 03: Connection
-//! - ICS 04: Channel
-//! - ICS 05: Port
-//! - ICS 07: Tendermint Client
-//! - ICS 18: Basic relayer functions
-//! - ICS 23: Vector Commitment Scheme
-//! - ICS 24: Host Requirements
-//! - ICS 26: Routing
-//! - Applications:
-//!    - ICS 20: Fungible Token Transfer
+//! At its highest level, IBC can be categorized into on-chain modules and off-chain processes.
+//! On-chain modules, so called because they are the building blocks upon which IBC chains are
+//! built, is where the main data structures and abstractions of the IBC protocol reside. These 
+//! abstractions, sometimes also referred to as 'handlers', are specified in _I_nter_C_hain 
+//! _S_tandards (ICSs), which can be found [here][ibc-standards]. 
+//!
+//! The main IBC handlers are `Client`s, `Connection`s, and `Channel`s:
+//! - `Client`s ([ICS 02][ics02]) encapsulate encapsulate the verification methods of another
+//! IBC-enabled chain in order to ensure that the other chain adheres to the IBC protocol and does
+//! not exhibit any misbehavior.
+//! - `Connection`s ([ICS 03][ics03]) associate a chain with another chain by connecting a `Client`
+//! on the local chain with a `Client` on the remote chain.
+//! - `Channel`s ([ICS 04][ics04]) ensure, among other things, that packets sent between connected
+//! chains are well-ordered.
+//!
+//! [ics02]: https://github.com/cosmos/ibc/blob/master/spec/core/ics-002-client-semantics/README.md
+//! [ics03]: https://github.com/cosmos/ibc/tree/master/spec/core/ics-003-connection-semantics
+//! [ics04]: https://github.com/cosmos/ibc/tree/master/spec/core/ics-004-channel-and-packet-semantics
+//! [ics-standards]: https://github.com/cosmos/ibc#interchain-standards
 
 extern crate alloc;
 extern crate std;
