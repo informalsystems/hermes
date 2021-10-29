940c957a1c0ebce8bf3fedaecc6770eee6421e9f
Adi Seredinschi <adi@informal.systems>
2021-10-29 11:16:37 +0200

    Nit: renamed arg to timed_out, added comment.

--- modules/src/core/ics04_channel/packet.rs
+++ modules/src/core/ics04_channel/packet.rs
@@ -115,10 +115,20 @@ pub struct Packet {
 }
 
 impl Packet {
-    pub fn timed_out(&self, now: &Timestamp, dst_chain_height: Height) -> bool {
+    /// Checks whether a packet from a [`SendPacket`] event is
+    /// timed-out relative to the current state of the destination
+    /// chain.
+    ///
+    /// Checks both for time-out relative to the destination chain's
+    /// current timestamp `dst_chain_ts` as well as relative to
+    /// the height `dst_chain_height`.
+    ///
+    /// Note: a timed-out packet should result in a [`MsgTimeout`],
+    /// instead of the common-case where it results in [`MsgRecvPacket`].
+    pub fn timed_out(&self, dst_chain_ts: &Timestamp, dst_chain_height: Height) -> bool {
         (self.timeout_height != Height::zero() && self.timeout_height < dst_chain_height)
             || (self.timeout_timestamp != Timestamp::none()
-                && now.check_expiry(&self.timeout_timestamp) == Expired)
+                && dst_chain_ts.check_expiry(&self.timeout_timestamp) == Expired)
     }
 }
 
