ca0cdba7b53719257f4bea0b53c0f4cf95da24cb
Romain Ruetschi <romain@informal.systems>
2021-11-10 10:49:53 +0100

    Use oneline logs if RUST_BACKTRACE is unset, set to 0 or empty

--- relayer-cli/src/bin/hermes/main.rs
+++ relayer-cli/src/bin/hermes/main.rs
@@ -4,15 +4,19 @@
 #![forbid(unsafe_code)]
 
 use ibc_relayer_cli::application::APPLICATION;
-use ibc_relayer_cli::components::enable_ansi;
 
 /// Boot Cli
 fn main() -> eyre::Result<()> {
-    if enable_ansi() {
-        color_eyre::install()?;
-    } else {
+    if compact_logs() {
         oneline_eyre::install()?;
+    } else {
+        color_eyre::install()?;
     }
 
     abscissa_core::boot(&APPLICATION);
+}
+
+fn compact_logs() -> bool {
+    let backtrace = std::option_env!("RUST_BACKTRACE").unwrap_or("");
+    backtrace.is_empty() || backtrace == "0"
 }
