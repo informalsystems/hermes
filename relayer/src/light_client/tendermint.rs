921d11763f14e028cde4b01d2b16abda18023873
Zack Grannan <zgrannan@protonmail.com>
2021-12-08 08:46:49 -0800

    add relayer verification

--- relayer/src/light_client/tendermint.rs
+++ relayer/src/light_client/tendermint.rs
@@ -1,3 +1,6 @@
+#[cfg(feature="prusti")]
+use prusti_contracts::*;
+
 use itertools::Itertools;
 
 use tendermint_light_client::{
@@ -40,6 +43,196 @@ pub struct LightClient {
     io: components::io::ProdIo,
 }
 
+#[cfg(feature="prusti")]
+#[extern_spec]
+mod tendermint {
+    mod Time {
+
+        use prusti_contracts::*;
+        use tendermint_light_client::types::Time;
+
+        #[pure]
+        pub fn now() -> Time;
+    }
+}
+
+#[cfg(feature="prusti")]
+#[extern_spec]
+impl tendermint_light_client::state::State {
+    #[ensures(
+        forall(|i : usize| (i < result.len() ==>
+        target.signed_header.header.time > get_lightblock_header_time(&result, i)
+        ))
+    )]
+    #[ensures(
+        forall(|i: usize, j: usize| i < j ==> get_lightblock_height(&result, i) >= get_lightblock_height(&result, j))
+    )]
+    #[ensures(
+        forall(|i: usize, j: usize| i < j ==> get_lightblock_header_time(&result, i) >= get_lightblock_header_time(&result, j))
+    )]
+    pub fn get_trace_for_target(&self, target: &LightBlock) -> Vec<LightBlock>;
+}
+
+#[cfg(feature="prusti")]
+#[extern_spec]
+impl ibc::ics02_client::client_state::AnyClientState {
+    #[pure]
+    #[ensures(matches!(self, AnyClientState::Tendermint(_)) ==> result == get_trusting_period(self))]
+    pub fn trusting_period(&self) -> Duration;
+}
+
+#[cfg(feature="prusti")]
+#[extern_spec]
+impl <T, A: std::alloc::Allocator> std::vec::Vec<T, A> {
+
+    #[pure]
+    pub fn len(&self) -> usize;
+
+    #[ensures(self.len() == old(self.len()) + 1)]
+    pub fn push(&mut self, value: T);
+}
+
+#[cfg(feature="prusti")]
+#[extern_spec]
+impl tendermint_light_client::light_client::LightClient {
+
+    #[ensures(result.options.trusting_period == options.trusting_period)]
+    pub fn new(
+       options: TmOptions
+    ) -> tendermint_light_client::light_client::LightClient;
+
+    #[ensures(
+        result.is_ok() ==>
+        is_within_trust_period(
+          unwrap_verified(&result),
+          self.options.trusting_period,
+          0
+        )
+    )]
+    pub fn verify_to_target(
+        &self,
+        target_height: tendermint::block::Height,
+        state: &mut tendermint_light_client::state::State,
+    ) -> Result<LightBlock, tendermint_light_client::errors::Error>;
+}
+
+#[cfg(feature="prusti")]
+#[pure]
+#[trusted]
+fn get_lightblock_height(m: &Vec<LightBlock>, i: usize) -> Height {
+    m[i].height()
+}
+
+#[cfg(feature="prusti")]
+#[extern_spec]
+mod tendermint_light_client {
+    mod contracts {
+
+        use prusti_contracts::*;
+        use tendermint_light_client::types::Time;
+        use tendermint_light_client::types::LightBlock;
+        use crate::light_client::tendermint::header_within_trust_period;
+
+        #[pure]
+        #[ensures(header_within_trust_period(&light_block.signed_header, trusting_period, now))]
+        pub fn is_within_trust_period(
+            light_block: &LightBlock,
+            trusting_period: i32,
+            now: i32) -> bool;
+    }
+}
+
+#[cfg(feature="prusti")]
+#[pure]
+#[requires(matches!(v, Ok(_)))]
+fn get_options(v: &Result<TmLightClient, Error>) -> TmOptions {
+  match v {
+      Ok(r) => r.options,
+      Err(_) => unreachable!()
+  }
+}
+
+#[cfg(feature="prusti")]
+#[pure]
+fn get_witness(m: &AnyMisbehaviour) -> &SignedHeader {
+   match m {
+       AnyMisbehaviour::Tendermint(t) => &t.header2.signed_header,
+       _ => unreachable!()
+   }
+}
+
+#[cfg(feature="prusti")]
+#[pure]
+fn get_witness_time(m: &AnyMisbehaviour) -> Time {
+    match m {
+        AnyMisbehaviour::Tendermint(t) => t.header2.signed_header.header.time,
+        _ => unreachable!()
+    }
+}
+
+#[cfg(feature="prusti")]
+predicate! {
+   fn verify_spec_internal(target: &LightBlock, supporting: &Vec<LightBlock>) -> bool {
+    forall(|i: usize| (0 <= i && i < supporting.len()) ==>
+           target.signed_header.header.time >
+            get_lightblock_header_time(&supporting, i)
+    )
+   }
+}
+
+#[cfg(feature="prusti")]
+predicate! {
+    fn verify_spec(r: &Result<Verified<LightBlock>, Error>) -> bool {
+        match r {
+            Ok(v) => verify_spec_internal(&v.target, &v.supporting)
+            _ => true
+        }
+    }
+}
+
+#[cfg(feature="prusti")]
+predicate! {
+    fn check_misbehaviour_spec(client_state: &AnyClientState, r: &Result<Option<MisbehaviourEvidence>, Error>) -> bool {
+        match r {
+            Ok(Some(m)) =>
+                misbehaviour_invariant(m) &&
+                header_within_trust_period(&get_witness(&m.misbehaviour), client_state.trusting_period(), 0),
+            _ => true
+        }
+    }
+}
+
+#[cfg(feature="prusti")]
+predicate! {
+    fn misbehaviour_invariant(m: &MisbehaviourEvidence) -> bool {
+    forall(
+        |i : usize|
+        (0 <= i && i < m.supporting_headers.len() ==>
+          get_supporting_header_time(&m.supporting_headers, i) < get_witness_time(&m.misbehaviour)))
+    }
+}
+
+#[cfg(feature="prusti")]
+predicate! {
+    fn adjust_headers_time_spec(header: &TmHeader, sup: &Vec<TmHeader>) -> bool {
+        forall(|i: usize| (0 <= i && i < sup.len()) ==>
+            header.signed_header.header.time > get_header_time(sup, i)
+        )
+    }
+}
+
+#[cfg(feature="prusti")]
+predicate! {
+    fn adjust_headers_spec(target: &LightBlock, r: &Result<(TmHeader, Vec<TmHeader>), Error>) -> bool {
+        match r {
+            Ok((header, sup)) => header.signed_header == target.signed_header &&
+                adjust_headers_time_spec(header, sup),
+            _ => true
+        }
+    }
+}
+
+
 impl super::LightClient<CosmosSdkChain> for LightClient {
     fn header_and_minimal_set(
         &mut self,
@@ -52,6 +245,14 @@ impl super::LightClient<CosmosSdkChain> for LightClient {
         Ok(Verified { target, supporting })
     }
 
+    #[cfg_attr(feature="prusti", ensures(result.is_ok() ==>
+        is_within_trust_period(
+          &get_verified_target(&result),
+          client_state.trusting_period(),
+          0
+        )
+     ))]
+    #[cfg_attr(feature="prusti", ensures(verify_spec(&result)))]
     fn verify(
         &mut self,
         trusted: ibc::Height,
@@ -98,6 +299,7 @@ impl super::LightClient<CosmosSdkChain> for LightClient {
     ///
     /// ## TODO
     /// - [ ] Return intermediate headers as well
+    #[cfg_attr(feature="prusti", ensures(check_misbehaviour_spec(old(client_state), &result)))]
     fn check_misbehaviour(
         &mut self,
         update: UpdateClient,
@@ -177,6 +379,7 @@ impl LightClient {
         })
     }
 
+    #[cfg_attr(feature="prusti", ensures(result.is_ok() ==> get_options(&result).trusting_period == client_state.trusting_period()))]
     fn prepare_client(&self, client_state: &AnyClientState) -> Result<TmLightClient, Error> {
         let clock = components::clock::SystemClock;
         let hasher = operations::hasher::ProdHasher;
@@ -228,6 +431,11 @@ impl LightClient {
             .map_err(|e| Error::light_client_io(self.chain_id.to_string(), e))
     }
 
+    #[cfg_attr(feature="prusti", requires(
+        forall(|i : usize| (0 <= i && i < supporting.len()) ==>
+               target.signed_header.header.time > get_lightblock_header_time(&supporting, i)
+        )))]
+    #[cfg_attr(feature="prusti", ensures(adjust_headers_spec(old(&target), &result)))]
     fn adjust_headers(
         &mut self,
         trusted_height: ibc::Height,
