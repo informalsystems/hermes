4869758222db5c07d4be3b271ae0fb747b629173
Soares Chen <soares.chen@gmail.com>
2021-12-17 18:16:25 +0100

    Remove connection expiration test
    The same test is now within the channel expiration test

--- tools/integration-test/src/tests/manual/client_expiration.rs
+++ tools/integration-test/src/tests/manual/client_expiration.rs
@@ -26,43 +26,6 @@ use crate::relayer::connection::{
 const CLIENT_EXPIRY: Duration = Duration::from_secs(15);
 
 /**
-   A manual test to verify that the connection worker is properly terminated
-   instead of looping indefinitely when it is not possible to perform
-   connection handshake due to the client being expired or frozen.
-
-   Since the test involves long-running background tasks, it has to
-   be verified manually by inspecting the logs. Run the test with
-   the following command:
-
-   ```bash
-   RUST_BACKTRACE=0 RUST_LOG=info cargo test --features manual \
-     -p ibc-integration-test -- test_connection_expiration
-   ```
-
-   And you should see simple failure toward the end of the test that looks like:
-
-   ```log
-   INFO ibc_integration_test::tests::client_expiration: Sleeping for 20 seconds to wait for IBC client to expire
-   INFO ibc_integration_test::tests::client_expiration: Trying to create connection after client is expired
-   INFO ibc_relayer::chain::cosmos: [ibc-beta-25716970] waiting for commit of tx hashes(s) 15030785428E01BE2EAEE35275A29977593F3E5ADCC5ED8AA7CDA4D1EDEC6515
-   WARN ibc_integration_test::util::suspend: suspending the test indefinitely. you can still interact with any spawned chains and relayers
-   ERROR ibc_relayer::connection: failed to establish connection handshake on frozen client:
-   0: failed during an operation on client (07-tendermint-0) hosted by chain (ibc-beta-25716970)
-   1: client 07-tendermint-0 on chain id ibc-beta-25716970 is expired or frozen
-   ERROR ibc_relayer::util::task: aborting task connection_worker after encountering fatal error:
-   0: Worker failed after 1 retries
-   ```
-
-   The error above should not repeat more than once. In the original code,
-   the connection worker would keep retrying and indefinitely flooding
-   the log with errors.
-*/
-#[test]
-fn test_connection_expiration() -> Result<(), Error> {
-    run_binary_chain_test(&ConnectionExpirationTest)
-}
-
-/**
    A manual test to verify that the channel worker is properly terminated
    instead of looping indefinitely when it is not possible to perform
    channel handshake due to the client being expired or frozen.
@@ -121,8 +84,6 @@ fn wait_for_client_expiry() {
 
 pub struct ExpirationTestOverrides;
 
-pub struct ConnectionExpirationTest;
-
 pub struct ChannelExpirationTest;
 
 pub struct PacketExpirationTest;
@@ -163,83 +124,6 @@ impl TestOverrides for ExpirationTestOverrides {
         _registry: &SharedRegistry<impl ChainHandle>,
     ) -> Result<Option<SupervisorHandle>, Error> {
         Ok(None)
-    }
-}
-
-impl BinaryChainTest for ConnectionExpirationTest {
-    fn run<ChainA: ChainHandle, ChainB: ChainHandle>(
-        &self,
-        _config: &TestConfig,
-        chains: ConnectedChains<ChainA, ChainB>,
-    ) -> Result<(), Error> {
-        // We spawn a supervisor first, but the refresh client is not
-        // triggered because it is currently tied to channels.
-        let _supervisor =
-            spawn_supervisor(chains.config.clone(), chains.registry.clone(), None, false)?;
-
-        wait_for_client_expiry();
-
-        info!("Trying to create connection after client is expired");
-
-        // We can submit an CONN_INIT packet and the connection worker will
-        // pick it up, and we want to observe that it abort due to frozen client
-        // rather than looping indefinitely.
-
-        let (connection_id_b, _) = init_connection(
-            &chains.handle_a,
-            &chains.handle_b,
-            &chains.client_b_to_a.tagged_client_id(),
-            &chains.client_a_to_b.tagged_client_id(),
-        )?;
-
-        info!("Sleeping for 10 seconds to make sure that connection fails to establish");
-
-        sleep(Duration::from_secs(10));
-
-        let connection_end_b = query_connection_end(&chains.handle_b, &connection_id_b.as_ref())?;
-
-        assert_eq(
-            "connection end status should remain init",
-            connection_end_b.value().state(),
-            &ConnectionState::Init,
-        )?;
-
-        assert_eq(
-            "connection end should not have counterparty",
-            &connection_end_b.tagged_counterparty_connection_id(),
-            &None,
-        )?;
-
-        info!("Trying to create new connection and worker after previous connection worker failed");
-
-        let client_b_to_a_2 = bootstrap_foreign_client(&chains.handle_b, &chains.handle_a)?;
-
-        let client_a_to_b_2 = bootstrap_foreign_client(&chains.handle_a, &chains.handle_b)?;
-
-        // Need to spawn refresh client for new clients to make sure they don't expire
-
-        let _refresh_task_a = spawn_refresh_client(client_b_to_a_2.clone())
-            .ok_or_else(|| eyre!("expect refresh task spawned"))?;
-
-        let _refresh_task_b = spawn_refresh_client(client_a_to_b_2.clone())
-            .ok_or_else(|| eyre!("expect refresh task spawned"))?;
-
-        // The second init connection on an unexpired client should succeed.
-
-        let (connection_id_b, _) = init_connection(
-            &chains.handle_a,
-            &chains.handle_b,
-            &client_b_to_a_2.tagged_client_id(),
-            &client_a_to_b_2.tagged_client_id(),
-        )?;
-
-        assert_eventually_connection_established(
-            &chains.handle_b,
-            &chains.handle_a,
-            &connection_id_b.as_ref(),
-        )?;
-
-        Ok(())
     }
 }
 
@@ -542,14 +426,6 @@ impl BinaryChainTest for CreateOnExpiredClientTest {
 }
 
 impl HasOverrides for CreateOnExpiredClientTest {
-    type Overrides = ExpirationTestOverrides;
-
-    fn get_overrides(&self) -> &ExpirationTestOverrides {
-        &ExpirationTestOverrides
-    }
-}
-
-impl HasOverrides for ConnectionExpirationTest {
     type Overrides = ExpirationTestOverrides;
 
     fn get_overrides(&self) -> &ExpirationTestOverrides {
