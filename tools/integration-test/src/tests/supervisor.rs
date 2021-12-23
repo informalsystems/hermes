db84c64fecf207892fca3956ee01b82f0e62d637
Soares Chen <soares.chen@gmail.com>
2021-12-23 19:06:14 +0100

    Add test for packet worker recovering from account sequence mismatch error

--- tools/integration-test/src/tests/supervisor.rs
+++ tools/integration-test/src/tests/supervisor.rs
@@ -1,4 +1,5 @@
 use ibc_relayer::config::{self, Config, ModeConfig};
+use crate::ibc::denom::derive_ibc_denom;
 
 use crate::prelude::*;
 use crate::relayer::channel::{assert_eventually_channel_established, init_channel};
@@ -65,11 +66,59 @@ impl BinaryChainTest for SupervisorTest {
             &port_b.as_ref(),
         )?;
 
-        assert_eventually_channel_established(
+        let channel_id_a = assert_eventually_channel_established(
             &chains.handle_b,
             &chains.handle_a,
             &channel_id_b.as_ref(),
             &port_b.as_ref(),
+        )?;
+
+        let denom_a = chains.node_a.denom();
+
+        let chaina_user1_balance = chains
+            .node_a
+            .chain_driver()
+            .query_balance(&chains.node_a.wallets().user1().address(), &denom_a)?;
+
+        let a_to_b_amount = 100;
+
+        info!(
+            "Sending IBC transfer from chain {} to chain {} with amount of {} {}",
+            chains.chain_id_a(),
+            chains.chain_id_b(),
+            a_to_b_amount,
+            denom_a
+        );
+
+        chains.node_a.chain_driver().transfer_token(
+            &port_a.as_ref(),
+            &channel_id_a.as_ref(),
+            &chains.node_a.wallets().relayer().address(),
+            &chains.node_b.wallets().user1().address(),
+            a_to_b_amount,
+            &denom_a,
+        )?;
+
+        let denom_b = derive_ibc_denom(
+            &port_b.as_ref(),
+            &channel_id_b.as_ref(),
+            &denom_a,
+        )?;
+
+        info!(
+            "Packet worker should still succeed and recover from account sequence mismatch error",
+        );
+
+        chains.node_a.chain_driver().assert_eventual_wallet_amount(
+            &chains.node_a.wallets().relayer(),
+            chaina_user1_balance - a_to_b_amount,
+            &denom_a,
+        )?;
+
+        chains.node_b.chain_driver().assert_eventual_wallet_amount(
+            &chains.node_b.wallets().user1(),
+            a_to_b_amount,
+            &denom_b.as_ref(),
         )?;
 
         Ok(())
