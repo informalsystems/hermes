1a69c4d448e2ebab1134c08810ef01dfea440a6a
Romain Ruetschi <romain@informal.systems>
2022-02-04 12:59:59 +0100

    Handle non-standard ports in channel handshake (#1840)
    * Use empty version for `ChanOpenInit` when port is non-standard and source channel version for `ChanOpenTry` steps
    
    * Remove unused error variant
    
    * Remove misleading comment
    
    * Add changelog entry

---
+++ .changelog/unreleased/improvements/1837-non-standard-ports.md
@@ -0,0 +1,2 @@
+- Handle non-standard ports in channel handshake
+  ([#1837](https://github.com/informalsystems/ibc-rs/issues/1837))
--- relayer/src/channel/version.rs
+++ relayer/src/channel/version.rs
@@ -4,32 +4,24 @@
 //! channel version to be used in a channel open
 //! handshake.
 
-use tracing::warn;
-
 use ibc::{
     applications::{ics20_fungible_token_transfer, ics27_interchain_accounts},
     core::{ics04_channel::Version, ics24_host::identifier::PortId},
 };
 
-use crate::channel::ChannelError;
-
-/// Returns the default channel version, depending on the the given [`PortId`].
-pub fn default_by_port(port_id: &PortId) -> Result<Version, ChannelError> {
-    if port_id.as_str() == ics20_fungible_token_transfer::PORT_ID {
-        // https://github.com/cosmos/ibc/tree/master/spec/app/ics-020-fungible-token-transfer#forwards-compatibility
-        Ok(Version::ics20())
-    } else if port_id
-        .as_str()
-        .starts_with(ics27_interchain_accounts::PORT_ID_PREFIX)
-    {
-        // https://github.com/cosmos/ibc/tree/master/spec/app/ics-027-interchain-accounts#channel-lifecycle-management
-        Ok(Version::ics27())
-    } else {
-        warn!(
-            port = %port_id,
-            "cannot resolve channel version for unknown port",
-        );
-
-        Err(ChannelError::invalid_port_id(port_id.clone()))
+/// Returns the default channel version, for the given [`PortId`].
+///
+/// Currently only ICS20 and ICS27 ports are recognized.
+pub fn default_by_port(port_id: &PortId) -> Option<Version> {
+    match port_id.as_str() {
+        ics20_fungible_token_transfer::PORT_ID => {
+            // https://github.com/cosmos/ibc/tree/master/spec/app/ics-020-fungible-token-transfer#forwards-compatibility
+            Some(Version::ics20())
+        }
+        port_id if port_id.starts_with(ics27_interchain_accounts::PORT_ID_PREFIX) => {
+            // https://github.com/cosmos/ibc/tree/master/spec/app/ics-027-interchain-accounts#channel-lifecycle-management
+            Some(Version::ics27())
+        }
+        _ => None,
     }
 }
