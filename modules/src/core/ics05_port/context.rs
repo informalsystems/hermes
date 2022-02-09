786826da4440ccfce15bc92b4f2be95bd5035b06
Shoaib Ahmed <sufialhussaini@gmail.com>
2022-02-09 20:54:44 +0530

    Add CapabilityName newtype

--- modules/src/core/ics05_port/context.rs
+++ modules/src/core/ics05_port/context.rs
@@ -1,4 +1,4 @@
-use crate::core::ics05_port::capabilities::Capability;
+use crate::core::ics05_port::capabilities::{Capability, CapabilityName};
 use crate::core::ics05_port::error::Error;
 use crate::core::ics24_host::identifier::PortId;
 use crate::core::ics24_host::path::PortsPath;
@@ -17,14 +17,22 @@ pub trait PortReader: CapabilityReader {
 
     /// Check if the specified port_id is already bounded
     fn is_bound(&self, port_id: PortId) -> bool {
-        self.get_capability(PortsPath(port_id).to_string()).is_ok()
+        self.get_capability(&Self::port_capability_name(port_id))
+            .is_ok()
     }
 
     /// Authenticate a capability key against a port_id by checking if the capability was previously
     /// generated and bound to the specified port
     fn authenticate(&self, port_id: PortId, capability: &Capability) -> bool {
-        self.authenticate_capability(PortsPath(port_id).to_string(), capability)
+        self.authenticate_capability(&Self::port_capability_name(port_id), capability)
             .is_ok()
+    }
+
+    fn port_capability_name(port_id: PortId) -> CapabilityName {
+        PortsPath(port_id)
+            .to_string()
+            .parse()
+            .expect("PortsPath cannot be empty string")
     }
 }
 
@@ -34,7 +42,7 @@ pub trait PortKeeper: CapabilityKeeper + PortReader {
         if self.is_bound(port_id.clone()) {
             Err(Error::port_already_bound(port_id))
         } else {
-            self.new_capability(PortsPath(port_id).to_string())
+            self.new_capability(Self::port_capability_name(port_id))
         }
     }
 }
@@ -42,25 +50,25 @@ pub trait PortKeeper: CapabilityKeeper + PortReader {
 pub trait CapabilityKeeper {
     /// Create a new capability with the given name.
     /// Return an error if the capability was already taken.
-    fn new_capability(&mut self, name: impl AsRef<str>) -> Result<Capability, Error>;
+    fn new_capability(&mut self, name: CapabilityName) -> Result<Capability, Error>;
 
     /// Claim the specified capability using the specified name.
     /// Return an error if the capability was already taken.
-    fn claim_capability(&mut self, name: impl AsRef<str>, capability: Capability);
+    fn claim_capability(&mut self, name: CapabilityName, capability: Capability);
 
     /// Release a previously claimed or created capability
-    fn release_capability(&mut self, name: impl AsRef<str>, capability: Capability);
+    fn release_capability(&mut self, name: CapabilityName, capability: Capability);
 }
 
 pub trait CapabilityReader {
     /// Fetch a capability which was previously claimed by specified name
-    fn get_capability(&self, name: impl AsRef<str>) -> Result<Capability, Error>;
+    fn get_capability(&self, name: &CapabilityName) -> Result<Capability, Error>;
 
     /// Authenticate a given capability and name. Lookup the capability from the internal store and
     /// check against the provided name.
     fn authenticate_capability(
         &self,
-        name: impl AsRef<str>,
+        name: &CapabilityName,
         capability: &Capability,
     ) -> Result<(), Error>;
 }
