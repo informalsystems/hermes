3fbf9dfb12ece4e6e27b10c3592b472590386cd7
Soares Chen <soares.chen@maybevoid.com>
2022-02-15 13:42:38 +0000

    Add documentation for array utilities

--- tools/integration-test/src/util/array.rs
+++ tools/integration-test/src/util/array.rs
@@ -1,13 +1,25 @@
+/**
+   Helpers for manipulating fixed-sized arrays.
+*/
 use core::convert::TryInto;
 use eyre::eyre;
 
 use crate::error::Error;
 
+/**
+   Converts a dynamic-sized vector `Vec<T>` into a fixed-sized array
+   `[T; SIZE]`. Fails if the vector is not the same length as `SIZE`.
+*/
 pub fn try_into_array<T, const SIZE: usize>(list: Vec<T>) -> Result<[T; SIZE], Error> {
     list.try_into()
         .map_err(|_| Error::generic(eyre!("vector is not of length {}", SIZE)))
 }
 
+/**
+   Converts a dynamic-sized nested vector `Vec<Vec<T>>` into a fixed-sized
+   nested array `[[T; SIZE]; SIZE]`. Fails if the nested vector is not of
+   `SIZE`x`SIZE` length.
+*/
 pub fn try_into_nested_array<T, const SIZE: usize>(
     list: Vec<Vec<T>>,
 ) -> Result<[[T; SIZE]; SIZE], Error> {
@@ -19,10 +31,17 @@ pub fn try_into_nested_array<T, const SIZE: usize>(
     try_into_array(list_a)
 }
 
+/**
+   Converts a fixed-sized nested array `[[T; SIZE]; SIZE]` into a nested
+   vector `Vec<Vec<T>>`.
+*/
 pub fn into_nested_vec<T, const SIZE: usize>(array: [[T; SIZE]; SIZE]) -> Vec<Vec<T>> {
     array.map(|array_b| array_b.into()).into()
 }
 
+/**
+   Map the elements in the fixed-sized array `[[T; SIZE]; SIZE]`.
+*/
 pub fn map_nested_array<T, R, const SIZE: usize>(
     array: [[T; SIZE]; SIZE],
     mapper: impl Fn(T) -> Result<R, Error>,
@@ -40,7 +59,11 @@ pub fn map_nested_array<T, R, const SIZE: usize>(
     try_into_nested_array(mapped)
 }
 
-pub fn assert_same_dimension<T>(size: usize, list: &[Vec<T>]) -> Result<(), Error> {
+/**
+   Asserts that a nested vector `Vec<Vec<T>>` has the same dimension
+   in its inner vectors.
+*/
+pub fn assert_same_dimension<T>(size: usize, list: &Vec<Vec<T>>) -> Result<(), Error> {
     if list.len() != size {
         return Err(Error::generic(eyre!(
             "expect nested vector to have the dimension {} x {}",
