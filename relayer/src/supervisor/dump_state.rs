f49f2c43343e4c20bd8cfb58385dec042832bb54
Romain Ruetschi <romain@informal.systems>
2022-03-15 13:56:04 +0100

    Show whether client workers are doing refresh and/or misbehavior in supervisor state

--- relayer/src/supervisor/dump_state.rs
+++ relayer/src/supervisor/dump_state.rs
@@ -8,18 +8,19 @@ use tracing::info;
 
 use crate::{
     object::{Object, ObjectType},
-    worker::WorkerId,
+    worker::{WorkerData, WorkerHandle, WorkerId},
 };
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct WorkerDesc {
     pub id: WorkerId,
     pub object: Object,
+    pub data: Option<WorkerData>,
 }
 
 impl WorkerDesc {
-    pub fn new(id: WorkerId, object: Object) -> Self {
-        Self { id, object }
+    pub fn new(id: WorkerId, object: Object, data: Option<WorkerData>) -> Self {
+        Self { id, object, data }
     }
 }
 
@@ -32,12 +33,12 @@ pub struct SupervisorState {
 impl SupervisorState {
     pub fn new<'a>(
         mut chains: Vec<ChainId>,
-        workers: impl Iterator<Item = (WorkerId, &'a Object)>,
+        workers: impl Iterator<Item = &'a WorkerHandle>,
     ) -> Self {
         chains.sort();
 
         let workers = workers
-            .map(|(id, o)| WorkerDesc::new(id, o.clone()))
+            .map(|h| WorkerDesc::new(h.id(), h.object().clone(), h.data().cloned()))
             .into_group_map_by(|desc| desc.object.object_type())
             .into_iter()
             .update(|(_, os)| os.sort_by_key(|desc| desc.object.short_name()))
