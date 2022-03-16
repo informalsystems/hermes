8c688c43f803d8560125a2e5cc27b23bd261f8a4
Romain Ruetschi <romain@informal.systems>
2022-03-16 15:00:26 +0100

    Comment out ICA filter test

--- tools/integration-test/src/tests/ica_filter.rs
+++ tools/integration-test/src/tests/ica_filter.rs
@@ -1,93 +1,93 @@
-use std::str::FromStr;
+// use std::str::FromStr;
 
-use ibc::core::ics04_channel::Version;
+// use ibc::core::ics04_channel::Version;
 
-use crate::{
-    bootstrap::binary::{
-        channel::bootstrap_channel_with_connection, connection::bootstrap_connection,
-    },
-    prelude::*,
-};
+// use crate::{
+//     bootstrap::binary::{
+//         channel::bootstrap_channel_with_connection, connection::bootstrap_connection,
+//     },
+//     prelude::*,
+// };
 
-// #[test]
-// fn test_ica_filter() -> Result<(), Error> {
-//     run_binary_chain_test(&IcaFilterTest)
+// // #[test]
+// // fn test_ica_filter() -> Result<(), Error> {
+// //     run_binary_chain_test(&IcaFilterTest)
+// // }
+
+// pub struct IcaFilterTest;
+
+// impl TestOverrides for IcaFilterTest {
+//     // Use deterministic identifiers for clients, connections, and channels
+//     fn modify_test_config(&self, config: &mut TestConfig) {
+//         config.bootstrap_with_random_ids = false;
+//     }
+
+//     // Do not start supervisor at the beginning of test
+//     fn spawn_supervisor(
+//         &self,
+//         _config: &SharedConfig,
+//         _registry: &SharedRegistry<impl ChainHandle>,
+//     ) -> Result<Option<SupervisorHandle>, Error> {
+//         Ok(None)
+//     }
 // }
 
-pub struct IcaFilterTest;
+// impl BinaryChainTest for IcaFilterTest {
+//     fn run<ChainA: ChainHandle, ChainB: ChainHandle>(
+//         &self,
+//         _config: &TestConfig,
+//         chains: ConnectedChains<ChainA, ChainB>,
+//     ) -> Result<(), Error> {
+//         let connection = bootstrap_connection(&chains.client_a_to_b, &chains.client_b_to_a, false)?;
 
-impl TestOverrides for IcaFilterTest {
-    // Use deterministic identifiers for clients, connections, and channels
-    fn modify_test_config(&self, config: &mut TestConfig) {
-        config.bootstrap_with_random_ids = false;
-    }
+//         dbg!(&connection);
 
-    // Do not start supervisor at the beginning of test
-    fn spawn_supervisor(
-        &self,
-        _config: &SharedConfig,
-        _registry: &SharedRegistry<impl ChainHandle>,
-    ) -> Result<Option<SupervisorHandle>, Error> {
-        Ok(None)
-    }
-}
+//         let wallet_a = chains.node_a.wallets().user1().cloned();
 
-impl BinaryChainTest for IcaFilterTest {
-    fn run<ChainA: ChainHandle, ChainB: ChainHandle>(
-        &self,
-        _config: &TestConfig,
-        chains: ConnectedChains<ChainA, ChainB>,
-    ) -> Result<(), Error> {
-        let connection = bootstrap_connection(&chains.client_a_to_b, &chains.client_b_to_a, false)?;
+//         dbg!(&wallet_a);
 
-        dbg!(&connection);
+//         chains.node_a.chain_driver().register_interchain_account(
+//             &wallet_a.address(),
+//             &connection.connection_id_b.as_ref(),
+//         )?;
 
-        let wallet_a = chains.node_a.wallets().user1().cloned();
+//         let icahost = PortId::from_str("icahost").unwrap();
+//         let icacontroller =
+//             PortId::from_str(&format!("icacontroller-{}", wallet_a.address().value())).unwrap();
 
-        dbg!(&wallet_a);
+//         let port_a: TaggedPortIdRef<ChainA, ChainB> = DualTagged::new(&icahost);
+//         let port_b: TaggedPortIdRef<ChainB, ChainA> = DualTagged::new(&icacontroller);
 
-        chains.node_a.chain_driver().register_interchain_account(
-            &wallet_a.address(),
-            &connection.connection_id_b.as_ref(),
-        )?;
+//         let version = Version::new(format!(
+//             r#"""
+//                 {{
+//                   "version": "ics27-1",
+//                   "controller_connection_id": {:?},
+//                   "host_connection_id": {:?},
+//                   "address": "",
+//                   "encoding": "proto3",
+//                   "tx_type": "sdk_multi_msg"
+//                 }}
+//             """#,
+//             connection.connection_id_a.value().to_string(),
+//             connection.connection_id_b.value().to_string(),
+//         ));
 
-        let icahost = PortId::from_str("icahost").unwrap();
-        let icacontroller =
-            PortId::from_str(&format!("icacontroller-{}", wallet_a.address().value())).unwrap();
+//         dbg!(&version);
 
-        let port_a: TaggedPortIdRef<ChainA, ChainB> = DualTagged::new(&icahost);
-        let port_b: TaggedPortIdRef<ChainB, ChainA> = DualTagged::new(&icacontroller);
+//         let channel = bootstrap_channel_with_connection(
+//             &chains.handle_b,
+//             &chains.handle_a,
+//             connection,
+//             &port_b,
+//             &port_a,
+//             Order::Ordered,
+//             version,
+//             false,
+//         )?;
 
-        let version = Version::new(format!(
-            r#"""
-                {{
-                  "version": "ics27-1",
-                  "controller_connection_id": {:?},
-                  "host_connection_id": {:?},
-                  "address": "",
-                  "encoding": "proto3",
-                  "tx_type": "sdk_multi_msg"
-                }}
-            """#,
-            connection.connection_id_a.value().to_string(),
-            connection.connection_id_b.value().to_string(),
-        ));
+//         dbg!(&channel);
 
-        dbg!(&version);
-
-        let channel = bootstrap_channel_with_connection(
-            &chains.handle_b,
-            &chains.handle_a,
-            connection,
-            &port_b,
-            &port_a,
-            Order::Ordered,
-            version,
-            false,
-        )?;
-
-        dbg!(&channel);
-
-        Ok(())
-    }
-}
+//         Ok(())
+//     }
+// }
