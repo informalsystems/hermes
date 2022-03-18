fc46971d955b569037de595020d5b0d0294745f3
Romain Ruetschi <romain@informal.systems>
2022-03-18 09:58:07 +0100

    Refactor ICA test

--- tools/integration-test/src/tests/ica_filter.rs
+++ tools/integration-test/src/tests/ica_filter.rs
@@ -46,41 +46,22 @@ impl BinaryChainTest for IcaFilterTestAllow {
         relayer: RelayerDriver,
         chains: ConnectedChains<ChainA, ChainB>,
     ) -> Result<(), Error> {
-        let connection =
-            bootstrap_connection(&chains.foreign_clients, default_connection_delay(), false)?;
-        dbg!(&connection);
-
-        let wallet_a = chains.node_a.wallets().user1().cloned();
-        dbg!(&wallet_a);
-
-        let _handle = relayer.spawn_supervisor()?;
-
-        chains.node_a.chain_driver().register_interchain_account(
-            &wallet_a.address(),
-            &connection.connection_id_a.as_ref(),
-        )?;
-
-        let channel_id_a: TaggedChannelId<ChainA, ChainB> =
-            TaggedChannelId::new("channel-0".parse().unwrap());
-
-        let icacontroller =
-            PortId::from_str(&format!("icacontroller-{}", wallet_a.address().value())).unwrap();
-
-        let port_id_a: TaggedPortIdRef<ChainA, ChainB> = DualTagged::new(&icacontroller);
+        let (_handle, wallet_a, connection_a, channel_id_a, port_id_a) =
+            bootstrap_and_register_interchain_account(&relayer, &chains, Duration::from_secs(0))?;
 
         let counterparty_channel_id = assert_eventually_channel_established(
             chains.handle_a(),
             chains.handle_b(),
             &channel_id_a.as_ref(),
-            &port_id_a,
+            &port_id_a.as_ref(),
         )?;
 
         dbg!(counterparty_channel_id);
 
-        let ica_address = chains
-            .node_a
-            .chain_driver()
-            .query_interchain_account(&wallet_a.address(), &connection.connection_id_a.as_ref())?;
+        let ica_address = chains.node_a.chain_driver().query_interchain_account(
+            &wallet_a.address(),
+            &connection_a.connection_id_a.as_ref(),
+        )?;
 
         dbg!(ica_address);
 
@@ -121,29 +102,14 @@ impl BinaryChainTest for IcaFilterTestDeny {
         relayer: RelayerDriver,
         chains: ConnectedChains<ChainA, ChainB>,
     ) -> Result<(), Error> {
-        let connection =
-            bootstrap_connection(&chains.foreign_clients, default_connection_delay(), false)?;
+        let (_handle, _, _, channel_id_a, port_id_a) =
+            bootstrap_and_register_interchain_account(&relayer, &chains, Duration::from_secs(30))?;
 
-        let wallet_a = chains.node_a.wallets().user1().cloned();
-
-        let _handle = relayer.spawn_supervisor()?;
-
-        chains.node_a.chain_driver().register_interchain_account(
-            &wallet_a.address(),
-            &connection.connection_id_a.as_ref(),
+        let channel_end = query_channel_end(
+            chains.handle_a(),
+            &channel_id_a.as_ref(),
+            &port_id_a.as_ref(),
         )?;
-
-        std::thread::sleep(Duration::from_secs(30));
-
-        let channel_id_a: TaggedChannelId<ChainA, ChainB> =
-            TaggedChannelId::new("channel-0".parse().unwrap());
-
-        let icacontroller =
-            PortId::from_str(&format!("icacontroller-{}", wallet_a.address().value())).unwrap();
-
-        let port_id_a: TaggedPortIdRef<ChainA, ChainB> = DualTagged::new(&icacontroller);
-
-        let channel_end = query_channel_end(chains.handle_a(), &channel_id_a.as_ref(), &port_id_a)?;
 
         assert_eq(
             "channel end should still be in state Init",
@@ -151,4 +117,44 @@ impl BinaryChainTest for IcaFilterTestDeny {
             &State::Init,
         )
     }
+}
+
+#[allow(clippy::type_complexity)]
+fn bootstrap_and_register_interchain_account<ChainA: ChainHandle, ChainB: ChainHandle>(
+    relayer: &RelayerDriver,
+    chains: &ConnectedChains<ChainA, ChainB>,
+    sleep: Duration,
+) -> Result<
+    (
+        SupervisorHandle,
+        MonoTagged<ChainA, Wallet>,
+        ConnectedConnection<ChainA, ChainB>,
+        TaggedChannelId<ChainA, ChainB>,
+        TaggedPortId<ChainA, ChainB>,
+    ),
+    Error,
+> {
+    let connection_a =
+        bootstrap_connection(&chains.foreign_clients, default_connection_delay(), false)?;
+
+    let wallet_a = chains.node_a.wallets().user1().cloned();
+
+    let handle = relayer.spawn_supervisor()?;
+
+    chains
+        .node_a
+        .chain_driver()
+        .register_interchain_account(&wallet_a.address(), &connection_a.connection_id_a.as_ref())?;
+
+    std::thread::sleep(sleep);
+
+    let channel_id_a: TaggedChannelId<ChainA, ChainB> =
+        TaggedChannelId::new("channel-0".parse().unwrap());
+
+    let icacontroller =
+        PortId::from_str(&format!("icacontroller-{}", wallet_a.address().value())).unwrap();
+
+    let port_id_a: TaggedPortId<ChainA, ChainB> = TaggedPortId::new(icacontroller);
+
+    Ok((handle, wallet_a, connection_a, channel_id_a, port_id_a))
 }
