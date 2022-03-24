fa60ef1a45ba4626eaee5584fa6ecd76a4971d97
Shoaib Ahmed <sufialhussaini@gmail.com>
2022-03-25 00:52:54 +0530

    Define ICS20 callback functions

--- modules/src/applications/ics20_fungible_token_transfer/context.rs
+++ modules/src/applications/ics20_fungible_token_transfer/context.rs
@@ -1,11 +1,18 @@
 use super::error::Error as Ics20Error;
 use super::BaseCoin;
+use crate::core::ics04_channel::channel::{Counterparty, Order};
 use crate::core::ics04_channel::context::{ChannelKeeper, ChannelReader};
-use crate::core::ics05_port::capabilities::Capability;
+use crate::core::ics04_channel::error::Error as ChannelError;
+use crate::core::ics04_channel::msgs::acknowledgement::Acknowledgement as GenericAcknowledgement;
+use crate::core::ics04_channel::packet::Packet;
+use crate::core::ics04_channel::Version;
+use crate::core::ics05_port::capabilities::{Capability, ChannelCapability};
 use crate::core::ics05_port::context::{PortKeeper, PortReader};
 use crate::core::ics05_port::error::Error as PortError;
-use crate::core::ics24_host::identifier::{ChannelId, PortId};
+use crate::core::ics24_host::identifier::{ChannelId, ConnectionId, PortId};
+use crate::core::ics26_routing::context::{Acknowledgement, DeferredWriteResult};
 use crate::prelude::*;
+use crate::signer::Signer;
 
 pub trait Ics20Keeper:
     ChannelKeeper
@@ -106,4 +113,90 @@ pub trait Ics20Context:
     + Ics20Reader<AccountId = <Self as Ics20Context>::AccountId>
 {
     type AccountId: Into<String>;
+}
+
+#[allow(clippy::too_many_arguments)]
+pub fn on_chan_open_init(
+    _ctx: &mut impl Ics20Context,
+    _order: Order,
+    _connection_hops: &[ConnectionId],
+    _port_id: &PortId,
+    _channel_id: &ChannelId,
+    _channel_cap: &ChannelCapability,
+    _counterparty: &Counterparty,
+    _version: &Version,
+) -> Result<(), ChannelError> {
+    Ok(())
+}
+
+#[allow(clippy::too_many_arguments)]
+pub fn on_chan_open_try(
+    _ctx: &mut impl Ics20Context,
+    _order: Order,
+    _connection_hops: &[ConnectionId],
+    _port_id: &PortId,
+    _channel_id: &ChannelId,
+    _channel_cap: &ChannelCapability,
+    _counterparty: &Counterparty,
+    counterparty_version: &Version,
+) -> Result<Version, ChannelError> {
+    Ok(counterparty_version.clone())
+}
+
+pub fn on_chan_open_ack(
+    _ctx: &mut impl Ics20Context,
+    _port_id: &PortId,
+    _channel_id: &ChannelId,
+    _counterparty_version: &Version,
+) -> Result<(), ChannelError> {
+    Ok(())
+}
+
+pub fn on_chan_open_confirm(
+    _ctx: &mut impl Ics20Context,
+    _port_id: &PortId,
+    _channel_id: &ChannelId,
+) -> Result<(), ChannelError> {
+    Ok(())
+}
+
+pub fn on_chan_close_init(
+    _ctx: &mut impl Ics20Context,
+    _port_id: &PortId,
+    _channel_id: &ChannelId,
+) -> Result<(), ChannelError> {
+    Ok(())
+}
+
+pub fn on_chan_close_confirm(
+    _ctx: &mut impl Ics20Context,
+    _port_id: &PortId,
+    _channel_id: &ChannelId,
+) -> Result<(), ChannelError> {
+    Ok(())
+}
+
+pub fn on_recv_packet(
+    _ctx: &impl Ics20Context,
+    _packet: &Packet,
+    _relayer: &Signer,
+) -> DeferredWriteResult<dyn Acknowledgement> {
+    (None, None)
+}
+
+pub fn on_acknowledgement_packet(
+    _ctx: &mut impl Ics20Context,
+    _packet: &Packet,
+    _acknowledgement: &GenericAcknowledgement,
+    _relayer: &Signer,
+) -> Result<(), ChannelError> {
+    Ok(())
+}
+
+pub fn on_timeout_packet(
+    _ctx: &mut impl Ics20Context,
+    _packet: &Packet,
+    _relayer: &Signer,
+) -> Result<(), ChannelError> {
+    Ok(())
 }
