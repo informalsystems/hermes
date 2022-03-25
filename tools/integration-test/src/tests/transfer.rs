8145de4a16e851f9fe7d440cc5aaad4e4be07a87
Soares Chen <soares.chen@maybevoid.com>
2022-03-25 11:58:24 +0100

    Reproduce multiple IBC transfer failure

--- tools/integration-test/src/tests/transfer.rs
+++ tools/integration-test/src/tests/transfer.rs
@@ -2,6 +2,8 @@ use ibc_test_framework::ibc::denom::derive_ibc_denom;
 use ibc_test_framework::prelude::*;
 use ibc_test_framework::util::random::random_u64_range;
 
+const TRANSFER_COUNT: u64 = 2;
+
 #[test]
 fn test_ibc_transfer() -> Result<(), Error> {
     run_binary_channel_test(&IbcTransferTest)
@@ -61,24 +63,28 @@ impl BinaryChannelTest for IbcTransferTest {
                 .chain_driver()
                 .query_balance(&wallet_a.address(), &denom_a)?;
 
-            let a_to_b_amount = random_u64_range(1000, 5000);
+            let a_to_b_amount_each = random_u64_range(1000, 5000);
+            let a_to_b_amount_total = a_to_b_amount_each * TRANSFER_COUNT;
 
             info!(
-                "Sending IBC transfer from chain {} to chain {} with amount of {} {}",
+                "Sending IBC transfer from chain {} to chain {} with amount of {} {} x {}",
                 chains.chain_id_a(),
                 chains.chain_id_b(),
-                a_to_b_amount,
-                denom_a
+                a_to_b_amount_each,
+                denom_a,
+                TRANSFER_COUNT
             );
 
-            chains.node_a.chain_driver().transfer_token(
-                &channel.port_a.as_ref(),
-                &channel.channel_id_a.as_ref(),
-                &wallet_a.address(),
-                &wallet_b.address(),
-                a_to_b_amount,
-                &denom_a,
-            )?;
+            for _ in 0..TRANSFER_COUNT {
+                chains.node_a.chain_driver().transfer_token(
+                    &channel.port_a.as_ref(),
+                    &channel.channel_id_a.as_ref(),
+                    &wallet_a.address(),
+                    &wallet_b.address(),
+                    a_to_b_amount_each,
+                    &denom_a,
+                )?;
+            }
 
             let denom_b = derive_ibc_denom(
                 &channel.port_b.as_ref(),
@@ -88,18 +94,18 @@ impl BinaryChannelTest for IbcTransferTest {
 
             info!(
                 "Waiting for user on chain B to receive IBC transferred amount of {} {}",
-                a_to_b_amount, denom_b
+                a_to_b_amount_total, denom_b
             );
 
             chains.node_a.chain_driver().assert_eventual_wallet_amount(
                 &wallet_a.address(),
-                balance_a - a_to_b_amount,
+                balance_a - a_to_b_amount_total,
                 &denom_a,
             )?;
 
             chains.node_b.chain_driver().assert_eventual_wallet_amount(
                 &wallet_b.address(),
-                a_to_b_amount,
+                a_to_b_amount_total,
                 &denom_b.as_ref(),
             )?;
 
@@ -114,7 +120,7 @@ impl BinaryChannelTest for IbcTransferTest {
                 .chain_driver()
                 .query_balance(&wallet_c.address(), &denom_a)?;
 
-            let b_to_a_amount = random_u64_range(500, a_to_b_amount);
+            let b_to_a_amount = random_u64_range(500, a_to_b_amount_total);
 
             info!(
                 "Sending IBC transfer from chain {} to chain {} with amount of {} {}",
@@ -135,7 +141,7 @@ impl BinaryChannelTest for IbcTransferTest {
 
             chains.node_b.chain_driver().assert_eventual_wallet_amount(
                 &wallet_b.address(),
-                a_to_b_amount - b_to_a_amount,
+                a_to_b_amount_total - b_to_a_amount,
                 &denom_b.as_ref(),
             )?;
 
