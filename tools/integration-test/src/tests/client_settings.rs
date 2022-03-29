e4ba70669502aaa157a0d8b1cb21db5855bc0f3b
Mikhail Zabaluev <mikhail@informal.systems>
2022-03-29 14:37:46 +0300

    Draft test for custom client settings

---
+++ tools/integration-test/src/tests/client_settings.rs
@@ -0,0 +1,58 @@
+use std::time::Duration;
+
+use tendermint::trust_threshold::TrustThresholdFraction;
+
+use ibc_relayer::chain::client::ClientSettings;
+use ibc_relayer::chain::cosmos;
+
+use ibc_test_framework::prelude::*;
+use ibc_test_framework::relayer::refresh::spawn_refresh_client_tasks;
+
+/// A test to exercise customization of foreign client settings.
+#[test]
+fn test_client_settings() -> Result<(), Error> {
+    run_binary_chain_test(&ClientSettingsTest)
+}
+
+struct ClientSettingsTest;
+
+struct SettingsTestOverrides;
+
+impl TestOverrides for SettingsTestOverrides {
+    fn client_settings_a_to_b(&self) -> ClientSettings {
+        ClientSettings::Cosmos(cosmos::client::Settings {
+            max_clock_drift: Some(Duration::from_secs(3)),
+            trusting_period: Some(Duration::from_secs(120_000)),
+            trust_threshold: Some(TrustThresholdFraction::new(13, 23).unwrap()),
+        })
+    }
+
+    fn client_settings_b_to_a(&self) -> ClientSettings {
+        ClientSettings::Cosmos(cosmos::client::Settings {
+            max_clock_drift: Some(Duration::from_secs(6)),
+            trusting_period: Some(Duration::from_secs(340_000)),
+            trust_threshold: Some(TrustThresholdFraction::new(17, 29).unwrap()),
+        })
+    }
+}
+
+impl BinaryChainTest for ClientSettingsTest {
+    fn run<ChainA: ChainHandle, ChainB: ChainHandle>(
+        &self,
+        _config: &TestConfig,
+        relayer: RelayerDriver,
+        chains: ConnectedChains<ChainA, ChainB>,
+    ) -> Result<(), Error> {
+        let _refresh_tasks = spawn_refresh_client_tasks(&chains.foreign_clients)?;
+        let _supervisor = relayer.spawn_supervisor()?;
+        todo!("verify that the clients are created with the expected settings")
+    }
+}
+
+impl HasOverrides for ClientSettingsTest {
+    type Overrides = SettingsTestOverrides;
+
+    fn get_overrides(&self) -> &SettingsTestOverrides {
+        &SettingsTestOverrides
+    }
+}
--- tools/integration-test/src/tests/mod.rs
+++ tools/integration-test/src/tests/mod.rs
@@ -7,6 +7,7 @@
 
 pub mod clear_packet;
 pub mod client_expiration;
+mod client_settings;
 pub mod memo;
 pub mod supervisor;
 pub mod ternary_transfer;
