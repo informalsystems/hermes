be7e06f032ffb3f5b1738f0a13cad34205adfaff
Soares Chen <soares.chen@maybevoid.com>
2022-03-29 20:58:21 +0200

    Add connection delay test (#1960)
    * Add connection delay test
    
    * Fix merge error

--- tools/integration-test/src/tests/mod.rs
+++ tools/integration-test/src/tests/mod.rs
@@ -7,6 +7,7 @@
 
 pub mod clear_packet;
 pub mod client_expiration;
+pub mod connection_delay;
 pub mod memo;
 pub mod supervisor;
 pub mod ternary_transfer;
--- tools/test-framework/src/util/assert.rs
+++ tools/test-framework/src/util/assert.rs
@@ -23,3 +23,14 @@ pub fn assert_not_eq<T: Eq + Debug>(message: &str, left: &T, right: &T) -> Resul
         )))
     }
 }
+
+pub fn assert_gt<T: Ord + Debug>(message: &str, left: &T, right: &T) -> Result<(), Error> {
+    if left > right {
+        Ok(())
+    } else {
+        Err(Error::assertion(format!(
+            "expect left ({:?}) to be greater than right ({:?}): {}",
+            left, right, message
+        )))
+    }
+}
