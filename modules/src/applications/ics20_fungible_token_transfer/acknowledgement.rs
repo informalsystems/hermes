1ae5c325a67cfe07c22e53666df651b204f42697
Shoaib Ahmed <sufialhussaini@gmail.com>
2022-04-23 23:18:07 +0200

    Manual deserialize impl for Acknowledgement

--- modules/src/applications/ics20_fungible_token_transfer/acknowledgement.rs
+++ modules/src/applications/ics20_fungible_token_transfer/acknowledgement.rs
@@ -1,16 +1,14 @@
 use super::error::Error;
-use crate::core::ics04_channel::msgs::acknowledgement::Acknowledgement as GenericAcknowledgement;
 use crate::core::ics26_routing::context::Acknowledgement as AckTrait;
 use crate::prelude::*;
 
-use serde::Deserialize;
+use serde::{Deserialize, Deserializer};
 
 /// A string constant included in error acknowledgements.
 /// NOTE: Changing this const is state machine breaking as acknowledgements are written into state
 pub const ACK_ERR_STR: &str = "error handling packet on destination chain: see events for details";
 pub const ACK_SUCCESS_B64: &[u8] = b"AQ==";
 
-#[derive(Deserialize)]
 pub enum Acknowledgement {
     /// Equivalent to b"AQ==" (i.e. `base64::encode(0x01)`)
     Success(Vec<u8>),
@@ -27,22 +25,24 @@ impl Acknowledgement {
     }
 }
 
-impl From<GenericAcknowledgement> for Acknowledgement {
-    fn from(ack: GenericAcknowledgement) -> Self {
-        if ack.as_ref() == ACK_SUCCESS_B64 {
-            Self::Success(ACK_SUCCESS_B64.to_vec())
-        } else {
-            Self::Error(ACK_ERR_STR.to_owned())
-        }
-    }
-}
-
 impl AsRef<[u8]> for Acknowledgement {
     fn as_ref(&self) -> &[u8] {
         match self {
             Acknowledgement::Success(b) => b.as_slice(),
             Acknowledgement::Error(s) => s.as_bytes(),
         }
+    }
+}
+
+impl<'de> Deserialize<'de> for Acknowledgement {
+    fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+        let s = String::deserialize(deserializer)?;
+        let ack = if s.as_bytes() == ACK_SUCCESS_B64 {
+            Self::Success(ACK_SUCCESS_B64.to_vec())
+        } else {
+            Self::Error(s)
+        };
+        Ok(ack)
     }
 }
 
