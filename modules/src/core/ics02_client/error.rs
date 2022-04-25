3ebd6d3ae32bd3c4791fbaad112423680d69b97a
Davirain <davirain.yin@gmail.com>
2022-04-25 15:37:12 +0800

    remve ics07-tendermint client_def.rs unwrap()

--- modules/src/core/ics02_client/error.rs
+++ modules/src/core/ics02_client/error.rs
@@ -252,7 +252,22 @@ define_error! {
         MissingLocalConsensusState
             { height: Height }
             | e | { format_args!("the local consensus state could not be retrieved for height {}", e.height) },
-
+        
+        InvalidConnectionEnd 
+            [ TraceError<TendermintProtoError>]
+            | _ | { "invalid connection end" },
+        
+        InvalidChannelEnd 
+            [ TraceError<TendermintProtoError>]
+            | _ | { "invalid channel end" },
+        
+        InvalidAnyClientState 
+            [ TraceError<TendermintProtoError>]
+            | _ | { "invalid any client state" },
+        
+        InvalidAnyConsensusState
+            [ TraceError<TendermintProtoError> ]
+            | _ | { "invalid any client consensus state" },
     }
 }
 
