fa271d6de28a704bdd160ad48053b510c17f9607
Shoaib Ahmed <sufialhussaini@gmail.com>
2022-04-26 00:24:52 +0200

    Define ICS20 events

--- modules/src/applications/ics20_fungible_token_transfer/events.rs
+++ modules/src/applications/ics20_fungible_token_transfer/events.rs
@@ -1,8 +1,36 @@
+use crate::applications::ics20_fungible_token_transfer::acknowledgement::Acknowledgement;
+use crate::applications::ics20_fungible_token_transfer::address::Address;
+use crate::applications::ics20_fungible_token_transfer::{Amount, DenomTrace, HashedDenom};
+
 #[allow(unused)]
 pub enum Event {
-    Timeout,
-    Packet,
-    Transfer,
-    ChannelClose,
-    DenomTrace,
+    Recv(RecvEvent),
+    Ack(AckEvent),
+    Timeout(TimeoutEvent),
+    DenomTrace(DenomTraceEvent),
+}
+
+pub struct RecvEvent {
+    pub receiver: Address,
+    pub denom: DenomTrace,
+    pub amount: Amount,
+    pub ack_success: bool,
+}
+
+pub struct AckEvent {
+    pub receiver: Address,
+    pub denom: DenomTrace,
+    pub amount: Amount,
+    pub ack: Acknowledgement,
+}
+
+pub struct TimeoutEvent {
+    pub refund_receiver: Address,
+    pub refund_denom: DenomTrace,
+    pub refund_amount: Amount,
+}
+
+pub struct DenomTraceEvent {
+    pub trace_hash: HashedDenom,
+    pub denom: DenomTrace,
 }
