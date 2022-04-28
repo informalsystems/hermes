106d74701125d5d53c689c1c8e3c71ad9e271766
Romain Ruetschi <romain@informal.systems>
2022-04-28 14:46:53 +0200

    Update `bitcoin` to v0.28.0

--- relayer/src/chain/cosmos/encode.rs
+++ relayer/src/chain/cosmos/encode.rs
@@ -70,7 +70,7 @@ pub fn sign_tx(
 fn encode_key_bytes(key: &KeyEntry) -> Result<Vec<u8>, Error> {
     let mut pk_buf = Vec::new();
 
-    prost::Message::encode(&key.public_key.public_key.to_bytes(), &mut pk_buf)
+    prost::Message::encode(&key.public_key.to_pub().to_bytes(), &mut pk_buf)
         .map_err(|e| Error::protobuf_encode("PublicKey".into(), e))?;
 
     Ok(pk_buf)
