81bd1b271d5f66d04e966f52f4f4f383cfdfd4ac
Shoaib Ahmed <sufialhussaini@gmail.com>
2022-04-29 14:08:22 +0200

    Remove bech32 validation from Address

--- modules/src/applications/ics20_fungible_token_transfer/address.rs
+++ modules/src/applications/ics20_fungible_token_transfer/address.rs
@@ -2,12 +2,11 @@ use core::str::FromStr;
 
 use derive_more::Display;
 use serde::{Deserialize, Serialize};
-use subtle_encoding::bech32;
 
 use super::error::Error;
 use crate::prelude::*;
 
-/// A type representing a valid `bech32` address.
+/// A type representing an ICS20 address.
 #[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize, Display)]
 pub struct Address(String);
 
@@ -19,9 +18,6 @@ impl FromStr for Address {
         if s.trim().is_empty() {
             return Err(Error::empty_signer());
         }
-
-        let _ = bech32::decode(s.as_str()).map_err(Error::address_not_valid_bech32)?;
-
         Ok(Self(s))
     }
 }
