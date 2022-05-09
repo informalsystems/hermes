c9b9bcaed8cc4a0cdcd5b06cc4476c8725b64588
Philippe Laferriere <plafer@protonmail.com>
2022-05-09 16:13:37 -0400

    QueryChannelClientStateRequest: remove unused Protobuf

--- relayer/src/chain/requests.rs
+++ relayer/src/chain/requests.rs
@@ -2,27 +2,12 @@ use ibc::core::ics24_host::identifier::{ChannelId, PortId};
 use ibc::Height;
 use ibc_proto::ibc::core::channel::v1::QueryChannelClientStateRequest as RawQueryChannelClientStateRequest;
 
-use crate::error::Error;
 use serde::{Deserialize, Serialize};
-use tendermint_proto::Protobuf;
 
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct QueryChannelClientStateRequest {
     pub port_id: String,
     pub channel_id: String,
-}
-
-impl Protobuf<RawQueryChannelClientStateRequest> for QueryChannelClientStateRequest {}
-
-impl TryFrom<RawQueryChannelClientStateRequest> for QueryChannelClientStateRequest {
-    type Error = Error;
-
-    fn try_from(raw_request: RawQueryChannelClientStateRequest) -> Result<Self, Self::Error> {
-        Ok(QueryChannelClientStateRequest {
-            port_id: raw_request.port_id,
-            channel_id: raw_request.channel_id,
-        })
-    }
 }
 
 impl From<QueryChannelClientStateRequest> for RawQueryChannelClientStateRequest {
