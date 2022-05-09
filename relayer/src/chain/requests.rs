daf8afbdd774b94180874f8464157d90aa1971ff
Philippe Laferriere <plafer@protonmail.com>
2022-05-09 17:05:29 -0400

    PageRequest domain type

--- relayer/src/chain/requests.rs
+++ relayer/src/chain/requests.rs
@@ -1,8 +1,43 @@
 use ibc::core::ics24_host::identifier::{ChannelId, PortId};
 use ibc::Height;
 use ibc_proto::ibc::core::channel::v1::QueryChannelClientStateRequest as RawQueryChannelClientStateRequest;
+use ibc_proto::cosmos::base::query::v1beta1::PageRequest as RawPageRequest;
 
 use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct PageRequest {
+    /// key is a value returned in PageResponse.next_key to begin
+    /// querying the next page most efficiently. Only one of offset or key
+    /// should be set.
+    pub key: ::prost::alloc::vec::Vec<u8>,
+    /// offset is a numeric offset that can be used when key is unavailable.
+    /// It is less efficient than using key. Only one of offset or key should
+    /// be set.
+    pub offset: u64,
+    /// limit is the total number of results to be returned in the result page.
+    /// If left empty it will default to a value to be set by each app.
+    pub limit: u64,
+    /// count_total is set to true  to indicate that the result set should include
+    /// a count of the total number of items available for pagination in UIs.
+    /// count_total is only respected when offset is used. It is ignored when key
+    /// is set.
+    pub count_total: bool,
+    /// reverse is set to true if results are to be returned in the descending order.
+    pub reverse: bool,
+}
+
+impl From<PageRequest> for RawPageRequest {
+    fn from(request: PageRequest) -> Self {
+        RawPageRequest {
+            key: request.key,
+            offset: request.offset,
+            limit: request.limit,
+            count_total: request.count_total,
+            reverse: request.reverse,
+        }
+    }
+}
 
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct QueryChannelClientStateRequest {
