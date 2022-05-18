d013a37c4746c3459917c6e2424b56cdb26af347
Mikhail Zabaluev <mikhail@informal.systems>
2022-05-18 17:48:50 +0300

    [tendermint-rs 0.24] Use the new names for Io API

--- relayer/src/light_client/tendermint.rs
+++ relayer/src/light_client/tendermint.rs
@@ -1,7 +1,10 @@
 use itertools::Itertools;
 
 use tendermint_light_client::{
-    components::{self, io::AtHeight},
+    components::{
+        self,
+        io::{AsyncIo, AtHeight},
+    },
     light_client::LightClient as TmLightClient,
     state::State as LightClientState,
     store::{memory::MemoryStore, LightStore},
@@ -38,7 +41,7 @@ use super::Verified;
 pub struct LightClient {
     chain_id: ChainId,
     peer_id: PeerId,
-    io: components::io::ProdIo,
+    io: components::io::RpcIo,
 }
 
 impl super::LightClient<CosmosSdkChain> for LightClient {
@@ -169,7 +172,7 @@ impl LightClient {
         let rpc_client = rpc::HttpClient::new(config.rpc_addr.clone())
             .map_err(|e| Error::rpc(config.rpc_addr.clone(), e))?;
 
-        let io = components::io::ProdIo::new(peer_id, rpc_client, Some(config.rpc_timeout));
+        let io = components::io::RpcIo::new(peer_id, rpc_client, Some(config.rpc_timeout));
 
         Ok(Self {
             chain_id: config.id.clone(),
@@ -222,8 +225,6 @@ impl LightClient {
     }
 
     fn fetch_light_block(&self, height: AtHeight) -> Result<LightBlock, Error> {
-        use tendermint_light_client::components::io::Io;
-
         self.io
             .fetch_light_block(height)
             .map_err(|e| Error::light_client_io(self.chain_id.to_string(), e))
