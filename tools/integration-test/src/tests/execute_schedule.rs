3ddc4ef0b6063b8b18bb906aeecf7c8276d76cd7
Sean Chen <seanchen11235@gmail.com>
2022-05-20 09:37:23 -0500

    Refactor test so that it is actually testing the desired behavior

--- tools/integration-test/src/tests/execute_schedule.rs
+++ tools/integration-test/src/tests/execute_schedule.rs
@@ -3,16 +3,18 @@
 //! to send. Subsequent pieces of operational data that were scheduled should
 //! be re-queued and not dropped.
 //!
-//! In order to test this behavior, the test manually relays two IBC transfers
-//! between two chains, one transaction going from chain A to chain B, the other
-//! going from chain B to chain A. Chain B is then shut down and the transactions
-//! queued up again. The first of the two transactions should fail because the
-//! chain was shut down. The second transaction should still be queued up.
+//! In order to test this behavior, the test manually relays at least 2 IBC transfers
+//! from chain A to chain B. Chain B is then shut down in order to force the transactions
+//! to be queued up again for re-submission. It is expected that the first transfer
+//! be dropped and not be present in the pending queue, but all of the subsequent
+//! transactions should exist in the pending queue.
 
 use ibc_test_framework::prelude::*;
 use ibc_test_framework::util::random::random_u64_range;
 
 use ibc_relayer::link::{Link, LinkParameters};
+
+const NUM_TXS: i32 = 10;
 
 #[test]
 fn test_execute_schedule() -> Result<(), Error> {
@@ -36,43 +38,10 @@ impl BinaryChannelTest for ExecuteScheduleTest {
         channel: ConnectedChannel<ChainA, ChainB>,
     ) -> Result<(), Error> {
         let amount1 = random_u64_range(1000, 5000);
-        let amount2 = random_u64_range(1000, 5000);
-
-        info!(
-            "Performing first IBC transfer from chain A to chain B with amount {}",
-            amount1
-        );
-
-        chains.node_a.chain_driver().ibc_transfer_token(
-            &channel.port_a.as_ref(),
-            &channel.channel_id_a.as_ref(),
-            &chains.node_a.wallets().user1(),
-            &chains.node_b.wallets().user1().address(),
-            &chains.node_a.denom(),
-            amount1,
-        )?;
-
-        info!(
-            "Performing second IBC transfer from chain B to chain A with amount {}",
-            amount2
-        );
-
-        chains.node_b.chain_driver().ibc_transfer_token(
-            &channel.port_b.as_ref(),
-            &channel.channel_id_b.as_ref(),
-            &chains.node_b.wallets().user1(),
-            &chains.node_a.wallets().user1().address(),
-            &chains.node_b.denom(),
-            amount2,
-        )?;
 
         let chain_a_link_opts = LinkParameters {
             src_port_id: channel.port_a.clone().into_value(),
             src_channel_id: channel.channel_id_a.clone().into_value(),
-        };
-        let chain_b_link_opts = LinkParameters {
-            src_port_id: channel.port_b.clone().into_value(),
-            src_channel_id: channel.channel_id_b.clone().into_value(),
         };
 
         let chain_a_link = Link::new_from_opts(
@@ -81,29 +50,32 @@ impl BinaryChannelTest for ExecuteScheduleTest {
             chain_a_link_opts,
             true,
         )?;
-        let chain_b_link = Link::new_from_opts(
-            chains.handle_b().clone(),
-            chains.handle_a().clone(),
-            chain_b_link_opts,
-            true,
-        )?;
 
         let relay_path_a_to_b = chain_a_link.a_to_b;
-        let relay_path_b_to_a = chain_b_link.a_to_b;
 
-        relay_path_a_to_b.schedule_packet_clearing(None)?;
-        relay_path_b_to_a.schedule_packet_clearing(None)?;
+        for i in 0..NUM_TXS {
+            chains.node_a.chain_driver().ibc_transfer_token(
+                &channel.port_a.as_ref(),
+                &channel.channel_id_a.as_ref(),
+                &chains.node_a.wallets().user1(),
+                &chains.node_b.wallets().user1().address(),
+                &chains.node_a.denom(),
+                amount1,
+            )?;
 
-        assert_eq!(relay_path_a_to_b.dst_operational_data.len(), 1);
-        assert_eq!(relay_path_b_to_a.dst_operational_data.len(), 1);
+            relay_path_a_to_b.schedule_packet_clearing(None)?;
+
+            info!("Performing IBC transfer #{} from chain A to chain B", i,);
+        }
+
+        assert_eq!(relay_path_a_to_b.dst_operational_data.len(), NUM_TXS);
 
         chains.node_b.value().kill()?;
 
         match relay_path_a_to_b.execute_schedule() {
             Ok(_) => panic!("Expected an error when relaying tx from A to B"),
             Err(_e) => {
-                assert_eq!(relay_path_a_to_b.dst_operational_data.len(), 0);
-                assert_eq!(relay_path_b_to_a.dst_operational_data.len(), 1);
+                assert_eq!(relay_path_a_to_b.dst_operational_data.len(), NUM_TXS - 1);
             }
         }
 
