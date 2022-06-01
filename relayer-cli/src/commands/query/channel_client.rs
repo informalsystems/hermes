65a018955bfd75525bd16bfcffd060e6dd373147
Soares Chen <soares.chen@maybevoid.com>
2022-06-01 11:58:08 +0200

    Disable MBT tests on CI due to flakiness (#2253)

--- relayer-cli/src/commands/query/channel_client.rs
+++ relayer-cli/src/commands/query/channel_client.rs
@@ -35,7 +35,7 @@ impl Runnable for QueryChannelClientCmd {
 
         match chain.query_channel_client_state(QueryChannelClientStateRequest {
             port_id: self.port_id.clone(),
-            channel_id: self.channel_id.clone(),
+            channel_id: self.channel_id,
         }) {
             Ok(cs) => Output::success(cs).exit(),
             Err(e) => Output::error(format!("{}", e)).exit(),
