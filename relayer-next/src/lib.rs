7178f8c8068817628b820869bdb577cc688be090
Soares Chen <soares.chen@maybevoid.com>
2022-06-24 22:44:05 +0200

    Static test for running ReceivePacketRelayer working

--- relayer-next/src/lib.rs
+++ relayer-next/src/lib.rs
@@ -2,3 +2,6 @@ pub mod impls;
 pub mod tagged;
 pub mod traits;
 pub mod types;
+
+#[cfg(test)]
+pub mod tests;
---
+++ relayer-next/src/tests/cosmos.rs
@@ -0,0 +1,20 @@
+use ibc_relayer::chain::handle::ChainHandle;
+
+use crate::impls::cosmos::error::Error;
+use crate::impls::cosmos::relay_context::CosmosRelayContext;
+use crate::impls::packet_relayers::receive_packet::ReceivePacketRelayer;
+use crate::traits::packet_relayer::PacketRelayer;
+use ibc::core::ics04_channel::packet::Packet;
+
+pub async fn run_receive_packet_relayer<ChainA, ChainB>(
+    context: &CosmosRelayContext<ChainA, ChainB>,
+    packet: Packet,
+) -> Result<(), Error>
+where
+    ChainA: ChainHandle,
+    ChainB: ChainHandle,
+{
+    ReceivePacketRelayer.relay_packet(context, packet).await?;
+
+    Ok(())
+}
