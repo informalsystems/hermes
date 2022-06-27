86badb1263d1b1e1c4aa689a4436368b34dd2898
Achilleas Kalantzis <83584143+achilleas-kal@users.noreply.github.com>
2022-06-27 17:27:11 +0300

    feat: add flag to skip new_client_connection confirmation (#2316)
    * feat: add flag to skip new_client_connection confirmation
    
    * Add changelog entry
    
    * Formatting changes and improve doc comments
    
    * Shorten `--new-client-connection` -> `--new-client-conn`
    
    Co-authored-by: Sean Chen <skypemaster007@gmail.com>

--- relayer-cli/src/commands/create/channel.rs
+++ relayer-cli/src/commands/create/channel.rs
@@ -29,14 +29,16 @@ static HINT: &str = "Consider using the default invocation\n\nhermes create chan
 ///
 /// There are two possible ways to invoke this command:
 ///
-/// `create channel --port-a <Port-ID> --port-b <Port-ID> <Chain-A-ID> <Connection-ID>` is the default
-/// way in which this command should be used, specifying a `Connection-ID` for this new channel
-/// to re-use. The command expects that `Connection-ID` is associated with chain A.
+/// `create channel --a-port <A_PORT_ID> --b-port <B_PORT_ID> --a-chain <A_CHAIN_ID> --a-conn <A_CONNECTION_ID>`
+/// is the default way in which this command should be used, specifying a `Connection-ID`
+/// associated with chain A for this new channel to re-use.
 ///
-/// `create channel --port-a <Port-ID> --port-b <Port-ID> <Chain-A-ID> <Chain-B-ID> --new-client-connection`
-/// to indicate that a new connection/client pair is being created as part of this new channel.
-/// This brings up an interactive yes/no prompt to ensure that the operator at least
-/// considers the fact that they're initializing a new connection with the channel.
+/// `create channel --a-port <A_PORT_ID> --b-port <B_PORT_ID> --a-chain <A_CHAIN_ID> --b-chain <B_CHAIN_ID> --new-client-conn`
+/// can alternatively be used to indicate that a new connection/client pair is being
+/// created as part of this new channel. This brings up an interactive yes/no prompt
+/// to ensure that the operator at least considers the fact that they're initializing a
+/// new connection with the channel. This prompt can be skipped by appending the `--yes`
+/// flag to the command.
 ///
 /// Note that `Connection-ID`s have to be considered based off of the chain's perspective. Although
 /// chain A and chain B might refer to the connection with different names, they are actually referring
@@ -94,7 +96,10 @@ pub struct CreateChannelCommand {
         long,
         help = "Indicates that a new client and connection will be created underlying the new channel"
     )]
-    new_client_connection: bool,
+    new_client_conn: bool,
+
+    #[clap(long, help = "Skip new_client_conn confirmation")]
+    yes: bool,
 }
 
 impl Runnable for CreateChannelCommand {
@@ -103,40 +108,46 @@ impl Runnable for CreateChannelCommand {
             Some(conn) => self.run_reusing_connection(conn),
             None => match &self.chain_b {
                 Some(chain_b) => {
-                    if self.new_client_connection {
-                        match Confirm::new()
-                            .with_prompt(format!(
-                                "{}: {}\n{}: {}",
-                                style("WARN").yellow(),
-                                PROMPT,
-                                style("Hint").cyan(),
-                                HINT
-                            ))
-                            .interact()
-                        {
-                            Ok(confirm) => {
-                                if confirm {
-                                    self.run_using_new_connection(chain_b);
-                                } else {
-                                    Output::error("You elected not to create new clients and connections. Please re-invoke `create channel` with a pre-existing connection ID".to_string()).exit();
+                    if self.new_client_conn {
+                        if self.yes {
+                            self.run_using_new_connection(chain_b);
+                        } else {
+                            match Confirm::new()
+                                .with_prompt(format!(
+                                    "{}: {}\n{}: {}",
+                                    style("WARN").yellow(),
+                                    PROMPT,
+                                    style("Hint").cyan(),
+                                    HINT
+                                ))
+                                .interact()
+                            {
+                                Ok(confirm) => {
+                                    if confirm {
+                                        self.run_using_new_connection(chain_b);
+                                    } else {
+                                        Output::error("You elected not to create new clients and connections. Please re-invoke `create channel` with a pre-existing connection ID".to_string()).exit();
+                                    }
                                 }
-                            }
-                            Err(e) => {
-                                Output::error(format!(
-                                    "An error occurred while waiting for user input: {}",
-                                    e
-                                ));
+                                Err(e) => {
+                                    Output::error(format!(
+                                        "An error occurred while waiting for user input: {}",
+                                        e
+                                    ));
+                                }
                             }
                         }
                     } else {
                         Output::error(
-                                "The `--new-client-connection` flag is required if invoking with `--chain-b`".to_string()
-                            )
-                            .exit();
+                            "The `--new-client-conn` flag is required if invoking with `--b-chain`"
+                                .to_string(),
+                        )
+                        .exit();
                     }
                 }
-                None => Output::error("Missing one of `<chain-b>` or `<connection-a>`".to_string())
-                    .exit(),
+                None => {
+                    Output::error("Missing one of `--b-chain` or `--a-conn`".to_string()).exit()
+                }
             },
         }
     }
