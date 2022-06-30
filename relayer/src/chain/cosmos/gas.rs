e3d0b108831629e5f7fb541c06396e345af0228c
Romain Ruetschi <romain@informal.systems>
2022-06-30 16:58:32 +0200

    Deprecate `gas_adjustment` in favor of new `gas_multiplier` setting (#2353)
    # Description
    
    This PR does the following: (cf. https://github.com/informalsystems/ibc-rs/issues/2174#issuecomment-1168474595)
    
    1. Let's deprecate `gas_adjustment`. The name of the setting does not properly convey what is used for imho, and its semantics (multiply the estimated gas by the value and add it to the estimation) are confusing.
    2. Instead, replace it with a `gas_multiplier` setting, with the following semantics: take the estimated gas and multiply it by the value of the setting.
    
    Meaning that when we previously had `gas_adjustment = 0.1`, which increases the gas by 10%, we would now have `gas_multiplier = 1.1`.
    
    This PR enforces a lower bound of `1.0` for the new setting but no upper bound so that people are free to use as high gas as they want.
    
    # Commits
    
    * Deprecate `gas_adjustment` in favor of new `gas_multiplier` setting
    
    * Add changelog entry
    
    * Add tests for `adjust_gas`
    
    * Fix `unused_variable` warning
    
    * Improve tests
    
    * Fix typo in test code
    
    * Use 1.1 gas_multiplier in mock tests
    
    * Return zero when `gas_amount` is zero, instead of max_gas
    
    * Short-circuit when gas_amount = 0 or gas_multiplier = 1

--- relayer/src/chain/cosmos/gas.rs
+++ relayer/src/chain/cosmos/gas.rs
@@ -10,8 +10,12 @@ use crate::config::GasPrice;
 
 pub struct PrettyFee<'a>(pub &'a Fee);
 
-pub fn gas_amount_to_fees(config: &GasConfig, gas_amount: u64) -> Fee {
-    let adjusted_gas_limit = adjust_gas_with_simulated_fees(config, gas_amount);
+pub fn gas_amount_to_fee(config: &GasConfig, gas_amount: u64) -> Fee {
+    let adjusted_gas_limit = adjust_estimated_gas(AdjustGas {
+        gas_multiplier: config.gas_multiplier,
+        max_gas: config.max_gas,
+        gas_amount,
+    });
 
     // The fee in coins based on gas amount
     let amount = calculate_fee(adjusted_gas_limit, &config.gas_price);
@@ -42,21 +46,59 @@ pub fn mul_ceil(a: u64, f: f64) -> BigInt {
     (f * a).ceil().to_integer()
 }
 
-/// Adjusts the fee based on the configured `gas_adjustment` to prevent out of gas errors.
+/// Multiply `a` with `f` and round the result down to the nearest integer.
+pub fn mul_floor(a: u64, f: f64) -> BigInt {
+    assert!(f.is_finite());
+
+    let a = BigInt::from(a);
+    let f = BigRational::from_float(f).expect("f is finite");
+    (f * a).floor().to_integer()
+}
+
+struct AdjustGas {
+    gas_multiplier: f64,
+    max_gas: u64,
+    gas_amount: u64,
+}
+
+/// Adjusts the fee based on the configured `gas_multiplier` to prevent out of gas errors.
 /// The actual gas cost, when a transaction is executed, may be slightly higher than the
 /// one returned by the simulation.
-fn adjust_gas_with_simulated_fees(config: &GasConfig, gas_amount: u64) -> u64 {
-    let gas_adjustment = config.gas_adjustment;
+fn adjust_estimated_gas(
+    AdjustGas {
+        gas_multiplier,
+        max_gas,
+        gas_amount,
+    }: AdjustGas,
+) -> u64 {
+    assert!(gas_multiplier >= 1.0);
 
-    assert!(gas_adjustment <= 1.0);
+    // No need to compute anything if the gas amount is zero
+    if gas_amount == 0 {
+        return 0;
+    };
 
-    let (_, digits) = mul_ceil(gas_amount, gas_adjustment).to_u64_digits();
-    assert!(digits.len() == 1);
+    // If the multiplier is 1, no need to perform the multiplication
+    if gas_multiplier == 1.0 {
+        return min(gas_amount, max_gas);
+    }
 
-    let adjustment = digits[0];
-    let gas = gas_amount.checked_add(adjustment).unwrap_or(u64::MAX);
+    // Multiply the gas estimate by the gas_multiplier option
+    let (_sign, digits) = mul_floor(gas_amount, gas_multiplier).to_u64_digits();
 
-    min(gas, config.max_gas)
+    let gas = match digits.as_slice() {
+        // If there are no digits it means that the resulting amount is zero.
+        [] => 0,
+
+        // If there is a single "digit", it means that the result fits in a u64, so we can use that.
+        [gas] => *gas,
+
+        // Otherwise, the multiplication overflow and we use u64::MAX instead.
+        _ => u64::MAX,
+    };
+
+    // Bound the gas estimate by the max_gas option
+    min(gas, max_gas)
 }
 
 impl fmt::Display for PrettyFee<'_> {
@@ -70,5 +112,65 @@ impl fmt::Display for PrettyFee<'_> {
             .field("amount", &amount)
             .field("gas_limit", &self.0.gas_limit)
             .finish()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{adjust_estimated_gas, AdjustGas};
+
+    #[test]
+    fn adjust_zero_gas() {
+        let adjusted_gas = adjust_estimated_gas(AdjustGas {
+            gas_multiplier: 1.1,
+            max_gas: 1_000_000,
+            gas_amount: 0,
+        });
+
+        assert_eq!(adjusted_gas, 0);
+    }
+
+    #[test]
+    fn adjust_gas_one() {
+        let adjusted_gas = adjust_estimated_gas(AdjustGas {
+            gas_multiplier: 1.0,
+            max_gas: 1_000_000,
+            gas_amount: 400_000,
+        });
+
+        assert_eq!(adjusted_gas, 400_000);
+    }
+
+    #[test]
+    fn adjust_gas_small() {
+        let adjusted_gas = adjust_estimated_gas(AdjustGas {
+            gas_multiplier: 1.1,
+            max_gas: 1_000_000,
+            gas_amount: 400_000,
+        });
+
+        assert_eq!(adjusted_gas, 440_000);
+    }
+
+    #[test]
+    fn adjust_gas_over_max() {
+        let adjusted_gas = adjust_estimated_gas(AdjustGas {
+            gas_multiplier: 3.0,
+            max_gas: 1_000_000,
+            gas_amount: 400_000,
+        });
+
+        assert_eq!(adjusted_gas, 1_000_000);
+    }
+
+    #[test]
+    fn adjust_gas_overflow() {
+        let adjusted_gas = adjust_estimated_gas(AdjustGas {
+            gas_multiplier: 3.0,
+            max_gas: u64::MAX,
+            gas_amount: u64::MAX / 2,
+        });
+
+        assert_eq!(adjusted_gas, u64::MAX);
     }
 }
