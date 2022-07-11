0069658c8c027ebe3b33734a25e50bc77e3c379b
hu55a1n1 <sufialhussaini@gmail.com>
2022-07-11 11:38:58 +0200

    Add serde tests for ICS20 Acknowledgement

--- modules/src/applications/transfer/acknowledgement.rs
+++ modules/src/applications/transfer/acknowledgement.rs
@@ -60,3 +60,39 @@ impl Display for Acknowledgement {
 }
 
 impl AckTrait for Acknowledgement {}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_ack_ser() {
+        fn ser_json_assert_eq(ack: Acknowledgement, json_str: &str) {
+            let ser = serde_json::to_string(&ack).unwrap();
+            assert_eq!(ser, json_str)
+        }
+
+        ser_json_assert_eq(Acknowledgement::success(), r#"{"result":"AQ=="}"#);
+        ser_json_assert_eq(
+            Acknowledgement::Error("cannot unmarshal ICS-20 transfer packet data".to_owned()),
+            r#"{"error":"cannot unmarshal ICS-20 transfer packet data"}"#,
+        );
+    }
+
+    #[test]
+    fn test_ack_de() {
+        fn de_json_assert_eq(json_str: &str, ack: Acknowledgement) {
+            let de = serde_json::from_str::<Acknowledgement>(json_str).unwrap();
+            assert_eq!(de, ack)
+        }
+
+        de_json_assert_eq(r#"{"result":"AQ=="}"#, Acknowledgement::success());
+        de_json_assert_eq(
+            r#"{"error":"cannot unmarshal ICS-20 transfer packet data"}"#,
+            Acknowledgement::Error("cannot unmarshal ICS-20 transfer packet data".to_owned()),
+        );
+
+        assert!(serde_json::from_str::<Acknowledgement>(r#"{"result":"AQ="}"#).is_err());
+        assert!(serde_json::from_str::<Acknowledgement>(r#"{"success":"AQ=="}"#).is_err());
+    }
+}
