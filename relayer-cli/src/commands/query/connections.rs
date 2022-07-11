ff7d62cc93cb31925676dd977ed2d4bb2c05dab9
Luca Joss <luca@informal.systems>
2022-07-11 13:07:45 +0200

    Added 'verbose' and 'counterparty-chain' flags to 'query connections' command

--- relayer-cli/src/commands/query/connections.rs
+++ relayer-cli/src/commands/query/connections.rs
@@ -1,9 +1,12 @@
 use abscissa_core::clap::Parser;
 use abscissa_core::Runnable;
 
+use ibc::core::ics02_client::client_state::ClientState;
 use ibc::core::ics24_host::identifier::{ChainId, ConnectionId};
 use ibc_relayer::chain::handle::ChainHandle;
-use ibc_relayer::chain::requests::{PageRequest, QueryConnectionsRequest};
+use ibc_relayer::chain::requests::{
+    IncludeProof, PageRequest, QueryClientStateRequest, QueryConnectionsRequest, QueryHeight,
+};
 
 use crate::cli_utils::spawn_chain_runtime;
 use crate::conclude::{exit_with_unrecoverable_error, Output};
@@ -19,6 +22,19 @@ pub struct QueryConnectionsCmd {
         help = "Identifier of the chain to query"
     )]
     chain_id: ChainId,
+
+    #[clap(
+        long = "counterparty-chain",
+        value_name = "COUNTERPARTY_CHAIN_ID",
+        help = "Filter the query response by the this counterparty chain"
+    )]
+    counterparty_chain_id: Option<ChainId>,
+
+    #[clap(
+        long = "verbose",
+        help = "Enable verbose output, displaying the client for each connection in the response"
+    )]
+    verbose: bool,
 }
 
 // hermes query connections ibc-0
@@ -35,16 +51,51 @@ impl Runnable for QueryConnectionsCmd {
             pagination: Some(PageRequest::all()),
         });
 
-        match res {
+        let connections = match res {
             Ok(connections) => {
-                let ids: Vec<ConnectionId> = connections
-                    .into_iter()
-                    .map(|identified_connection| identified_connection.connection_id)
-                    .collect();
+                // Check the counterparty chain id only if filtering is required.
+                if let Some(counterparty_filter_id) = self.counterparty_chain_id.clone() {
+                    let mut output = connections.clone();
 
-                Output::success(ids).exit()
+                    for (id, connection) in connections.into_iter().enumerate() {
+                        let client_id = connection.end().client_id().to_owned();
+                        let chain_height = chain.query_latest_height();
+                        let (client_state, _) = chain
+                            .query_client_state(
+                                QueryClientStateRequest {
+                                    client_id,
+                                    height: QueryHeight::Specific(chain_height.unwrap()),
+                                },
+                                IncludeProof::No,
+                            )
+                            .unwrap();
+                        let counterparty_chain_id = client_state.chain_id();
+
+                        if counterparty_chain_id != counterparty_filter_id {
+                            output.remove(id);
+                        }
+                    }
+                    output
+                } else {
+                    connections
+                }
             }
-            Err(e) => Output::error(format!("{}", e)).exit(),
+            Err(e) => Output::error(format!(
+                "An error occurred trying to query connections: {}",
+                e
+            ))
+            .exit(),
+        };
+
+        if self.verbose {
+            Output::success(connections).exit()
+        } else {
+            let ids: Vec<ConnectionId> = connections
+                .into_iter()
+                .map(|identified_connection| identified_connection.connection_id)
+                .collect();
+
+            Output::success(ids).exit()
         }
     }
 }
@@ -57,12 +108,44 @@ mod tests {
     use ibc::core::ics24_host::identifier::ChainId;
 
     #[test]
-    fn test_query_connections() {
+    fn test_query_connections_required_only() {
         assert_eq!(
             QueryConnectionsCmd {
-                chain_id: ChainId::from_string("chain_id")
+                chain_id: ChainId::from_string("chain_id"),
+                counterparty_chain_id: None,
+                verbose: false,
             },
             QueryConnectionsCmd::parse_from(&["test", "--chain", "chain_id"])
+        )
+    }
+
+    #[test]
+    fn test_query_connections_counterparty_chain() {
+        assert_eq!(
+            QueryConnectionsCmd {
+                chain_id: ChainId::from_string("chain_id"),
+                counterparty_chain_id: Some(ChainId::from_string("chain_counterparty_id")),
+                verbose: false,
+            },
+            QueryConnectionsCmd::parse_from(&[
+                "test",
+                "--chain",
+                "chain_id",
+                "--counterparty-chain",
+                "chain_counterparty_id"
+            ])
+        )
+    }
+
+    #[test]
+    fn test_query_connections_verbose() {
+        assert_eq!(
+            QueryConnectionsCmd {
+                chain_id: ChainId::from_string("chain_id"),
+                counterparty_chain_id: None,
+                verbose: true,
+            },
+            QueryConnectionsCmd::parse_from(&["test", "--chain", "chain_id", "--verbose"])
         )
     }
 
