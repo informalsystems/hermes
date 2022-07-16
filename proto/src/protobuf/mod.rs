c1387bf5e4e14440fa4fe9a7e056d92dca68019e
hu55a1n1 <sufialhussaini@gmail.com>
2022-07-16 20:37:31 +0200

    Provide blanket impls for ToBoxed and TryFromIfSized

--- proto/src/protobuf/mod.rs
+++ proto/src/protobuf/mod.rs
@@ -12,12 +12,29 @@ pub trait ToBoxed<T: ?Sized> {
     fn to_boxed(&self) -> Box<T>;
 }
 
+impl<T: From<U>, U: Clone> ToBoxed<T> for U {
+    fn to_boxed(&self) -> Box<T> {
+        Box::new(T::from(self.clone().into()))
+    }
+}
+
 pub trait TryFromIfSized<T> {
     type Error;
 
     fn try_from(t: T) -> Result<Self, Self::Error>
     where
         Self: Sized;
+}
+
+impl<T, U: TryFrom<T>> TryFromIfSized<T> for U {
+    type Error = <Self as TryFrom<T>>::Error;
+
+    fn try_from(t: T) -> Result<Self, Self::Error>
+    where
+        Self: Sized,
+    {
+        <Self as TryFrom<T>>::try_from(t)
+    }
 }
 
 pub trait Protobuf<T: Message + Default>
