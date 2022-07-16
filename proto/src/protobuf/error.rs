b3843335b4c06ed7b87e3287d736f7e79647e621
hu55a1n1 <sufialhussaini@gmail.com>
2022-07-16 19:52:33 +0200

    Define protobuf error

---
+++ proto/src/protobuf/error.rs
@@ -0,0 +1,40 @@
+use core::fmt::Display;
+use core::num::TryFromIntError;
+
+use flex_error::{define_error, DisplayOnly};
+use prost::{DecodeError, EncodeError};
+
+use crate::protobuf::TryFromIfSized;
+
+define_error! {
+    Error {
+        TryFromProtobuf
+            { reason: String }
+            | e | {
+                format!("error converting message type into domain type: {}",
+                    e.reason)
+            },
+
+        EncodeMessage
+            [ DisplayOnly<EncodeError> ]
+            | _ | { "error encoding message into buffer" },
+
+        DecodeMessage
+            [ DisplayOnly<DecodeError> ]
+            | _ | { "error decoding buffer into message" },
+
+        ParseLength
+            [ DisplayOnly<TryFromIntError> ]
+            | _ | { "error parsing encoded length" },
+    }
+}
+
+impl Error {
+    pub fn try_from<Raw, T, E>(e: E) -> Error
+    where
+        E: Display,
+        T: TryFromIfSized<Raw, Error = E>,
+    {
+        Error::try_from_protobuf(format!("{}", e))
+    }
+}
