541f7d3c3b9ea39397f0f184397da6a3a9d19950
Sean Chen <seanchen11235@gmail.com>
2022-08-25 13:13:41 -0500

    Document ChainContext traits

--- relayer-framework/src/core/traits/contexts/chain.rs
+++ relayer-framework/src/core/traits/contexts/chain.rs
@@ -1,7 +1,15 @@
+//! The base chain contexts upon which higher level chain
+//! contexts such as [`OfaChain`] are composed from.
+//! 
+//! These traits can be implemented over the default 
+//! `OfaChain` trait if the default behavior is not 
+//! suitable.
+
 use crate::core::traits::contexts::runtime::HasRuntime;
 use crate::core::traits::core::Async;
 use crate::core::traits::message::{IbcMessage, Message};
 
+/// The minimal datatypes that any chain needs to expose.
 pub trait ChainContext: HasRuntime {
     type Height: Async;
 
@@ -12,6 +20,11 @@ pub trait ChainContext: HasRuntime {
     type Event: Async;
 }
 
+/// The datatypes that IBC chains need to expose in addition
+/// to the datatypes exposed by the base [`ChainContext`].
+///
+/// Each [`IbcChainContext`] is parameterized by a [`Counterparty`] 
+/// chain.
 pub trait IbcChainContext<Counterparty>: ChainContext
 where
     Counterparty: ChainContext,
