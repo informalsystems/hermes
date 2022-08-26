a3a06a3fbff4e40dda00d8cbbe02d475d39c149d
Soares Chen <soares.chen@maybevoid.com>
2022-08-26 22:29:43 +0200

    Use AfoCosmsoRelayContext in integration test

--- relayer-cosmos/src/cosmos/instances/packet_relayers.rs
+++ relayer-cosmos/src/cosmos/instances/packet_relayers.rs
@@ -1,17 +1,10 @@
+use crate::cosmos::all_for_one::relay::AfoCosmosRelayContext;
 use ibc_relayer_framework::core::impls::packet_relayers::top::TopRelayer;
 use ibc_relayer_framework::core::traits::packet_relayer::PacketRelayer;
-use ibc_relayer_framework::one_for_all::traits::components::relay::OfaRelayComponents;
-use ibc_relayer_framework::one_for_all::traits::relay::OfaRelayContext;
 
-use crate::cosmos::core::traits::relay::CosmosRelay;
-use crate::cosmos::core::types::relay::CosmosRelayContext;
-
-pub fn full_packet_relayer<Relay>(
-    max_retry: usize,
-) -> impl PacketRelayer<OfaRelayContext<CosmosRelayContext<Relay>>>
+pub fn full_packet_relayer<Relay>(max_retry: usize) -> impl PacketRelayer<Relay>
 where
-    Relay: CosmosRelay,
-    Relay::Components: OfaRelayComponents<CosmosRelayContext<Relay>>,
+    Relay: AfoCosmosRelayContext,
 {
     TopRelayer::new(max_retry)
 }
