fab16853e28161317275bf00ba999dea6b71da87
Sean Chen <seanchen11235@gmail.com>
2022-08-29 12:43:19 -0500

    Change `self` to `relay` in trait definitions

--- relayer-framework/src/core/traits/messages/timeout_packet.rs
+++ relayer-framework/src/core/traits/messages/timeout_packet.rs
@@ -7,7 +7,7 @@ use crate::std_prelude::*;
 #[async_trait]
 pub trait TimeoutUnorderedPacketMessageBuilder<Relay: RelayContext> {
     async fn build_timeout_unordered_packet_message(
-        &self,
+        relay: &Relay,
         height: Height<Relay::DstChain>,
         port_id: PortId<Relay::DstChain, Relay::SrcChain>,
         channel_id: ChannelId<Relay::DstChain, Relay::SrcChain>,
@@ -18,7 +18,7 @@ pub trait TimeoutUnorderedPacketMessageBuilder<Relay: RelayContext> {
 #[async_trait]
 pub trait TimeoutOrderedPacketMessageBuilder<Relay: RelayContext> {
     async fn build_timeout_ordered_packet_message(
-        &self,
+        relay: &Relay,
         height: Height<Relay::DstChain>,
         port_id: PortId<Relay::DstChain, Relay::SrcChain>,
         channel_id: ChannelId<Relay::DstChain, Relay::SrcChain>,
@@ -28,7 +28,7 @@ pub trait TimeoutOrderedPacketMessageBuilder<Relay: RelayContext> {
 #[async_trait]
 pub trait TimeoutChannelClosedMessageBuilder<Relay: RelayContext> {
     async fn build_timeout_channel_closed_message(
-        &self,
+        relay: &Relay,
         height: Height<Relay::DstChain>,
         port_id: PortId<Relay::DstChain, Relay::SrcChain>,
         channel_id: ChannelId<Relay::DstChain, Relay::SrcChain>,
