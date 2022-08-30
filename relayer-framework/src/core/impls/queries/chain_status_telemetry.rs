4d7ff40bd06e4da67c92723f93c80a5634a07010
ali <ali.benabdallah@epfl.ch>
2022-08-30 19:03:47 +0200

    step 2, issue : we need to initialize the metric

---
+++ relayer-framework/src/core/impls/queries/chain_status_telemetry.rs
@@ -0,0 +1,29 @@
+use async_trait::async_trait;
+
+use crate::core::traits::queries::status::*;
+use crate::core::traits::contexts::telemetry::HasTelemetry;
+use crate::core::traits::runtime::telemetry::TelemetryContext;
+
+use crate::std_prelude::*;
+
+pub struct ChainStatusTelemetryQuerier<InQuerier>{
+    pub querier: InQuerier,
+}
+
+#[async_trait]
+impl<InQuerier, Chain, Telemetry> ChainStatusQuerier<Chain> for ChainStatusTelemetryQuerier<InQuerier> 
+where 
+    InQuerier : ChainStatusQuerier<Chain>,
+    Chain: HasTelemetry<Telemetry = Telemetry> + HasChainStatus,
+    Telemetry: TelemetryContext
+{
+    
+    async fn query_chain_status(context: &Chain) -> Result<Chain::ChainStatus, Chain::Error> {
+        let telemetry = context.telemetry();
+        let label = Telemetry::new_label("key", "value");
+        telemetry.add_counter("query", 1, &[label]); // TODO: Error handling
+        let status = InQuerier::query_chain_status(context).await?;
+        Ok(status)
+    }
+}
+
---
+++ relayer-framework/src/core/impls/queries/mod.rs
@@ -0,0 +1,1 @@
+pub mod chain_status_telemetry;
