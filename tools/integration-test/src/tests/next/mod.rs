c6d3891a385cf76925359326e5ed66a76923b9df
Sean Chen <seanchen11235@gmail.com>
2022-09-02 14:47:22 -0500

    Make progress on timeout_transfer test

--- tools/integration-test/src/tests/next/mod.rs
+++ tools/integration-test/src/tests/next/mod.rs
@@ -1,2 +1,3 @@
 pub mod context;
+pub mod timeout_transfer;
 pub mod transfer;
---
+++ tools/integration-test/src/tests/next/timeout_transfer.rs
@@ -0,0 +1,109 @@
+use ibc_relayer_framework::core::impls::packet_relayers::timeout_unordered_packet::BaseTimeoutUnorderedPacketRelayer;
+use ibc_relayer_framework::core::traits::packet_relayers::timeout_unordered_packet::TimeoutUnorderedPacketRelayer;
+use ibc_test_framework::ibc::denom::derive_ibc_denom;
+use ibc_test_framework::prelude::*;
+use ibc_test_framework::util::random::random_u64_range;
+
+use crate::tests::next::context::build_cosmos_relay_context;
+
+#[test]
+fn test_ibc_transfer_next() -> Result<(), Error> {
+    run_binary_channel_test(&IbcTransferTest)
+}
+
+pub struct IbcTransferTest;
+
+impl TestOverrides for IbcTransferTest {
+    fn should_spawn_supervisor(&self) -> bool {
+        false
+    }
+}
+
+impl BinaryChannelTest for IbcTransferTest {
+    fn run<ChainA: ChainHandle, ChainB: ChainHandle>(
+        &self,
+        _config: &TestConfig,
+        _relayer: RelayerDriver,
+        chains: ConnectedChains<ChainA, ChainB>,
+        channel: ConnectedChannel<ChainA, ChainB>,
+    ) -> Result<(), Error> {
+        let relay_context = build_cosmos_relay_context(&chains);
+        let relayer = BaseTimeoutUnorderedPacketRelayer;
+
+        let runtime = chains.node_a.value().chain_driver.runtime.as_ref();
+
+        let denom_a = chains.node_a.denom();
+
+        let wallet_a = chains.node_a.wallets().user1().cloned();
+        let wallet_b = chains.node_b.wallets().user1().cloned();
+
+        let balance_a = chains
+            .node_a
+            .chain_driver()
+            .query_balance(&wallet_a.address(), &denom_a)?;
+
+        let a_to_b_amount = random_u64_range(1000, 5000);
+
+        info!(
+            "Sending IBC timeout from chain {} to chain {} with amount of {} {}",
+            chains.chain_id_a(),
+            chains.chain_id_b(),
+            a_to_b_amount,
+            denom_a
+        );
+
+        let packet = chains.node_a.chain_driver().ibc_transfer_token(
+            &channel.port_a.as_ref(),
+            &channel.channel_id_a.as_ref(),
+            &wallet_a.as_ref(),
+            &wallet_b.address(),
+            &denom_a,
+            a_to_b_amount,
+            Some(Duration::from_secs(3)),
+        )?;
+
+        info!("running relayer");
+
+        sleep(Duration::from_secs(4));
+
+        runtime.block_on(async {
+            relayer
+                .relay_timeout_unordered_packet(&relay_context, &packet)
+                .await
+                .unwrap()
+        });
+
+        info!("finished running relayer");
+
+        let denom_b = derive_ibc_denom(
+            &channel.port_b.as_ref(),
+            &channel.channel_id_b.as_ref(),
+            &denom_a,
+        )?;
+
+        info!(
+            "Waiting for user on chain B to receive IBC transferred amount of {} {}",
+            a_to_b_amount, denom_b
+        );
+
+        chains.node_a.chain_driver().assert_eventual_wallet_amount(
+            &wallet_a.address(),
+            balance_a - a_to_b_amount,
+            &denom_a,
+        )?;
+
+        chains.node_b.chain_driver().assert_eventual_wallet_amount(
+            &wallet_b.address(),
+            a_to_b_amount,
+            &denom_b.as_ref(),
+        )?;
+
+        info!(
+            "successfully performed IBC transfer from chain {} to chain {}",
+            chains.chain_id_a(),
+            chains.chain_id_b(),
+        );
+
+        Ok(())
+    }
+}
