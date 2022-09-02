71136ed68ef00bef20b6bdafa94dc2e720921684
ali <ali.benabdallah@epfl.ch>
2022-09-02 17:18:48 +0200

    more trivial filters

--- relayer-framework/src/core/impls/filters/trivial_filters.rs
+++ relayer-framework/src/core/impls/filters/trivial_filters.rs
@@ -5,6 +5,48 @@ use crate::std_prelude::*;
 use crate::core::traits::contexts::{filter::PacketFilter, relay::RelayContext};
 
 
+
+pub struct AlwaysFalsePacketFilter;
+
+#[async_trait]
+impl<Relay> PacketFilter<Relay> for AlwaysFalsePacketFilter
+where Relay: RelayContext {
+    async fn should_relay_packet(
+        _relay: &Relay,
+        _packet: &Relay::Packet,
+    ) -> Result<bool, Relay::Error> {
+        Ok(false)
+    }
+}
+
+pub struct AlwaysTruePacketFilter;
+
+#[async_trait]
+impl<Relay> PacketFilter<Relay> for AlwaysTruePacketFilter
+where Relay: RelayContext {
+    async fn should_relay_packet(
+        _relay: &Relay,
+        _packet: &Relay::Packet,
+    ) -> Result<bool, Relay::Error> {
+        Ok(true)
+    }
+}
+
+pub struct NegationPacketFilter<Filter>(PhantomData<(Filter)>);
+
+#[async_trait]
+impl<Relay, Filter> PacketFilter<Relay> for NegationPacketFilter<Filter> 
+where Relay: RelayContext, 
+Filter: PacketFilter<Relay>, 
+{
+    async fn should_relay_packet(
+        relay: &Relay,
+        packet: &Relay::Packet,
+    ) -> Result<bool, Relay::Error> {
+        Ok(!Filter::should_relay_packet(relay, packet).await?)
+    }
+}
+
 pub struct AndPacketFilter<FilterA, FilterB>(PhantomData<(FilterA, FilterB)>);
 
 #[async_trait]
