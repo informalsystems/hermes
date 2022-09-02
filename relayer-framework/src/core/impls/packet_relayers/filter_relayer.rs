746cb36ff0745353b9a4db4665836557a3e266d2
Soares Chen <soares.chen@maybevoid.com>
2022-09-02 15:23:10 +0200

    Draft potential ways to unstuck

--- relayer-framework/src/core/impls/packet_relayers/filter_relayer.rs
+++ relayer-framework/src/core/impls/packet_relayers/filter_relayer.rs
@@ -1,24 +1,31 @@
 use async_trait::async_trait;
+use core::marker::PhantomData;
 
-use crate::core::traits::contexts::filter::FilterContext;
+use crate::core::traits::contexts::filter::PacketFilter;
+use crate::core::traits::contexts::relay::RelayContext;
 use crate::core::traits::packet_relayer::PacketRelayer;
 use crate::core::types::aliases::Packet;
 use crate::std_prelude::*;
 
-pub struct FilterRelayer<InRelay> {
+pub struct FilterRelayer<Filter, InRelay> {
     pub in_relayer: InRelay,
+    pub phantom: PhantomData<Filter>,
 }
 
-impl<InRelay> FilterRelayer<InRelay> {
+impl<Filter, InRelay> FilterRelayer<Filter, InRelay> {
     pub fn new(in_relayer: InRelay) -> Self {
-        Self { in_relayer }
+        Self {
+            in_relayer,
+            phantom: PhantomData,
+        }
     }
 }
 
 #[async_trait]
-impl<Context, InRelay> PacketRelayer<Context> for FilterRelayer<InRelay>
+impl<Context, InRelay, Filter> PacketRelayer<Context> for FilterRelayer<Filter, InRelay>
 where
-    Context: FilterContext,
+    Context: RelayContext,
+    Filter: PacketFilter<Context>,
     InRelay: PacketRelayer<Context>,
 {
     async fn relay_packet(
@@ -26,7 +33,7 @@ where
         context: &Context,
         packet: &Packet<Context>,
     ) -> Result<(), Context::Error> {
-        if context.should_relay(packet) {
+        if Filter::should_relay(context, packet) {
             self.in_relayer.relay_packet(context, packet).await?;
         }
         Ok(())
--- relayer-framework/src/core/traits/contexts/filter.rs
+++ relayer-framework/src/core/traits/contexts/filter.rs
@@ -1,5 +1,44 @@
-use crate::core::traits::contexts::relay::RelayContext;
+use async_trait::async_trait;
+use core::marker::PhantomData;
 
-pub trait FilterContext: RelayContext {
-    fn should_relay(&self, packet: &Self::Packet) -> bool;
+use crate::core::traits::contexts::chain::ChainContext;
+use crate::core::traits::contexts::relay::RelayContext;
+use crate::std_prelude::*;
+
+// pub trait FilterContext: RelayContext {
+//     fn should_relay(&self, packet: &Self::Packet) -> bool;
+// }
+
+#[async_trait]
+pub trait PacketFilter<Relay>
+where
+    Relay: RelayContext,
+{
+    async fn should_relay_packet(
+        relay: &Relay,
+        packet: &Relay::Packet,
+    ) -> Result<bool, Relay::Error>;
 }
+
+#[async_trait]
+pub trait HasPacketFilter: RelayContext {
+    type PacketFilter: PacketFilter<Self>;
+
+    async fn should_relay_packet(&self, packet: &Self::Packet) -> Result<bool, Self::Error> {
+        Self::PacketFilter::should_relay_packet(self, packet).await
+    }
+}
+
+#[async_trait]
+pub trait PacketAccountQuerier<Relay: RelayContext> {
+    async fn query_packet_account(
+        relay: &Relay,
+        packet: &Relay::Packet,
+    ) -> Result<<Relay::SrcChain as ChainContext>::Signer, Relay::Error>;
+}
+
+pub struct PacketAccountFilter;
+
+pub struct ChannelFilter;
+
+pub struct AndPacketFilter<FilterA, FilterB>(PhantomData<(FilterA, FilterB)>);
