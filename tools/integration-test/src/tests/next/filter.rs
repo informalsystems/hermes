ee598b47cf8a6197f0ec9b25e3e2104cc59bec2b
ali <ali.benabdallah@epfl.ch>
2022-09-05 14:15:35 +0200

    tmp

--- tools/integration-test/src/tests/next/filter.rs
+++ tools/integration-test/src/tests/next/filter.rs
@@ -45,7 +45,6 @@ impl BinaryChannelTest for ChannelFilterTest {
         let runtime = chains.node_a.value().chain_driver.runtime.as_ref();
 
         let denom_a = chains.node_a.denom();
-        let denom_b = chains.node_b.denom();
 
         let wallet_a = chains.node_a.wallets().user1().cloned();
         let wallet_b = chains.node_b.wallets().user1().cloned();
@@ -54,11 +53,6 @@ impl BinaryChannelTest for ChannelFilterTest {
             .node_a
             .chain_driver()
             .query_balance(&wallet_a.address(), &denom_a)?;
-
-        let balance_b = chains
-            .node_b
-            .chain_driver()
-            .query_balance(&wallet_b.address(), &denom_b)?;
 
         let a_to_b_amount = random_u64_range(1000, 5000);
 
@@ -93,9 +87,9 @@ impl BinaryChannelTest for ChannelFilterTest {
 
         info!(
             "User on chain B should not receive the transfer of {} {}",
-            a_to_b_amount, denom_b
+            a_to_b_amount, &denom_b
         );
-
+            
         chains.node_a.chain_driver().assert_eventual_wallet_amount(
             &wallet_a.address(),
             balance_a - a_to_b_amount,
@@ -104,7 +98,7 @@ impl BinaryChannelTest for ChannelFilterTest {
 
         chains.node_b.chain_driver().assert_eventual_wallet_amount(
             &wallet_b.address(),
-            balance_b,
+            0,
             &denom_b.as_ref(),
         )?;
 
