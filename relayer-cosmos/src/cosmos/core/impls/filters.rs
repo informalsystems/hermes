1d12e26573052a1d04717aa209d63ac15b030890
ali <ali.benabdallah@epfl.ch>
2022-09-05 17:29:13 +0200

    PacketFilter<OfaRelayContext...>

--- relayer-cosmos/src/cosmos/core/impls/filters.rs
+++ relayer-cosmos/src/cosmos/core/impls/filters.rs
@@ -1,9 +1,12 @@
 use async_trait::async_trait;
-use ibc::core::ics24_host::identifier::{ChannelId, PortId};
 use ibc_relayer::config::filter::PacketFilter as CosmosFilter;
-use ibc_relayer_framework::core::traits::contexts::chain::IbcChainContext;
+use ibc_relayer_framework::core::traits::contexts::error::HasError;
 use ibc_relayer_framework::core::traits::contexts::filter::PacketFilter;
 use ibc_relayer_framework::core::traits::contexts::relay::RelayContext;
+use ibc_relayer_framework::one_for_all::traits::relay::OfaRelayContext;
+
+use crate::cosmos::core::traits::relay::CosmosRelay;
+use crate::cosmos::core::types::relay::CosmosRelayContext;
 
 pub struct CosmosChannelFilter {
     pub filter: CosmosFilter,
@@ -20,20 +23,18 @@ impl CosmosChannelFilter {
 }
 
 #[async_trait]
-impl<Relay> PacketFilter<Relay> for CosmosChannelFilter
-where Relay: RelayContext,
-<<Relay as RelayContext>::SrcChain as IbcChainContext<<Relay as RelayContext>::DstChain>>::ChannelId: Into<ChannelId>,
-<<Relay as RelayContext>::SrcChain as IbcChainContext<<Relay as RelayContext>::DstChain>>::ChannelId: Clone,
-<<Relay as RelayContext>::SrcChain as IbcChainContext<<Relay as RelayContext>::DstChain>>::PortId: Into<PortId>,
-<<Relay as RelayContext>::SrcChain as IbcChainContext<<Relay as RelayContext>::DstChain>>::PortId: Clone,
-
+impl<Relay> PacketFilter<OfaRelayContext<CosmosRelayContext<Relay>>> for CosmosChannelFilter
+where
+    Relay: CosmosRelay,
 {
     async fn should_relay_packet(
         &self,
-        packet: &Relay::Packet,
-    ) -> Result<bool, Relay::Error> {
-        let src_channel = <Relay as RelayContext>::packet_src_channel_id(packet).clone();
-        let src_port = <Relay as RelayContext>::packet_src_port(packet).clone();
-        Ok(self.filter().is_allowed(&src_port.into(), &src_channel.into()))
+        packet: &<OfaRelayContext<CosmosRelayContext<Relay>> as RelayContext>::Packet,
+    ) -> Result<bool, <OfaRelayContext<CosmosRelayContext<Relay>> as HasError>::Error> {
+        let src_channel =
+            <OfaRelayContext<CosmosRelayContext<Relay>>>::packet_src_channel_id(packet).clone();
+        let src_port =
+            <OfaRelayContext<CosmosRelayContext<Relay>>>::packet_src_port(packet).clone();
+        Ok(self.filter().is_allowed(&src_port, &src_channel))
     }
 }
