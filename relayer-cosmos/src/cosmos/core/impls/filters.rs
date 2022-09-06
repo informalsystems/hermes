c0053bc8cf7d77e9b2271380eed4e98aa7bac41e
ali <ali.benabdallah@epfl.ch>
2022-09-06 12:01:30 +0200

    comes back to 1d12e26 and renames filters

--- relayer-cosmos/src/cosmos/core/impls/filters.rs
+++ relayer-cosmos/src/cosmos/core/impls/filters.rs
@@ -1,5 +1,5 @@
 use async_trait::async_trait;
-use ibc_relayer::config::filter::PacketFilter as CosmosFilter;
+use ibc_relayer::config::filter::PacketFilter as IbcChannelFilter;
 use ibc_relayer_framework::core::traits::contexts::error::HasError;
 use ibc_relayer_framework::core::traits::contexts::filter::PacketFilter;
 use ibc_relayer_framework::core::traits::contexts::relay::RelayContext;
@@ -9,15 +9,15 @@ use crate::cosmos::core::traits::relay::CosmosRelay;
 use crate::cosmos::core::types::relay::CosmosRelayContext;
 
 pub struct CosmosChannelFilter {
-    pub filter: CosmosFilter,
+    pub filter: IbcChannelFilter,
 }
 
 impl CosmosChannelFilter {
-    pub fn new(filter: CosmosFilter) -> Self {
+    pub fn new(filter: IbcChannelFilter) -> Self {
         Self { filter }
     }
 
-    pub fn filter(&self) -> &CosmosFilter {
+    pub fn channel_filter(&self) -> &IbcChannelFilter {
         &self.filter
     }
 }
@@ -35,6 +35,6 @@ where
             <OfaRelayContext<CosmosRelayContext<Relay>>>::packet_src_channel_id(packet).clone();
         let src_port =
             <OfaRelayContext<CosmosRelayContext<Relay>>>::packet_src_port(packet).clone();
-        Ok(self.filter().is_allowed(&src_port, &src_channel))
+        Ok(self.channel_filter().is_allowed(&src_port, &src_channel))
     }
 }
