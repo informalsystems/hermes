24a29a7d131be6a921df3daaec288e1a96792c28
ali <ali.benabdallah@epfl.ch>
2022-09-15 17:56:57 +0200

    break up script into smaller functions + add comments

--- tools/check-guide/src/main.rs
+++ tools/check-guide/src/main.rs
@@ -1,3 +1,8 @@
+//! This crate verifies the correctness of every Hermes command in the guide by:
+//! 1. Extracting every line in the guide with '{{#template *templates/commands/hermes*}}', a macro call for mdbook template.
+//! 2. Replace every template call with the content of the template. It will replace macro call by what should be an Hermes command.
+//! 3. Check that an `EntryPoint` can be created from the command.
+
 use clap::Parser;
 use ibc_relayer_cli::entry::EntryPoint;
 use lazy_static::lazy_static;
@@ -12,11 +17,35 @@ use std::{
 };
 use walkdir::WalkDir;
 
+#[derive(Debug)]
+enum ParseError {
+    PathDoesNotExist(std::io::Error),
+    IncorrectHermesCommand(clap::Error),
+}
+
+impl From<clap::Error> for ParseError {
+    fn from(e: clap::Error) -> Self {
+        ParseError::IncorrectHermesCommand(e)
+    }
+}
+impl From<std::io::Error> for ParseError {
+    fn from(e: std::io::Error) -> Self {
+        ParseError::PathDoesNotExist(e)
+    }
+}
+
+// Constants
 lazy_static! {
+    // Path to the guide.
     static ref GUIDE_PATH: PathBuf = PathBuf::from("guide/src/");
+
+    // Path to the templates folder where hermes commands templates are defined.
     static ref TEMPLATES_PATH: PathBuf = PathBuf::from("guide/src/templates/commands/hermes/");
 
-    // List of directories which should not be visited
+    // Map of template path to the content of the template.
+    static ref TEMPLATE_MAP: HashMap<PathBuf, String> = build_templates_map();
+
+    // List of directories which should not be visited when checking for correctness.
     static ref EXCLUSIONS: HashSet<&'static str> = HashSet::from(["templates", "assets", "images", "theme"]);
 
     // Regex to match macro calls in the guide
@@ -44,7 +73,8 @@ fn build_templates_map() -> HashMap<PathBuf, String> {
 }
 
 fn build_map_from_macro(line: &str) -> HashMap<String, String> {
-    // Builds a map of {argument => value}
+    // Builds a map of {argument => value} from a macro call.
+    // i.e. `{{#template ../templates/commands/hermes/tx/raw.md arg1="value1" arg2="value2"}}` will return {arg1 => value1, arg2 => value2}
     KEY_VALUE
         .find_iter(line)
         .map(|m| {
@@ -55,7 +85,7 @@ fn build_map_from_macro(line: &str) -> HashMap<String, String> {
 }
 
 fn replace_args(line: &str, args: &HashMap<String, String>) -> String {
-    // Expects the content of a template file and replaces the arguments with the values provided in the HashMap (i.e):
+    // Replace the placeholders in the template's content with the values from the macro call.
     // replace_args(`hermes command [[#argument]]`, {"argument" => "value"})
     // returns `hermes command value`
     let mut line = line.to_string();
@@ -69,74 +99,78 @@ fn replace_args(line: &str, args: &HashMap<String, String>) -> String {
     line
 }
 
-fn verify_file(path: &Path, template_map: &HashMap<PathBuf, String>) -> i32 {
+fn get_template_path(line: &str) -> Result<Option<PathBuf>, ParseError> {
+    // Returns the template path if the line contains a macro call.
+    // get_template_path("... {{#template my_template}} ...") -> Some(PathBuf("/home/user/ibc-rs/guide/src/templates/commands/hermes/my_template.md"))
+    if let Some(captures) = TEMPLATE_RE.captures(line) {
+        let template_name = captures
+            .name("template_name")
+            .expect("Impossible to get template name");
+        let mut template_path = TEMPLATES_PATH.clone();
+        template_path.push(template_name.as_str());
+        // Transform relative path into absolute path.
+        return Ok(Some(canonicalize(template_path)?));
+    }
+    Ok(None)
+}
+
+fn check_correctness<'a, T>(command: T) -> Result<(), ParseError>
+where
+    T: IntoIterator<Item = &'a str>,
+{
+    EntryPoint::try_parse_from(command)?;
+    Ok(())
+}
+
+fn verify_line(line: &str) -> Result<(), ParseError> {
+    // Verify that the line contains a macro call and that the command is correct.
+    if let Some(template_path) = get_template_path(line)? {
+        // Extract the absolute path from the macro call
+        let template_content = TEMPLATE_MAP.get(template_path.as_path()).unwrap();
+        let map = build_map_from_macro(&line);
+        let template_replaced = replace_args(template_content, &map);
+        check_correctness(template_replaced.split_whitespace())?;
+    }
+    Ok(())
+}
+
+fn verify_file(path: &Path) -> i32 {
     // Verifies that every template macro call in the file can be replaced by a valid Hermes command.
     // Returns the number of invalid commands found.
 
-    let mut output = 0;
-    // Check that all templates are used in the guide can be replaced by a correct command
+    let mut error_founds = 0;
     let file = File::open(path);
     let reader =
         BufReader::new(file.unwrap_or_else(|_| panic!("File not found: {}", path.display())));
     let mut line_number = 1;
 
     for line in reader.lines() {
-        let line = line.expect("Failed to read line");
-        if let Some(captures) = TEMPLATE_RE.captures(line.as_str()) {
-            // Retrieve the template reference
-            let template_name = captures
-                .name("template_name")
-                .expect("Impossible to get template name");
-            let mut template_path = TEMPLATES_PATH.clone();
-            template_path.push(template_name.as_str());
-
-            if let Ok(template_path) = canonicalize(&template_path) {
-                let template_content = template_map.get(template_path.as_path()).unwrap();
-                let map = build_map_from_macro(&line);
-                let template_replaced = replace_args(template_content, &map);
-
-                if EntryPoint::try_parse_from(template_replaced.split_whitespace()).is_err() {
-                    eprintln!(
-                        "{} : Line {} - Failed to parse command {}",
-                        path.display(),
-                        line_number,
-                        template_replaced.as_str()
-                    );
-                    output = 1;
-                }
-            } else {
-                eprintln!(
-                    "{} : Line {} - path does not exist: {}",
-                    path.display(),
-                    line_number,
-                    template_path.display()
-                );
-                output = 1;
-            }
+        let line = line.unwrap_or_else( |_| panic!("{} : Failed to read line {}", path.display(), line_number));
+        if let Err(e) = verify_line(&line) {
+            eprintln!("{}:{}: {:?}", path.display(), line_number, e);
+            error_founds += 1;
         }
         line_number += 1;
     }
-    output
+    error_founds
 }
 
 fn main() {
-    let templates = build_templates_map();
-
     // Iterate over every markdown file in the guide directory except for the excluded ones
     let number_of_errors = WalkDir::new(GUIDE_PATH.as_path())
-        .into_iter()
+        .into_iter() // Iterate over all files in the guide directory
         .filter_entry(|e| {
             !EXCLUSIONS.contains(
                 e.file_name()
                     .to_str()
                     .expect("Unwrapping a file_name to str failed."),
             )
-        })
+        }) // Filter out the excluded directories
         .map(|e| e.expect("Failed to get an entry."))
-        .filter(|e| e.file_type().is_file())
-        .filter(|e| e.path().extension() == Some(OsStr::new("md")))
-        .map(|e| verify_file(e.path(), &templates))
-        .sum::<i32>();
+        .filter(|e| e.file_type().is_file()) // Keep only files
+        .filter(|e| e.path().extension() == Some(OsStr::new("md"))) // Keep only markdown files
+        .map(|e| verify_file(e.path())) // Verify that all command templates can be parsed to a Hermes command and return the number of errors
+        .sum::<i32>(); // Sum the number of errors
 
     exit(number_of_errors);
 }
