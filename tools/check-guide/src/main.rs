d6af3ed7ba16feb33cc68503893f2e5ffbe4d3cf
AlianBenabdallah <92268577+AlianBenabdallah@users.noreply.github.com>
2022-09-21 10:27:06 +0200

    Update tools/check-guide/src/main.rs
    Co-authored-by: Sean Chen <seanchen11235@gmail.com>

--- tools/check-guide/src/main.rs
+++ tools/check-guide/src/main.rs
@@ -73,9 +73,9 @@ fn verify_line(line: &str, path: &Path, line_number: i32) -> i32 {
     errors_found
 }
 
+/// Verifies that every template macro call in the file can be replaced by a valid Hermes command.
+/// Returns the number of invalid commands found.
 fn verify_file(path: &Path) -> i32 {
-    // Verifies that every template macro call in the file can be replaced by a valid Hermes command.
-    // Returns the number of invalid commands found.
     let mut errors_found = 0;
     let file = File::open(path);
     let reader =
