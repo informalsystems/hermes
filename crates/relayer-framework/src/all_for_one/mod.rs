c1acfd01ff9de00e1528e05ec059fc6cb795f278
Sean Chen <seanchen11235@gmail.com>
2022-10-05 15:07:41 -0500

    Document traits and function

--- crates/relayer-framework/src/all_for_one/mod.rs
+++ crates/relayer-framework/src/all_for_one/mod.rs
@@ -1,1 +1,4 @@
+//! A trait that encapsulates the functionality that applications gain access
+//! to once they satisfy the [`one_for_all`] trait.
+
 pub mod traits;
--- crates/relayer-framework/src/core/mod.rs
+++ crates/relayer-framework/src/core/mod.rs
@@ -1,3 +1,5 @@
+//! The core traits, types, and impls of the relayer framework.
+
 pub mod impls;
 pub mod traits;
 pub mod types;
