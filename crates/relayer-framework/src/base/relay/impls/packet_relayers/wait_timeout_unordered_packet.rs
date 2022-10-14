e3e113a393dfc035a0e0b72b173e4636a58d12d1
Sean Chen <seanchen11235@gmail.com>
2022-10-14 14:20:35 -0500

    Remove `Target` and `TargetChain` generic params

--- crates/relayer-framework/src/base/relay/impls/packet_relayers/wait_timeout_unordered_packet.rs
+++ crates/relayer-framework/src/base/relay/impls/packet_relayers/wait_timeout_unordered_packet.rs
@@ -4,11 +4,9 @@ use core::time::Duration;
 
 use async_trait::async_trait;
 
-use crate::base::chain::traits::context::HasChainTypes;
 use crate::base::chain::types::aliases::Message;
-use crate::base::relay::traits::target::ChainTarget;
 use crate::base::{
-    chain::traits::{context::HasIbcChainTypes, queries::status::HasChainStatusQuerier},
+    chain::traits::queries::status::HasChainStatusQuerier,
     core::traits::{runtime::HasRuntime, runtimes::sleep::CanSleep, sync::Async},
     relay::traits::{
         context::HasRelayTypes, messages::timeout_packet::TimeoutUnorderedPacketMessageBuilder,
@@ -19,24 +17,18 @@ use crate::std_prelude::*;
 /// An unordered packet message builder variant that waits for the counterparty
 /// chain's height to exceed the source chain's height so that the timeout
 /// packet proof can be constructed before broadcasting the timeout packet.
-pub struct WaitTimeoutUnorderedPacketMessageBuilder<InMessageBuilder, Target>(
+pub struct WaitTimeoutUnorderedPacketMessageBuilder<InMessageBuilder>(
     PhantomData<InMessageBuilder>,
-    PhantomData<Target>,
 );
 
 #[async_trait]
-impl<Relay, Target, TargetChain, InMessageBuilder, CounterpartyChain, Runtime, Height, Error>
-    TimeoutUnorderedPacketMessageBuilder<Relay>
-    for WaitTimeoutUnorderedPacketMessageBuilder<InMessageBuilder, Target>
+impl<Relay, InMessageBuilder, Runtime, Height, Error> TimeoutUnorderedPacketMessageBuilder<Relay>
+    for WaitTimeoutUnorderedPacketMessageBuilder<InMessageBuilder>
 where
     Relay: HasRelayTypes<Error = Error>,
     Relay: HasRuntime<Runtime = Runtime>,
-    Relay::DstChain: HasChainTypes<Height = Height>,
-    Target: ChainTarget<Relay, TargetChain = TargetChain, CounterpartyChain = CounterpartyChain>,
-    TargetChain: HasIbcChainTypes<CounterpartyChain>,
+    Relay::DstChain: HasChainStatusQuerier<Height = Height>,
     InMessageBuilder: TimeoutUnorderedPacketMessageBuilder<Relay>,
-    CounterpartyChain: HasIbcChainTypes<TargetChain, Height = Height, Error = Error>,
-    CounterpartyChain: HasChainStatusQuerier,
     Runtime: CanSleep,
     Height: Ord + Async,
 {
@@ -45,11 +37,11 @@ where
         destination_height: &Height,
         packet: &Relay::Packet,
     ) -> Result<Message<Relay::SrcChain>, Relay::Error> {
-        let chain = Target::counterparty_chain(context);
+        let chain = context.destination_chain();
 
         loop {
             let counterparty_status = chain.query_chain_status().await?;
-            let counterparty_height = CounterpartyChain::chain_status_height(&counterparty_status);
+            let counterparty_height = Relay::DstChain::chain_status_height(&counterparty_status);
 
             if counterparty_height > destination_height {
                 return InMessageBuilder::build_timeout_unordered_packet_message(
