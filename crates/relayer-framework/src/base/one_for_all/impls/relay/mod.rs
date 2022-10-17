fd80f59c5e640a2e02682a9950793f83a40857f8
Soares Chen <soares.chen@maybevoid.com>
2022-10-17 11:26:37 +0200

    Reorganize OFA relay impls

--- crates/relayer-framework/src/base/one_for_all/impls/relay/mod.rs
+++ crates/relayer-framework/src/base/one_for_all/impls/relay/mod.rs
@@ -1,12 +1,9 @@
+pub mod types;
+
 use async_trait::async_trait;
 
-use crate::base::chain::types::aliases::{ChannelId, Height, PortId, Sequence, Timestamp};
-use crate::base::core::traits::error::HasError;
-use crate::base::core::traits::runtime::HasRuntime;
 use crate::base::one_for_all::traits::chain::{OfaBaseChain, OfaBaseChainTypes};
 use crate::base::one_for_all::traits::relay::OfaBaseRelay;
-use crate::base::one_for_all::traits::runtime::OfaRuntimeContext;
-use crate::base::one_for_all::types::chain::OfaChainWrapper;
 use crate::base::one_for_all::types::relay::OfaRelayWrapper;
 use crate::base::relay::impls::packet_relayers::general::retry::SupportsPacketRetry;
 use crate::base::relay::traits::ibc_message_sender::InjectMismatchIbcEventsCountError;
@@ -21,72 +18,7 @@ use crate::base::relay::traits::messages::timeout_packet::{
 };
 use crate::base::relay::traits::messages::update_client::UpdateClientMessageBuilder;
 use crate::base::relay::traits::target::{DestinationTarget, SourceTarget};
-use crate::base::relay::traits::types::HasRelayTypes;
 use crate::std_prelude::*;
-
-impl<Relay: OfaBaseRelay> HasError for OfaRelayWrapper<Relay> {
-    type Error = Relay::Error;
-}
-
-impl<Relay: OfaBaseRelay> HasRuntime for OfaRelayWrapper<Relay> {
-    type Runtime = OfaRuntimeContext<Relay::Runtime>;
-
-    fn runtime(&self) -> &Self::Runtime {
-        self.relay.runtime()
-    }
-}
-
-impl<Relay: OfaBaseRelay> HasRelayTypes for OfaRelayWrapper<Relay> {
-    type SrcChain = OfaChainWrapper<Relay::SrcChain>;
-
-    type DstChain = OfaChainWrapper<Relay::DstChain>;
-
-    type Packet = Relay::Packet;
-
-    fn packet_src_port(packet: &Self::Packet) -> &PortId<Self::SrcChain, Self::DstChain> {
-        Relay::packet_src_port(packet)
-    }
-
-    fn packet_src_channel_id(packet: &Self::Packet) -> &ChannelId<Self::SrcChain, Self::DstChain> {
-        Relay::packet_src_channel_id(packet)
-    }
-
-    fn packet_dst_port(packet: &Self::Packet) -> &PortId<Self::DstChain, Self::SrcChain> {
-        Relay::packet_dst_port(packet)
-    }
-
-    fn packet_dst_channel_id(packet: &Self::Packet) -> &ChannelId<Self::DstChain, Self::SrcChain> {
-        Relay::packet_dst_channel_id(packet)
-    }
-
-    fn packet_sequence(packet: &Self::Packet) -> &Sequence<Self::SrcChain, Self::DstChain> {
-        Relay::packet_sequence(packet)
-    }
-
-    fn packet_timeout_height(packet: &Self::Packet) -> Option<&Height<Self::DstChain>> {
-        Relay::packet_timeout_height(packet)
-    }
-
-    fn packet_timeout_timestamp(packet: &Self::Packet) -> &Timestamp<Self::DstChain> {
-        Relay::packet_timeout_timestamp(packet)
-    }
-
-    fn source_chain(&self) -> &Self::SrcChain {
-        self.relay.src_chain()
-    }
-
-    fn destination_chain(&self) -> &Self::DstChain {
-        self.relay.dst_chain()
-    }
-
-    fn source_client_id(&self) -> &<Relay::SrcChain as OfaBaseChainTypes>::ClientId {
-        self.relay.src_client_id()
-    }
-
-    fn destination_client_id(&self) -> &<Relay::DstChain as OfaBaseChainTypes>::ClientId {
-        self.relay.dst_client_id()
-    }
-}
 
 pub struct OfaUpdateClientMessageBuilder;
 
---
+++ crates/relayer-framework/src/base/one_for_all/impls/relay/types.rs
@@ -0,0 +1,74 @@
+use crate::base::chain::types::aliases::{ChannelId, Height, PortId, Sequence, Timestamp};
+use crate::base::core::traits::error::HasError;
+use crate::base::core::traits::runtime::HasRuntime;
+use crate::base::one_for_all::traits::chain::OfaBaseChainTypes;
+use crate::base::one_for_all::traits::relay::OfaBaseRelay;
+use crate::base::one_for_all::traits::runtime::OfaRuntimeContext;
+use crate::base::one_for_all::types::chain::OfaChainWrapper;
+use crate::base::one_for_all::types::relay::OfaRelayWrapper;
+use crate::base::relay::traits::types::HasRelayTypes;
+use crate::std_prelude::*;
+
+impl<Relay: OfaBaseRelay> HasError for OfaRelayWrapper<Relay> {
+    type Error = Relay::Error;
+}
+
+impl<Relay: OfaBaseRelay> HasRuntime for OfaRelayWrapper<Relay> {
+    type Runtime = OfaRuntimeContext<Relay::Runtime>;
+
+    fn runtime(&self) -> &Self::Runtime {
+        self.relay.runtime()
+    }
+}
+
+impl<Relay: OfaBaseRelay> HasRelayTypes for OfaRelayWrapper<Relay> {
+    type SrcChain = OfaChainWrapper<Relay::SrcChain>;
+
+    type DstChain = OfaChainWrapper<Relay::DstChain>;
+
+    type Packet = Relay::Packet;
+
+    fn packet_src_port(packet: &Self::Packet) -> &PortId<Self::SrcChain, Self::DstChain> {
+        Relay::packet_src_port(packet)
+    }
+
+    fn packet_src_channel_id(packet: &Self::Packet) -> &ChannelId<Self::SrcChain, Self::DstChain> {
+        Relay::packet_src_channel_id(packet)
+    }
+
+    fn packet_dst_port(packet: &Self::Packet) -> &PortId<Self::DstChain, Self::SrcChain> {
+        Relay::packet_dst_port(packet)
+    }
+
+    fn packet_dst_channel_id(packet: &Self::Packet) -> &ChannelId<Self::DstChain, Self::SrcChain> {
+        Relay::packet_dst_channel_id(packet)
+    }
+
+    fn packet_sequence(packet: &Self::Packet) -> &Sequence<Self::SrcChain, Self::DstChain> {
+        Relay::packet_sequence(packet)
+    }
+
+    fn packet_timeout_height(packet: &Self::Packet) -> Option<&Height<Self::DstChain>> {
+        Relay::packet_timeout_height(packet)
+    }
+
+    fn packet_timeout_timestamp(packet: &Self::Packet) -> &Timestamp<Self::DstChain> {
+        Relay::packet_timeout_timestamp(packet)
+    }
+
+    fn source_chain(&self) -> &Self::SrcChain {
+        self.relay.src_chain()
+    }
+
+    fn destination_chain(&self) -> &Self::DstChain {
+        self.relay.dst_chain()
+    }
+
+    fn source_client_id(&self) -> &<Relay::SrcChain as OfaBaseChainTypes>::ClientId {
+        self.relay.src_client_id()
+    }
+
+    fn destination_client_id(&self) -> &<Relay::DstChain as OfaBaseChainTypes>::ClientId {
+        self.relay.dst_client_id()
+    }
+}
