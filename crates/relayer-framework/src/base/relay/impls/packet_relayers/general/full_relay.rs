9fc6bd1697ca99a7fbec993423143ce97d862f3c
Sean Chen <seanchen11235@gmail.com>
2022-10-24 11:44:41 -0500

    Start adding timeout relayer functionality to FullRelayer

--- crates/relayer-framework/src/base/relay/impls/packet_relayers/general/full_relay.rs
+++ crates/relayer-framework/src/base/relay/impls/packet_relayers/general/full_relay.rs
@@ -5,6 +5,7 @@ use crate::base::chain::traits::queries::status::{CanQueryChainStatus, HasChainS
 use crate::base::relay::traits::packet_relayer::PacketRelayer;
 use crate::base::relay::traits::packet_relayers::ack_packet::CanRelayAckPacket;
 use crate::base::relay::traits::packet_relayers::receive_packet::CanRelayReceivePacket;
+use crate::base::relay::traits::packet_relayers::timeout_unordered_packet::CanRelayTimeoutUnorderedPacket;
 use crate::base::relay::traits::types::HasRelayTypes;
 use crate::base::relay::types::aliases::Packet;
 use crate::std_prelude::*;
@@ -17,15 +18,26 @@ where
     Relay: HasRelayTypes,
     Relay: CanRelayAckPacket,
     Relay: CanRelayReceivePacket,
+    Relay: CanRelayTimeoutUnorderedPacket,
     Relay::DstChain: HasIbcEvents<Relay::SrcChain>,
     Relay::SrcChain: CanQueryChainStatus,
     Relay::DstChain: CanQueryChainStatus,
 {
     async fn relay_packet(relay: &Relay, packet: &Packet<Relay>) -> Result<(), Relay::Error> {
-        let source_status = relay.source_chain().query_chain_status().await?;
+        let source_chain_status = relay.source_chain().query_chain_status().await?;
+        let source_chain_height = Relay::SrcChain::chain_status_height(&source_chain_status);
+        let source_chain_timestamp = Relay::SrcChain::chain_status_timestamp(&source_chain_status);
+
+        let packet_timeout_height = Relay::packet_timeout_height(&packet);
+        let packet_timeout_timestamp = Relay::packet_timeout_timestamp(&packet);
+
+        let has_packet_timed_out = false;
 
         let write_ack = relay
-            .relay_receive_packet(Relay::SrcChain::chain_status_height(&source_status), packet)
+            .relay_receive_packet(
+                Relay::SrcChain::chain_status_height(&source_chain_status),
+                packet,
+            )
             .await?;
 
         if let Some(ack) = write_ack {
