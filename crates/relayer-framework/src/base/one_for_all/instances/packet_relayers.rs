eb5ebe4a55823e5a817791e9deaa5454982fdff2
Sean Chen <seanchen11235@gmail.com>
2022-11-08 16:23:12 -0600

    Cargo fmt

--- crates/relayer-framework/src/base/one_for_all/instances/packet_relayers.rs
+++ crates/relayer-framework/src/base/one_for_all/instances/packet_relayers.rs
@@ -4,14 +4,14 @@ use crate::base::one_for_all::traits::relay::OfaBaseRelay;
 use crate::base::one_for_all::traits::relay::OfaRelayPreset;
 use crate::base::relay::impls::packet_relayers::ack::base_ack_packet::BaseAckPacketRelayer;
 use crate::base::relay::impls::packet_relayers::general::full_relay::FullRelayer;
-use crate::full::one_for_all::traits::relay::OfaFullRelay;
-use crate::full::relay::impls::packet_relayers::retry::RetryRelayer;
 use crate::base::relay::impls::packet_relayers::receive::base_receive_packet::BaseReceivePacketRelayer;
 use crate::base::relay::impls::packet_relayers::receive::skip_received_packet::SkipReceivedPacketRelayer;
 use crate::base::relay::traits::packet_relayer::PacketRelayer;
 use crate::base::relay::traits::packet_relayers::ack_packet::AckPacketRelayer;
 use crate::base::relay::traits::packet_relayers::receive_packet::ReceivePacketRelayer;
 use crate::common::one_for_all::types::relay::OfaRelayWrapper;
+use crate::full::one_for_all::traits::relay::OfaFullRelay;
+use crate::full::relay::impls::packet_relayers::retry::RetryRelayer;
 
 pub fn full_packet_relayer<Relay>() -> PhantomData<impl PacketRelayer<OfaRelayWrapper<Relay>>>
 where
