f72382065dca2ff5df387377096214cb17248c21
Sean Chen <seanchen11235@gmail.com>
2022-11-08 16:14:14 -0600

    Change OfaRelayPreset PacketRelayer instance for MinimalPreset to FullRelayer

--- crates/relayer-framework/src/base/one_for_all/impls/preset.rs
+++ crates/relayer-framework/src/base/one_for_all/impls/preset.rs
@@ -2,6 +2,10 @@ use crate::base::one_for_all::impls::chain::queries::consensus_state::SendConsen
 use crate::base::one_for_all::impls::chain::queries::status::SendChainStatusQueryToOfa;
 use crate::base::one_for_all::traits::chain::{OfaBaseChain, OfaIbcChain};
 use crate::base::one_for_all::traits::chain::{OfaChainPreset, OfaIbcChainPreset};
+use crate::base::one_for_all::traits::relay::{OfaBaseRelay, OfaRelayPreset};
+use crate::base::relay::impls::message_senders::chain_sender::SendIbcMessagesToChain;
+use crate::base::relay::impls::message_senders::update_client::SendIbcMessagesWithUpdateClient;
+use crate::base::relay::impls::packet_relayers::general::full_relay::FullRelayer;
 use crate::common::one_for_all::presets::MinimalPreset;
 
 impl<Chain> OfaChainPreset<Chain> for MinimalPreset
@@ -17,4 +21,13 @@ where
     Counterparty: OfaIbcChain<Chain>,
 {
     type ConsensusStateQuerier = SendConsensusStateQueryToOfa;
+}
+
+impl<Relay> OfaRelayPreset<Relay> for MinimalPreset
+where
+    Relay: OfaBaseRelay<Preset = MinimalPreset>,
+{
+    type PacketRelayer = FullRelayer;
+
+    type IbcMessageSender = SendIbcMessagesWithUpdateClient<SendIbcMessagesToChain>;
 }
