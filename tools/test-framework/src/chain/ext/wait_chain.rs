1b5185f32afc84b912e2ab482c4ab0d20782e975
Luca Joss <luca@informal.systems>
2022-11-09 14:06:59 +0100

    Improved 'wait_for_chain_height' method

--- tools/test-framework/src/chain/ext/wait_chain.rs
+++ tools/test-framework/src/chain/ext/wait_chain.rs
@@ -23,21 +23,15 @@ pub fn wait_for_chain_height<ChainA: ChainHandle, ChainB: ChainHandle>(
     let start = SystemTime::now();
     while reference_application_latest_height < target_height_of_a {
         // Check if the wait time has timed out.
-        match SystemTime::now().duration_since(start) {
-            Ok(elapsed_time) => {
-                if elapsed_time > timeout_bound {
-                    return Err(Error::generic(eyre!(
-                        "chain did not reach desired height after {} seconds",
-                        timeout_bound.as_secs()
-                    )));
-                }
-            }
-            Err(e) => {
-                return Err(Error::generic(eyre!(
-                    "error checking elapsed time in wait_for_chain_height: {}",
-                    e
-                )));
-            }
+        let elapsed_time = SystemTime::now()
+            .duration_since(start)
+            .map_err(handle_generic_error)?;
+
+        if elapsed_time > timeout_bound {
+            return Err(Error::generic(eyre!(
+                "chain did not reach desired height after {} seconds",
+                timeout_bound.as_secs()
+            )));
         }
         std::thread::sleep(Duration::from_millis(500));
 
@@ -45,9 +39,7 @@ pub fn wait_for_chain_height<ChainA: ChainHandle, ChainB: ChainHandle>(
         reference_application_latest_height = foreign_clients
             .client_a_to_b
             .src_chain()
-            .query_latest_height()
-            .ok()
-            .unwrap();
+            .query_latest_height()?;
     }
     Ok(())
 }
