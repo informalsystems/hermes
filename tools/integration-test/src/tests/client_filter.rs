bbc8b0098a89b85ddfb3ea306c5cabd7c03a5890
Sean Chen <seanchen11235@gmail.com>
2022-11-09 12:49:14 -0600

    Check for Client workers

--- tools/integration-test/src/tests/client_filter.rs
+++ tools/integration-test/src/tests/client_filter.rs
@@ -9,18 +9,25 @@ use ibc_relayer_types::core::ics02_client::trust_threshold::TrustThreshold;
 use ibc_relayer::chain::requests::{IncludeProof, QueryClientStateRequest, QueryHeight};
 use ibc_relayer::client_state::AnyClientState;
 use ibc_relayer::foreign_client::CreateOptions;
+use ibc_relayer::object::ObjectType;
 use ibc_relayer_types::clients::ics07_tendermint::client_state::ClientState as TmClientState;
 
 use ibc_test_framework::prelude::*;
 
+struct ClientFilterBlocksConnectionTest;
+struct ClientFilterAllowsConnectionTest;
+
 #[test]
-fn test_client_filter() -> Result<(), Error> {
-    run_binary_channel_test(&ClientFilterTest)
+fn test_client_filter_blocks_connection() -> Result<(), Error> {
+    run_binary_channel_test(&ClientFilterBlocksConnectionTest)
 }
 
-struct ClientFilterTest;
+#[test]
+fn test_client_filter_allows_connection() -> Result<(), Error> {
+    run_binary_channel_test(&ClientFilterAllowsConnectionTest)
+}
 
-impl TestOverrides for ClientFilterTest {
+impl TestOverrides for ClientFilterBlocksConnectionTest {
     fn client_options_a_to_b(&self) -> CreateOptions {
         CreateOptions {
             max_clock_drift: Some(Duration::from_secs(3)),
@@ -38,7 +45,25 @@ impl TestOverrides for ClientFilterTest {
     }
 }
 
-impl BinaryChannelTest for ClientFilterTest {
+impl TestOverrides for ClientFilterAllowsConnectionTest {
+    fn client_options_a_to_b(&self) -> CreateOptions {
+        CreateOptions {
+            max_clock_drift: Some(Duration::from_secs(3)),
+            trusting_period: Some(Duration::from_secs(120_000)),
+            trust_threshold: Some(TrustThreshold::new(13, 23).unwrap()),
+        }
+    }
+
+    fn client_options_b_to_a(&self) -> CreateOptions {
+        CreateOptions {
+            max_clock_drift: Some(Duration::from_secs(6)),
+            trusting_period: Some(Duration::from_secs(340_000)),
+            trust_threshold: Some(TrustThreshold::ONE_THIRD),
+        }
+    }
+}
+
+impl BinaryChannelTest for ClientFilterBlocksConnectionTest {
     fn run<ChainA: ChainHandle, ChainB: ChainHandle>(
         &self,
         _config: &TestConfig,
@@ -69,7 +94,44 @@ impl BinaryChannelTest for ClientFilterTest {
         let supervisor = relayer.spawn_supervisor()?;
         let state = supervisor.dump_state()?;
 
-        println!("Supervisor state: {}", state);
+        assert_eq!(state.workers.contains_key(&ObjectType::Client), false);
+
+        Ok(())
+    }
+}
+
+impl BinaryChannelTest for ClientFilterAllowsConnectionTest {
+    fn run<ChainA: ChainHandle, ChainB: ChainHandle>(
+        &self,
+        _config: &TestConfig,
+        relayer: RelayerDriver,
+        chains: ConnectedChains<ChainA, ChainB>,
+        _channel: ConnectedChannel<ChainA, ChainB>,
+    ) -> Result<(), Error> {
+        let mut policy = FilterPolicy::default();
+
+        let client_id = chains.foreign_clients.client_a_to_b.id();
+        let chain_id = chains.handle_b.id();
+        let state = query_client_state(chains.handle_b, client_id)?;
+        let state = AnyClientState::Tendermint(state);
+        assert_eq!(
+            policy.control_client(&chain_id, client_id, &state),
+            Permission::Allow
+        );
+
+        let client_id = chains.foreign_clients.client_b_to_a.id();
+        let chain_id = chains.handle_a.id();
+        let state = query_client_state(chains.handle_a, client_id)?;
+        let state = AnyClientState::Tendermint(state);
+        assert_eq!(
+            policy.control_client(&chain_id, client_id, &state),
+            Permission::Allow
+        );
+
+        let supervisor = relayer.spawn_supervisor()?;
+        let state = supervisor.dump_state()?;
+
+        assert_eq!(state.workers.contains_key(&ObjectType::Client), true);
 
         Ok(())
     }
