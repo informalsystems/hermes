32e3723bcd2d7a2a593c2c0cf133ae3c2904a9f0
Sean Chen <seanchen11235@gmail.com>
2022-11-14 08:39:12 -0600

    Filter by a range of `TrustThreshold` values (#2839)
    * Add in new filtering logic
    
    * Move upper and lower bound values to constants
    
    * Dump supervisor state in client_filter test
    
    * Remove a println
    
    * Switch to running a binary channel test
    
    * Check for Client workers
    
    * Document test
    
    * Add changelog entry
    
    * Fix clippy warnings
    
    * Reword changelog entry
    
    Signed-off-by: Romain Ruetschi <romain.ruetschi@gmail.com>
    
    * Perform the trust threshold comparison by conversion to rational integers
    
    * Represent the `TrustThreshold` as a `num_rational::Ratio` (#2853)
    
    * Represent the `TrustThreshold` as a `num_rational::Ratio`
    
    * Rename `TrustThreshold::ZERO` to `CLIENT_STATE_REST` and use 0/0 value
    
    * Fix broken test
    
    * Remove manual implementations of `PartialEq`, etc.
    
    * Enforce that the trust threshold must be a proper rational number
    
    * Allow decoding a client state with a 0/0 trust threshold
    
    Signed-off-by: Romain Ruetschi <romain.ruetschi@gmail.com>
    Signed-off-by: Sean Chen <seanchen11235@gmail.com>
    Co-authored-by: Romain Ruetschi <romain@informal.systems>

--- crates/relayer-types/src/clients/ics07_tendermint/client_state.rs
+++ crates/relayer-types/src/clients/ics07_tendermint/client_state.rs
@@ -54,7 +54,7 @@ impl ClientState {
     #[allow(clippy::too_many_arguments)]
     pub fn new(
         chain_id: ChainId,
-        trust_level: TrustThreshold,
+        trust_threshold: TrustThreshold,
         trusting_period: Duration,
         unbonding_period: Duration,
         max_clock_drift: Duration,
@@ -85,24 +85,33 @@ impl ClientState {
             )));
         }
 
-        // `TrustThreshold` is guaranteed to be in the range `[0, 1)`, but a `TrustThreshold::ZERO`
-        // value is invalid in this context
-        if trust_level == TrustThreshold::ZERO {
+        // `TrustThreshold` is guaranteed to be in the range `[0, 1)`,
+        // but a zero value is invalid in this context.
+        if trust_threshold.numerator() == 0 {
             return Err(Error::validation(
-                "ClientState trust-level cannot be zero".to_string(),
+                "ClientState trust threshold cannot be zero".to_string(),
+            ));
+        }
+
+        // Dividing by zero is undefined so we also rule out a zero denominator.
+        // This should be checked already by the `TrustThreshold` constructor
+        // but it does not hurt to redo the check here.
+        if trust_threshold.denominator() == 0 {
+            return Err(Error::validation(
+                "ClientState trust threshold cannot divide by zero".to_string(),
             ));
         }
 
         // Disallow empty proof-specs
         if proof_specs.is_empty() {
             return Err(Error::validation(
-                "ClientState proof-specs cannot be empty".to_string(),
+                "ClientState proof specs cannot be empty".to_string(),
             ));
         }
 
         Ok(Self {
             chain_id,
-            trust_level,
+            trust_level: trust_threshold,
             trusting_period,
             unbonding_period,
             max_clock_drift,
@@ -233,7 +242,7 @@ impl Ics2ClientState for ClientState {
 
         // Reset custom fields to zero values
         self.trusting_period = ZERO_DURATION;
-        self.trust_level = TrustThreshold::ZERO;
+        self.trust_level = TrustThreshold::CLIENT_STATE_RESET;
         self.allow_update.after_expiry = false;
         self.allow_update.after_misbehaviour = false;
         self.frozen_height = None;
@@ -256,10 +265,16 @@ impl TryFrom<RawTmClientState> for ClientState {
     type Error = Error;
 
     fn try_from(raw: RawTmClientState) -> Result<Self, Self::Error> {
-        let trust_level = raw
-            .trust_level
-            .clone()
-            .ok_or_else(Error::missing_trusting_period)?;
+        let trust_level = raw.trust_level.ok_or_else(Error::missing_trust_threshold)?;
+
+        // We need to handle the case where the client is being upgraded and the trust threshold is set to 0/0
+        let trust_threshold = if trust_level.denominator == 0 && trust_level.numerator == 0 {
+            TrustThreshold::CLIENT_STATE_RESET
+        } else {
+            trust_level
+                .try_into()
+                .map_err(|e| Error::invalid_trust_threshold(format!("{}", e)))?
+        };
 
         // In `RawClientState`, a `frozen_height` of `0` means "not frozen".
         // See:
@@ -271,9 +286,7 @@ impl TryFrom<RawTmClientState> for ClientState {
         #[allow(deprecated)]
         Ok(Self {
             chain_id: ChainId::from_string(raw.chain_id.as_str()),
-            trust_level: trust_level
-                .try_into()
-                .map_err(|e| Error::invalid_trust_threshold(format!("{}", e)))?,
+            trust_level: trust_threshold,
             trusting_period: raw
                 .trusting_period
                 .ok_or_else(Error::missing_trusting_period)?
@@ -464,9 +477,9 @@ mod tests {
                 want_pass: false,
             },
             Test {
-                name: "Invalid (too small) trusting trust threshold".to_string(),
+                name: "Invalid (zero) trust threshold".to_string(),
                 params: ClientStateParams {
-                    trust_level: TrustThreshold::ZERO,
+                    trust_level: TrustThreshold::new(0, 3).unwrap(),
                     ..default_params.clone()
                 },
                 want_pass: false,
