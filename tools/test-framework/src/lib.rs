be0005e3fca8453465828ab92dd1b4ff3dbe835e
Sean Chen <seanchen11235@gmail.com>
2022-11-30 15:59:25 -0600

    Continue working on MVP tutorial

--- tools/test-framework/src/lib.rs
+++ tools/test-framework/src/lib.rs
@@ -45,10 +45,60 @@
 //! most test function typically does nothing more than to specify what kind of test we want 
 //! to run. In the case of this example, we're running a 
 //! [`BinaryChannelTest`](ibc_test_framework::framework::binary::channel::BinaryChannelTest),
-//! which sets up a relayer instance between two chains connected via channels. 
+//! which sets up a relayer instance between two full nodes connected via IBC channels with
+//! completed handshakes.
 //! 
 //! Note that the `run_binary_channel_test` (and indeed every `run_*` test function) takes as
-//! its single parameter an empty struct that represents the test case. 
+//! its single parameter an empty struct that represents the test case. In order to customize
+//! the behavior of a test, different traits need to implemented on the empty struct, depending
+//! on how you wish to modify the test.
+//! 
+//! This example tests showcases implementing the `TestOverrides` trait, which is used to set 
+//! configuration and initialization values for the relayer instance that is being tested (in 
+//! this case though, nothing is being overriden).
+//! 
+//! The main logic of the test is implemented in the `run` function of the `BinaryChannelTest` 
+//! trait. This trait is implemented for our empty test struct since we're choosing to run a
+//! test between two chains connected via IBC channels. If we had instead opted to run a binary
+//! _chain_ test using the `run_binary_chain_test`, then we would instead implement the 
+//! [`BinaryChainTest`](ibc_test_framework::framework::binary::chain::BinaryChannelTest) 
+//! trait for our empty test struct. 
+//! 
+//! The `run` function's parameters are:
+//! 1. `config`: for accessing any relayer configuration values during the course of the test
+//! 2. `relayer`: the relayer instance that is being tested
+//! 3. `chains`: handles to the two chains, `ChainA` and `ChainB`, that are being relayed between
+//! 4. `channel`: handles to the uni-directional channels connecting both ends of the two chains
+//! 
+//! In this simple example test, the `run` function simply calls the [`suspend`] function,
+//! which suspends the test indefinitely. While this means the test will never actually
+//! pass, we can use this as a starting point in order to perform _manual testing_ with
+//! the chains that have been set up in the test.
+//! 
+//! ## Running Tests
+//! By convention, tests are written to the [`tests`](ibc_test_framework::tests) module. We
+//! can then run the test via the command line as follows:
+//! 
+//! ```bash
+//! RUST_LOG=info RUST_BACKTRACE=1 \
+//!     cargo test -p ibc-relayer-test --features example -- --test-threads=1 \
+//!     example_test
+//! ```
+//! 
+//! The environment variable RUST_LOG controls the log level. The RUST_BACKTRACE variable
+//! displays a backtrace when errors occur in a test. The test flag `--test-threads=1` is
+//! set so that tests are run serially (this makes it easier to follow what is going on
+//! via the log output). Take a look at the [`TestConfig`](ibc_test_framework::types::config::TestConfig)
+//! type for more information about configuring how tests can be run.
+//! 
+//! For this example, we disable the test from running by default, since it calls the 
+//! [`suspend`] function and will thus never pass. We explicitly pass `--features example`
+//! so that the `example` feature is activated such that this test will run. Finally, we
+//! specify the name of the test so that _only_ our example test is run.
+//! 
+//! After starting the test run, we may see logs such as the following:
+//! 
+//! 
 
 extern crate alloc;
 
