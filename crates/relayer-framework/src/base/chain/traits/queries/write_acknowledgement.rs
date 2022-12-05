4935ed740e529f214143c8a3654f47810db89ec0
Sean Chen <seanchen11235@gmail.com>
2022-12-05 09:50:42 -0600

    Remove commented-out WriteAckQuerier trait definition

--- crates/relayer-framework/src/base/chain/traits/queries/write_acknowledgement.rs
+++ crates/relayer-framework/src/base/chain/traits/queries/write_acknowledgement.rs
@@ -4,20 +4,6 @@ use crate::base::chain::traits::ibc_event::HasIbcEvents;
 use crate::base::chain::traits::types::HasIbcChainTypes;
 use crate::std_prelude::*;
 
-// #[async_trait]
-// pub trait WriteAckQuerier<Chain, Counterparty>
-// where
-//     Chain: HasIbcChainTypes<Counterparty>,
-//     Counterparty: HasIbcChainTypes<Chain>,
-// {
-//     async fn query_write_ack_event(
-//         channel_id: &Chain::ChannelId,
-//         port_id: &Chain::PortId,
-//         counterparty_channel_id: &Counterparty::ChannelId,
-//         counterparty_port_id: &Counterparty::PortId,
-//     ) -> Result<Option<Chain::WriteAcknowledgementEvent>, Chain::Error>;
-// }
-
 #[async_trait]
 pub trait CanQueryWriteAck<Counterparty>: HasIbcEvents<Counterparty>
 where
