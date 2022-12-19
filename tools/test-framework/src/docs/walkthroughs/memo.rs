b0f80b79db2a44e789ab9f738915df0ecf949e4d
Sean Chen <skypemaster007@gmail.com>
2022-12-19 09:48:20 -0600

    Run cargo fmt

--- tools/test-framework/src/docs/walkthroughs/memo.rs
+++ tools/test-framework/src/docs/walkthroughs/memo.rs
@@ -4,10 +4,10 @@
 //! struct as test input. This test tests that the process of transferring
 //! IBC messages preserves the `memo` field. For the purposes of this explanation,
 //! the `memo` field is nothing more than a `String` field for carrying along
-//! some arbitrary metadata as part of the transaction. 
-//! 
+//! some arbitrary metadata as part of the transaction.
+//!
 //! The test in most of its entirety (some parts omitted for brevity) looks like this:
-//! 
+//!
 //! ```no_run
 //! #[test]
 //! fn test_memo() -> Result<(), Error> {
@@ -15,11 +15,11 @@
 //!     let test = MemoTest { memo };
 //!     run_binary_channel_test(&test)
 //! }
-//! 
+//!
 //! pub struct MemoTest {
 //!     memo: Memo,
 //! }
-//! 
+//!
 //! impl:TestOverrides for MemoTest {
 //!     fn modify_relayer_config(&self, config: &mut Config) {
 //!         for mut chain in config.chains.iter_mut() {
@@ -27,7 +27,7 @@
 //!         }
 //!     }
 //! }
-//! 
+//!
 //! impl BinaryChannelTest for MemoTest {
 //!     fn run<ChainA: ChainHandle, ChainB: ChainHandle>(
 //!         &self,
@@ -37,9 +37,9 @@
 //!         channel: ConnectedChannel<ChainA, ChainB>,
 //!     ) -> Result<(), Error> {
 //!         let denom_a = chains.node_a.denom();
-//! 
+//!
 //!         let a_to_b_amount = random_u128_range(1000, 5000);
-//! 
+//!
 //!         chains.node_a.chain_driver().ibc_transfer_token(
 //!             &channel.port_a.as_ref(),
 //!             &channel.channel_id_a.as_ref(),
@@ -47,25 +47,25 @@
 //!             &chains.node_b.wallets().user1().address(),
 //!             &denom_a.with_amount(a_to_b_amount).as_ref(),
 //!         )?;
-//! 
+//!
 //!         let denom_b = derive_ibc_denom(
 //!             &channel.port_b.as_ref(),
 //!             &channel.channel_id_b.as_ref(),
 //!             &denom_a,
 //!         )?;
-//! 
+//!
 //!         chains.node_b.chain_driver().assert_eventual_wallet_amount(
 //!             &chains.node_b.wallets().user1().address(),
 //!             &denom_b.with_amount(a_to_b_amount).as_ref(),
 //!         )?;
-//! 
+//!
 //!         let tx_info = chains
 //!             .node_b
 //!             .chain_driver()
 //!             .query_recipient_transactions(&chains.node_b.wallets().user1().address())?;
-//! 
+//!
 //!         assert_tx_memo_equals(&tx_info, self.memo.as_str())?;
-//! 
+//!
 //!         Ok(())
 //!     }
 //! }
