e62e33e11883df5bb1ea9bcaafce35f0ac25ee38
Sean Chen <skypemaster007@gmail.com>
2023-01-04 15:26:25 -0600

    Implement CanBuildTimeoutUnorderedPacketMessage for OfaChainWrapper

--- crates/relayer-framework/src/base/one_for_all/impls/chain/message/timeout_unordered_packet.rs
+++ crates/relayer-framework/src/base/one_for_all/impls/chain/message/timeout_unordered_packet.rs
@@ -1,1 +1,21 @@
-
+#[async_trait]
+impl<Chain, Counterparty> CanBuildTimeoutUnorderedPacketMessage<OfaChainWrapper<Counterparty>>
+    for OfaChainWrapper<Chain>
+where
+    Chain: OfaIbcChain<Counterparty>,
+    Counterparty: OfaIbcChain<
+        Chain,
+        IncomingPacket = Chain::OutgoingPacket,
+        OutgoingPacket = Chain::IncomingPacket,
+    >,
+{
+    async fn build_timeout_unordered_packet_message(
+        &self,
+        height: &Self::Height,
+        packet: &Self::OutgoingPacket,
+    ) -> Result<Counterparty::Message, Self::Error> {
+        self.chain
+            .build_timeout_unordered_packet_message(height, packet)
+            .await
+    }
+}
