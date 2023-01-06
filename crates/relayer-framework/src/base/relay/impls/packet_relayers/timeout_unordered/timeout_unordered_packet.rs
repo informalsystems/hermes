c5d10302b112a0ade430753511fae87b44a6b891
Sean Chen <skypemaster007@gmail.com>
2023-01-06 13:28:56 -0600

    Change trait impls for BasePacketRelayer types

--- crates/relayer-framework/src/base/relay/impls/packet_relayers/timeout_unordered/timeout_unordered_packet.rs
+++ crates/relayer-framework/src/base/relay/impls/packet_relayers/timeout_unordered/timeout_unordered_packet.rs
@@ -1,36 +1,40 @@
 use async_trait::async_trait;
 
+use crate::base::chain::traits::ibc_event::HasIbcEvents;
+use crate::base::chain::traits::message::timeout_unordered_packet::CanBuildTimeoutUnorderedPacketMessage;
 use crate::base::chain::types::aliases::Height;
 use crate::base::relay::traits::ibc_message_sender::{CanSendIbcMessages, IbcMessageSenderExt};
-use crate::base::relay::traits::messages::timeout_unordered_packet::CanBuildTimeoutUnorderedPacketMessage;
 use crate::base::relay::traits::packet_relayers::timeout_unordered_packet::TimeoutUnorderedPacketRelayer;
 use crate::base::relay::traits::target::SourceTarget;
 use crate::base::relay::traits::types::HasRelayTypes;
 use crate::base::relay::types::aliases::Packet;
 use crate::std_prelude::*;
 
-/// The lowest-level minimal type that implements timeout packet relayer
+/// The minimal component that implements timeout packet relayer
 /// capabilities. Timeout packet relayers with more capabilities can be
 /// implemented on top of this base type.
 pub struct BaseTimeoutUnorderedPacketRelayer;
 
 #[async_trait]
-impl<Context> TimeoutUnorderedPacketRelayer<Context> for BaseTimeoutUnorderedPacketRelayer
+impl<Relay, TimeoutEvent> TimeoutUnorderedPacketRelayer<Relay> for BaseTimeoutUnorderedPacketRelayer
 where
-    Context: CanBuildTimeoutUnorderedPacketMessage,
-    Context: CanSendIbcMessages<SourceTarget>,
-    Context: HasRelayTypes,
+    Relay: HasRelayTypes,
+    Relay: CanSendIbcMessages<SourceTarget>,
+    Relay::SrcChain: HasIbcEvents<Relay::DstChain, WriteAcknowledgementEvent = TimeoutEvent>,
+    Relay::DstChain: CanBuildTimeoutUnorderedPacketMessage<Relay::SrcChain>,
 {
     async fn relay_timeout_unordered_packet(
-        context: &Context,
-        destination_height: &Height<Context::DstChain>,
-        packet: &Packet<Context>,
-    ) -> Result<(), Context::Error> {
-        let message = context
+        relay: &Relay,
+        destination_height: &Height<Relay::DstChain>,
+        packet: &Packet<Relay>,
+    ) -> Result<(), Relay::Error> {
+        let message = relay
+            .destination_chain()
             .build_timeout_unordered_packet_message(destination_height, packet)
-            .await?;
+            .await
+            .map_err(Relay::dst_chain_error)?;
 
-        context.send_message(message).await?;
+        relay.send_message(message).await?;
 
         Ok(())
     }
