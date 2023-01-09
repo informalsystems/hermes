a45b6e99ac48488b929b627bfb09135a0909a3af
Romain Ruetschi <romain@informal.systems>
2023-01-09 15:05:20 +0100

    Update integration test to use internal helper

--- tools/integration-test/src/tests/consensus_states.rs
+++ tools/integration-test/src/tests/consensus_states.rs
@@ -1,8 +1,8 @@
 use std::time::Duration;
 
-use ibc_relayer::chain::requests::{
-    PageRequest,
-    QueryConsensusStateHeightsRequest, // QueryConsensusStatesRequest,
+use ibc_relayer::chain::{
+    cosmos::query::consensus_state::query_consensus_states,
+    requests::{PageRequest, QueryConsensusStateHeightsRequest, QueryConsensusStatesRequest},
 };
 
 use ibc_test_framework::prelude::*;
@@ -29,23 +29,12 @@ impl BinaryChainTest for ConsensusStateHeights {
         let client = &chains.foreign_clients.client_a_to_b;
 
         for i in 1..=UPDATES_COUNT {
-            eprintln!("updating client ({i}/{UPDATES_COUNT})...");
-
             client.update().map_err(Error::foreign_client)?;
 
             if i != UPDATES_COUNT {
                 std::thread::sleep(Duration::from_secs(5));
             }
         }
-
-        // let states = chains
-        //     .handle_b()
-        //     .query_consensus_states(QueryConsensusStatesRequest {
-        //         client_id: (*chains.client_id_b().value()).clone(),
-        //         pagination: Some(PageRequest::all()),
-        //     })?;
-
-        // assert_eq!(states.len(), CONSENSUS_STATES_COUNT);
 
         let heights =
             chains
@@ -55,14 +44,52 @@ impl BinaryChainTest for ConsensusStateHeights {
                     pagination: Some(PageRequest::all()),
                 })?;
 
-        assert_eq!(heights.len(), CONSENSUS_STATES_COUNT);
+        assert_eq(
+            "did not find the expected amount of consensus state heights",
+            &heights.len(),
+            &CONSENSUS_STATES_COUNT,
+        )?;
 
-        // states
-        //     .into_iter()
-        //     .zip(heights.into_iter())
-        //     .for_each(|(state, height)| {
-        //         assert_eq!(state.height, height);
-        //     });
+        let grpc_address = chains
+            .node_b
+            .value()
+            .chain_driver
+            .grpc_address()
+            .as_str()
+            .parse()
+            .unwrap();
+
+        let states =
+            chains
+                .node_b
+                .value()
+                .chain_driver
+                .runtime
+                .block_on(query_consensus_states(
+                    chains.node_b.chain_id().value(),
+                    &grpc_address,
+                    QueryConsensusStatesRequest {
+                        client_id: (*chains.client_id_b().value()).clone(),
+                        pagination: Some(PageRequest::all()),
+                    },
+                ))?;
+
+        assert_eq(
+            "did not find the expected number of consensus states",
+            &states.len(),
+            &CONSENSUS_STATES_COUNT,
+        )?;
+
+        states
+            .into_iter()
+            .zip(heights.into_iter())
+            .try_for_each(|(state, height)| {
+                assert_eq(
+                    "did not find matching height for each consensus state",
+                    &state.height,
+                    &height,
+                )
+            })?;
 
         Ok(())
     }
