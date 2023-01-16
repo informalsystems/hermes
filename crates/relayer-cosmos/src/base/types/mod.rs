90fe21210a16edb6dc8a0f67e41afb071826e7ef
Soares Chen <soares.chen@maybevoid.com>
2023-01-16 14:07:11 +0100

    Add documentation for event relayers

--- crates/relayer-cosmos/src/base/types/mod.rs
+++ crates/relayer-cosmos/src/base/types/mod.rs
@@ -1,5 +1,4 @@
 pub mod chain;
-pub mod event_source;
 pub mod message;
 pub mod relay;
 pub mod transaction;
--- crates/relayer-framework/src/base/relay/traits/event_relayer.rs
+++ crates/relayer-framework/src/base/relay/traits/event_relayer.rs
@@ -1,6 +1,6 @@
 use async_trait::async_trait;
 
-use crate::base::chain::traits::ibc_event::HasSendPacketEvent;
+use crate::base::chain::traits::ibc_event::{HasSendPacketEvent, HasWriteAcknowledgementEvent};
 use crate::base::chain::traits::types::event::HasEventType;
 use crate::base::chain::types::aliases::{Event, Height};
 use crate::base::core::traits::sync::Async;
@@ -10,6 +10,15 @@ use crate::base::relay::traits::target::{DestinationTarget, SourceTarget};
 use crate::base::relay::traits::types::HasRelayTypes;
 use crate::std_prelude::*;
 
+/**
+   An event relayer performs relay actions based on one event at a time from
+   the target chain.
+
+   The event relayer is a general abstraction over other relayer types that
+   need to be reactive to chain events. This includes the
+   [packet relayer](CanRelayPacket), but also future relayers such as
+   connection and channel handshake relayers.
+*/
 #[async_trait]
 pub trait EventRelayer<Relay, Target>: Async
 where
@@ -17,6 +26,13 @@ where
     Target: ChainTarget<Relay>,
     Target::TargetChain: HasEventType,
 {
+    /**
+       Relay a chain event which is emitted from the target chain at a given
+       height.
+
+       The chain event could be anything. If the given event is not related to
+       IBC, the relayer should do nothing and return `Ok(())`.
+    */
     async fn relay_chain_event(
         relay: &Relay,
         height: &Height<Target::TargetChain>,
@@ -37,10 +53,27 @@ where
     ) -> Result<(), Self::Error>;
 }
 
-struct BaseEventRelayer;
+/**
+   A packet event relayer that performs packet relaying based on chain events
+   related to IBC packets.
+
+   The implementation of `PacketEventRelayer` for the [`SourceTarget`] is
+   different from the [`DestinationTarget`]. This is because the packet
+   relaying operations from the source chain is different from the target chain.
+
+   When relaying events from the source chain, the packet event relayer is
+   mostly interested in the `SendPacket` event, so that it can relay a
+   `RecvPacket` message to the destination chain, or a `TimeoutPacket` message
+   to the source chain.
+
+   When relaying events from the destination chain, the packet event relayer
+   is mostly interested in the `WriteAcknowledgement` event, so that it can
+   relay a `AckPacket` message to the source chain.
+*/
+struct PacketEventRelayer;
 
 #[async_trait]
-impl<Relay> EventRelayer<Relay, SourceTarget> for BaseEventRelayer
+impl<Relay> EventRelayer<Relay, SourceTarget> for PacketEventRelayer
 where
     Relay: CanRelayPacket,
     Relay::SrcChain: HasSendPacketEvent<Relay::DstChain>,
@@ -61,16 +94,19 @@ where
 }
 
 #[async_trait]
-impl<Relay> EventRelayer<Relay, DestinationTarget> for BaseEventRelayer
+impl<Relay> EventRelayer<Relay, DestinationTarget> for PacketEventRelayer
 where
     Relay: CanRelayPacket,
-    Relay::SrcChain: HasSendPacketEvent<Relay::DstChain>,
+    Relay::SrcChain: HasWriteAcknowledgementEvent<Relay::DstChain>,
 {
     async fn relay_chain_event(
         _relayer: &Relay,
         _height: &Height<Relay::DstChain>,
         _event: &Event<Relay::DstChain>,
     ) -> Result<(), Relay::Error> {
+        // TODO: implementing the logic for extracting write acknowledgement
+        // and relaying Ack packets.
+
         Ok(())
     }
 }
