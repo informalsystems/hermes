ffdb5be2481e1f6946a1d261c4f304179ddc9822
Anca Zamfir <zamfiranca@gmail.com>
2023-02-10 16:07:40 +0100

    Cleanup

--- crates/relayer-types/src/core/ics02_client/height.rs
+++ crates/relayer-types/src/core/ics02_client/height.rs
@@ -15,7 +15,7 @@ use crate::core::ics02_client::error::Error;
 #[derive(Copy, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct Height {
     /// Previously known as "epoch"
-    pub revision_number: u64,
+    revision_number: u64,
 
     /// The height of a block
     revision_height: u64,
