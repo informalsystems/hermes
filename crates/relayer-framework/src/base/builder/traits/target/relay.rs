477ea77093566debda71f30cc6202fe5a996e913
Soares Chen <soares.chen@maybevoid.com>
2023-02-17 23:30:36 +0100

    Fix gitignore

---
+++ crates/relayer-framework/src/base/builder/traits/target/relay.rs
@@ -0,0 +1,52 @@
+use crate::base::builder::traits::birelay::HasBiRelayType;
+use crate::base::builder::traits::target::chain::{ChainATarget, ChainBTarget, ChainBuildTarget};
+use crate::base::builder::types::aliases::{RelayAToB, RelayBToA, RelayError};
+use crate::base::core::traits::sync::Async;
+use crate::base::relay::traits::types::HasRelayTypes;
+
+#[derive(Default)]
+pub struct RelayAToBTarget;
+
+#[derive(Default)]
+pub struct RelayBToATarget;
+
+pub trait RelayBuildTarget<Build>: Default + Async
+where
+    Build: HasBiRelayType,
+{
+    type TargetRelay: HasRelayTypes<Error = RelayError<Build>>;
+
+    type SrcChainTarget: ChainBuildTarget<
+        Build,
+        TargetChain = <Self::TargetRelay as HasRelayTypes>::SrcChain,
+        CounterpartyChain = <Self::TargetRelay as HasRelayTypes>::DstChain,
+    >;
+
+    type DstChainTarget: ChainBuildTarget<
+        Build,
+        TargetChain = <Self::TargetRelay as HasRelayTypes>::DstChain,
+        CounterpartyChain = <Self::TargetRelay as HasRelayTypes>::SrcChain,
+    >;
+}
+
+impl<Build> RelayBuildTarget<Build> for RelayAToBTarget
+where
+    Build: HasBiRelayType,
+{
+    type TargetRelay = RelayAToB<Build>;
+
+    type SrcChainTarget = ChainATarget;
+
+    type DstChainTarget = ChainBTarget;
+}
+
+impl<Build> RelayBuildTarget<Build> for RelayBToATarget
+where
+    Build: HasBiRelayType,
+{
+    type TargetRelay = RelayBToA<Build>;
+
+    type SrcChainTarget = ChainBTarget;
+
+    type DstChainTarget = ChainATarget;
+}
