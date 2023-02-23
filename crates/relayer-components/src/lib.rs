c70d2a4929c04997945ebfb0511acbf1e3672054
Soares Chen <soares.chen@maybevoid.com>
2023-02-23 10:37:58 +0100

    Move base module to ibc-relayer-components crate

---
+++ crates/relayer-components/src/lib.rs
@@ -0,0 +1,13 @@
+#![no_std]
+#![allow(clippy::type_complexity)]
+#![allow(clippy::too_many_arguments)]
+
+mod std_prelude;
+extern crate alloc;
+
+pub mod builder;
+pub mod chain;
+pub mod core;
+pub mod relay;
+pub mod runtime;
+pub mod transaction;
