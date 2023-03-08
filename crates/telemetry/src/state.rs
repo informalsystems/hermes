c427eda6d0836689885b4452fdd4d8759fe739bb
Luca Joss <luca@informal.systems>
2023-03-08 10:58:25 +0100

    Update guide with new ICS29 metrics

--- crates/telemetry/src/state.rs
+++ crates/telemetry/src/state.rs
@@ -1056,7 +1056,7 @@ impl Default for TelemetryState {
 
             period_fees: meter
                 .u64_observable_gauge("ics29_period_fees")
-                .with_description("Amount of ICS29 fees rewarded last 7 days.")
+                .with_description("Amount of ICS29 fees rewarded last 7 days")
                 .init(),
         }
     }
