946ae4dbc7f58ae765acd5f3cfc4723614b820b2
Soares Chen <soares.chen@maybevoid.com>
2023-03-16 12:06:04 +0100

    Refactor Cosmos event subscription

--- crates/relayer-cosmos/src/base/impls/subscription.rs
+++ crates/relayer-cosmos/src/base/impls/subscription.rs
@@ -1,6 +1,7 @@
 use alloc::sync::Arc;
 use core::pin::Pin;
 use tendermint_rpc::client::CompatMode;
+use tracing::error;
 
 use async_trait::async_trait;
 use futures::stream::{self, Stream, StreamExt, TryStreamExt};
@@ -30,7 +31,7 @@ pub trait CanCreateAbciEventSubscription: Async {
 impl<Runtime> CanCreateAbciEventSubscription for Runtime
 where
     Runtime:
-        CanCreateRpcEventStream + CanCreateClosureSubscription + CanMultiplexSubscription + Clone,
+        CanCreateAbciEventStream + CanCreateClosureSubscription + CanMultiplexSubscription + Clone,
 {
     fn new_abci_event_subscription(
         &self,
@@ -48,16 +49,27 @@ where
                 let queries = queries.clone();
 
                 Box::pin(async move {
-                    // TODO: log error
-                    let rpc_event_stream = runtime
-                        .new_rpc_event_stream(websocket_url.clone(), compat_mode, &queries)
-                        .await
-                        .ok()?;
+                    let stream_result = runtime
+                        .new_abci_event_stream(
+                            chain_version,
+                            &websocket_url,
+                            &compat_mode,
+                            &queries,
+                        )
+                        .await;
 
-                    let abci_event_stream =
-                        rpc_event_to_abci_event_stream(chain_version, rpc_event_stream);
+                    match stream_result {
+                        Ok(event_stream) => Some(event_stream),
+                        Err(e) => {
+                            error!(
+                                error = "failed to create new ABCI event stream. terminating subscription",
+                                details = %e,
+                                websocket_url = %websocket_url,
+                            );
 
-                    Some(abci_event_stream)
+                            None
+                        }
+                    }
                 })
             })
         };
@@ -68,11 +80,68 @@ where
     }
 }
 
-pub fn rpc_event_to_abci_event_stream(
+#[async_trait]
+pub trait CanCreateAbciEventStream: Async {
+    async fn new_abci_event_stream(
+        &self,
+        chain_version: u64,
+        websocket_url: &WebSocketClientUrl,
+        compat_mode: &CompatMode,
+        queries: &[Query],
+    ) -> Result<Pin<Box<dyn Stream<Item = (Height, AbciEvent)> + Send + 'static>>, Error>;
+}
+
+#[async_trait]
+impl<Runtime> CanCreateAbciEventStream for Runtime
+where
+    Runtime: HasSpawner,
+{
+    async fn new_abci_event_stream(
+        &self,
+        chain_version: u64,
+        websocket_url: &WebSocketClientUrl,
+        compat_mode: &CompatMode,
+        queries: &[Query],
+    ) -> Result<Pin<Box<dyn Stream<Item = (Height, AbciEvent)> + Send + 'static>>, Error> {
+        let builder = WebSocketClient::builder(websocket_url.clone()).compat_mode(*compat_mode);
+
+        let (client, driver) = builder.build().await.map_err(BaseError::tendermint_rpc)?;
+
+        let spawner = self.spawner();
+        spawner.spawn(async move {
+            let _ = driver.run().await;
+        });
+
+        let event_stream =
+            new_abci_event_stream_with_queries(chain_version, &client, queries).await?;
+
+        Ok(event_stream)
+    }
+}
+
+async fn new_abci_event_stream_with_queries(
     chain_version: u64,
-    rpc_event_stream: Pin<Box<dyn Stream<Item = RpcEvent> + Send + 'static>>,
-) -> Pin<Box<dyn Stream<Item = (Height, AbciEvent)> + Send + 'static>> {
-    let abci_event_stream = rpc_event_stream
+    websocket_client: &WebSocketClient,
+    queries: &[Query],
+) -> Result<Pin<Box<dyn Stream<Item = (Height, AbciEvent)> + Send + 'static>>, Error> {
+    let event_streams = stream::iter(queries)
+        .then(|query| new_abci_event_stream_with_query(chain_version, websocket_client, query))
+        .try_collect::<Vec<_>>()
+        .await?;
+
+    let event_stream = stream::iter(event_streams).flatten();
+
+    Ok(Box::pin(event_stream))
+}
+
+async fn new_abci_event_stream_with_query(
+    chain_version: u64,
+    websocket_client: &WebSocketClient,
+    query: &Query,
+) -> Result<Pin<Box<dyn Stream<Item = (Height, AbciEvent)> + Send + 'static>>, Error> {
+    let rpc_stream = new_rpc_event_stream(websocket_client, query).await?;
+
+    let abci_stream = rpc_stream
         .filter_map(move |rpc_event| async move {
             match rpc_event.data {
                 RpcEventData::Tx { tx_result } => {
@@ -93,47 +162,19 @@ pub fn rpc_event_to_abci_event_stream(
         })
         .flatten();
 
-    Box::pin(abci_event_stream)
+    Ok(Box::pin(abci_stream))
 }
 
-#[async_trait]
-pub trait CanCreateRpcEventStream: Async {
-    async fn new_rpc_event_stream(
-        &self,
-        websocket_url: WebSocketClientUrl,
-        compat_mode: CompatMode,
-        queries: &[Query],
-    ) -> Result<Pin<Box<dyn Stream<Item = RpcEvent> + Send + 'static>>, Error>;
-}
+async fn new_rpc_event_stream(
+    websocket_client: &WebSocketClient,
+    query: &Query,
+) -> Result<Pin<Box<dyn Stream<Item = RpcEvent> + Send + 'static>>, Error> {
+    let subscription = websocket_client
+        .subscribe(query.clone())
+        .await
+        .map_err(BaseError::tendermint_rpc)?;
 
-#[async_trait]
-impl<Runtime> CanCreateRpcEventStream for Runtime
-where
-    Runtime: HasSpawner,
-{
-    async fn new_rpc_event_stream(
-        &self,
-        websocket_url: WebSocketClientUrl,
-        compat_mode: CompatMode,
-        queries: &[Query],
-    ) -> Result<Pin<Box<dyn Stream<Item = RpcEvent> + Send + 'static>>, Error> {
-        let builder = WebSocketClient::builder(websocket_url).compat_mode(compat_mode);
+    let stream = subscription.filter_map(|event| async { event.ok() });
 
-        let (client, driver) = builder.build().await.map_err(BaseError::tendermint_rpc)?;
-
-        let spawner = self.spawner();
-        spawner.spawn(async move {
-            let _ = driver.run().await;
-        });
-
-        let subscriptions = stream::iter(queries.iter())
-            .then(|query| async { client.subscribe(query.clone()).await })
-            .try_collect::<Vec<_>>()
-            .await
-            .map_err(BaseError::tendermint_rpc)?;
-
-        let stream = stream::select_all(subscriptions).filter_map(|event| async { event.ok() });
-
-        Ok(Box::pin(stream))
-    }
+    Ok(Box::pin(stream))
 }
