84340da84f621ae44976134080f9eea3cd9f2eb6
Romain Ruetschi <romain@informal.systems>
2023-04-05 11:12:12 +0200

    Do not take the ABCI event index into accout when deduping events since it is not deterministic

--- crates/relayer/src/event/monitor.rs
+++ crates/relayer/src/event/monitor.rs
@@ -267,16 +267,35 @@ fn dedupe(events: Vec<abci::Event>) -> Vec<abci::Event> {
     use itertools::Itertools;
     use std::hash::{Hash, Hasher};
 
-    #[derive(Clone, PartialEq, Eq)]
+    #[derive(Clone)]
     struct HashEvent(abci::Event);
+
+    impl PartialEq for HashEvent {
+        fn eq(&self, other: &Self) -> bool {
+            // NOTE: We don't compare on the index because it is not deterministic
+            // NOTE: We need to check the length of the attributes in order
+            // to not miss any attribute
+            self.0.kind == other.0.kind
+                && self.0.attributes.len() == other.0.attributes.len()
+                && self
+                    .0
+                    .attributes
+                    .iter()
+                    .zip(other.0.attributes.iter())
+                    .all(|(a, b)| a.key == b.key && a.value == b.value)
+        }
+    }
+
+    impl Eq for HashEvent {}
 
     impl Hash for HashEvent {
         fn hash<H: Hasher>(&self, state: &mut H) {
             self.0.kind.hash(state);
+
             for attr in &self.0.attributes {
+                // NOTE: We don't hash the index because it is not deterministic
                 attr.key.hash(state);
                 attr.value.hash(state);
-                attr.index.hash(state);
             }
         }
     }
