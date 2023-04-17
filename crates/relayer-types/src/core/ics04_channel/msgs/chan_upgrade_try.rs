b131e275bf0110dd223f684bd6762dd785bb1b8a
Romain Ruetschi <romain@informal.systems>
2023-04-17 15:27:25 +0200

    Re-enable tests

--- crates/relayer-types/src/core/ics04_channel/msgs/chan_upgrade_try.rs
+++ crates/relayer-types/src/core/ics04_channel/msgs/chan_upgrade_try.rs
@@ -159,130 +159,141 @@ impl From<MsgChannelUpgradeTry> for RawMsgChannelUpgradeTry {
     }
 }
 
-// #[cfg(test)]
-// pub mod test_util {
-//     use crate::prelude::*;
-//     use ibc_proto::ibc::core::channel::v1::MsgChannelUpgradeTry as RawMsgChannelUpgradeTry;
+#[cfg(test)]
+pub mod test_util {
+    use ibc_proto::ibc::core::channel::v1::MsgChannelUpgradeTry as RawMsgChannelUpgradeTry;
+    use ibc_proto::ibc::core::client::v1::Height;
 
-//     use crate::core::ics04_channel::channel::test_util::get_dummy_raw_channel_end;
-//     use crate::core::ics24_host::identifier::{ChannelId, PortId};
-//     use crate::test_utils::get_dummy_bech32_account;
+    use crate::core::ics04_channel::channel::test_util::get_dummy_raw_channel_end;
+    use crate::core::ics24_host::identifier::{ChannelId, PortId};
+    use crate::prelude::*;
+    use crate::test_utils::get_dummy_bech32_account;
 
-//     /// Returns a dummy `RawMsgChannelUpgradeTry`, for testing only!
-//     pub fn get_dummy_raw_chan_upgrade_try() -> RawMsgChannelUpgradeTry {
-//         RawMsgChannelUpgradeTry {
-//             port_id: PortId::default().to_string(),
-//             channel_id: ChannelId::default().to_string(),
-//             signer: get_dummy_bech32_account(),
-//             counterparty_channel: Some(get_dummy_raw_channel_end()),
-//             counterparty_sequence: 1,
-//             proposed_upgrade_channel: Some(get_dummy_raw_channel_end()),
-//             timeout_height: Some(get_dummy_raw_height()),
-//             timeout_timestamp: Timestamp::now().into(),
-//         }
-//     }
-// }
+    /// Returns a dummy `RawMsgChannelUpgradeTry`, for testing only!
+    pub fn get_dummy_raw_msg_chan_upgrade_try() -> RawMsgChannelUpgradeTry {
+        RawMsgChannelUpgradeTry {
+            port_id: PortId::default().to_string(),
+            channel_id: ChannelId::default().to_string(),
+            signer: get_dummy_bech32_account(),
+            counterparty_channel: Some(get_dummy_raw_channel_end()),
+            counterparty_sequence: 1,
+            proposed_upgrade_channel: Some(get_dummy_raw_channel_end()),
+            timeout_height: Some(Height {
+                revision_number: 1,
+                revision_height: 1,
+            }),
+            timeout_timestamp: 1681737922,
+            proof_channel: vec![],
+            proof_upgrade_timeout: vec![],
+            proof_upgrade_sequence: vec![],
+            proof_height: Some(Height {
+                revision_number: 1,
+                revision_height: 1,
+            }),
+        }
+    }
+}
 
-// #[cfg(test)]
-// mod tests {
-//     use crate::prelude::*;
+#[cfg(test)]
+mod tests {
+    use crate::prelude::*;
 
-//     use test_log::test;
+    use test_log::test;
 
-//     use ibc_proto::ibc::core::channel::v1::MsgChannelUpgradeTry as RawMsgChannelUpgradeTry;
+    use ibc_proto::ibc::core::channel::v1::MsgChannelUpgradeTry as RawMsgChannelUpgradeTry;
 
-//     use crate::core::ics04_channel::msgs::chan_upgrade_init::test_util::get_dummy_raw_msg_chan_upgrade_init;
-//     use crate::core::ics04_channel::msgs::chan_upgrade_init::MsgChannelUpgradeTry;
+    use crate::core::ics04_channel::msgs::chan_upgrade_try::test_util::get_dummy_raw_msg_chan_upgrade_try;
+    use crate::core::ics04_channel::msgs::chan_upgrade_try::MsgChannelUpgradeTry;
 
-//     #[test]
-//     fn parse_channel_upgrade_init_msg() {
-//         struct Test {
-//             name: String,
-//             raw: RawMsgChannelUpgradeTry,
-//             want_pass: bool,
-//         }
+    #[test]
+    fn parse_channel_upgrade_init_msg() {
+        struct Test {
+            name: String,
+            raw: RawMsgChannelUpgradeTry,
+            want_pass: bool,
+        }
 
-//         let default_raw_msg = get_dummy_raw_msg_chan_upgrade_init();
+        let default_raw_msg = get_dummy_raw_msg_chan_upgrade_try();
 
-//         let tests: Vec<Test> = vec![
-//             Test {
-//                 name: "Good parameters".to_string(),
-//                 raw: default_raw_msg.clone(),
-//                 want_pass: true,
-//             },
-//             Test {
-//                 name: "Correct port ID".to_string(),
-//                 raw: RawMsgChannelUpgradeTry {
-//                     port_id: "p36".to_string(),
-//                     ..default_raw_msg.clone()
-//                 },
-//                 want_pass: true,
-//             },
-//             Test {
-//                 name: "Port too short".to_string(),
-//                 raw: RawMsgChannelUpgradeTry {
-//                     port_id: "p".to_string(),
-//                     ..default_raw_msg.clone()
-//                 },
-//                 want_pass: false,
-//             },
-//             Test {
-//                 name: "Port too long".to_string(),
-//                 raw: RawMsgChannelUpgradeTry {
-//                     port_id: "abcdefsdfasdfasdfasdfasdfasdfadsfasdgafsgadfasdfasdfasdfsdfasdfaghijklmnopqrstuabcdefsdfasdfasdfasdfasdfasdfadsfasdgafsgadfasdfasdfasdfsdfasdfaghijklmnopqrstu".to_string(),
-//                     ..default_raw_msg.clone()
-//                 },
-//                 want_pass: false,
-//             },
-//             Test {
-//                 name: "Correct channel ID".to_string(),
-//                 raw: RawMsgChannelUpgradeTry {
-//                     channel_id: "channel-2".to_string(),
-//                     ..default_raw_msg.clone()
-//                 },
-//                 want_pass: true,
-//             },
-//             Test {
-//                 name: "Channel name too short".to_string(),
-//                 raw: RawMsgChannelUpgradeTry {
-//                     channel_id: "c".to_string(),
-//                     ..default_raw_msg.clone()
-//                 },
-//                 want_pass: false,
-//             },
-//             Test {
-//                 name: "Channel name too long".to_string(),
-//                 raw: RawMsgChannelUpgradeTry {
-//                     channel_id: "channel-128391283791827398127398791283912837918273981273987912839".to_string(),
-//                     ..default_raw_msg.clone()
-//                 },
-//                 want_pass: false,
-//             },
-//         ]
-//         .into_iter()
-//         .collect();
+        let tests: Vec<Test> = vec![
+            Test {
+                name: "Good parameters".to_string(),
+                raw: default_raw_msg.clone(),
+                want_pass: true,
+            },
+            Test {
+                name: "Correct port ID".to_string(),
+                raw: RawMsgChannelUpgradeTry {
+                    port_id: "p36".to_string(),
+                    ..default_raw_msg.clone()
+                },
+                want_pass: true,
+            },
+            Test {
+                name: "Port too short".to_string(),
+                raw: RawMsgChannelUpgradeTry {
+                    port_id: "p".to_string(),
+                    ..default_raw_msg.clone()
+                },
+                want_pass: false,
+            },
+            Test {
+                name: "Port too long".to_string(),
+                raw: RawMsgChannelUpgradeTry {
+                    port_id: "abcdefsdfasdfasdfasdfasdfasdfadsfasdgafsgadfasdfasdfasdfsdfasdfaghijklmnopqrstuabcdefsdfasdfasdfasdfasdfasdfadsfasdgafsgadfasdfasdfasdfsdfasdfaghijklmnopqrstu".to_string(),
+                    ..default_raw_msg.clone()
+                },
+                want_pass: false,
+            },
+            Test {
+                name: "Correct channel ID".to_string(),
+                raw: RawMsgChannelUpgradeTry {
+                    channel_id: "channel-2".to_string(),
+                    ..default_raw_msg.clone()
+                },
+                want_pass: true,
+            },
+            Test {
+                name: "Channel name too short".to_string(),
+                raw: RawMsgChannelUpgradeTry {
+                    channel_id: "c".to_string(),
+                    ..default_raw_msg.clone()
+                },
+                want_pass: false,
+            },
+            Test {
+                name: "Channel name too long".to_string(),
+                raw: RawMsgChannelUpgradeTry {
+                    channel_id: "channel-128391283791827398127398791283912837918273981273987912839".to_string(),
+                    ..default_raw_msg
+                },
+                want_pass: false,
+            },
+        ]
+        .into_iter()
+        .collect();
 
-//         for test in tests {
-//             let res = MsgChannelUpgradeTry::try_from(test.raw.clone());
+        for test in tests {
+            let res = MsgChannelUpgradeTry::try_from(test.raw.clone());
 
-//             assert_eq!(
-//                 test.want_pass,
-//                 res.is_ok(),
-//                 "MsgChannelUpgradeTry::try_from failed for test {}, \nraw msg {:?} with err {:?}",
-//                 test.name,
-//                 test.raw,
-//                 res.err()
-//             );
-//         }
-//     }
+            assert_eq!(
+                test.want_pass,
+                res.is_ok(),
+                "MsgChannelUpgradeTry::try_from failed for test {}, \nraw msg {:?} with err {:?}",
+                test.name,
+                test.raw,
+                res.err()
+            );
+        }
+    }
 
-//     #[test]
-//     fn to_and_from() {
-//         let raw = get_dummy_raw_msg_chan_upgrade_init();
-//         let msg = MsgChannelUpgradeTry::try_from(raw.clone()).unwrap();
-//         let raw_back = RawMsgChannelUpgradeTry::from(msg.clone());
-//         let msg_back = MsgChannelUpgradeTry::try_from(raw_back.clone()).unwrap();
-//         assert_eq!(raw, raw_back);
-//         assert_eq!(msg, msg_back);
-//     }
-// }
+    #[test]
+    fn to_and_from() {
+        let raw = get_dummy_raw_msg_chan_upgrade_try();
+        let msg = MsgChannelUpgradeTry::try_from(raw.clone()).unwrap();
+        let raw_back = RawMsgChannelUpgradeTry::from(msg.clone());
+        let msg_back = MsgChannelUpgradeTry::try_from(raw_back.clone()).unwrap();
+        assert_eq!(raw, raw_back);
+        assert_eq!(msg, msg_back);
+    }
+}
