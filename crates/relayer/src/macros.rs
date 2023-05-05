84af5e11bb9f2ef9a17c5a7b442b1ddf8f2e0b3c
Luca Joss <43531661+ljoss17@users.noreply.github.com>
2023-05-05 11:59:24 +0200

    Profiling to json
    * Initial version to store profiling in file
    
    * Open profiling file only on start
    
    * Use OnceCell instead of RefCell
    
    * Update tracing from info to trace for profiling
    
    * Revert "Update tracing from info to trace for profiling"
    
    This reverts commit 5c9117a4daa83664a562f0bdbcb135996586c910.

--- crates/relayer/src/macros.rs
+++ crates/relayer/src/macros.rs
@@ -3,16 +3,28 @@ pub mod profiling {
 
     use core::sync::atomic::AtomicUsize;
     use core::sync::atomic::Ordering::Relaxed;
+    use once_cell::sync::OnceCell;
+    use serde_derive::Serialize;
+    use std::fs::File;
+    use std::fs::OpenOptions;
+    use std::path::Path;
 
     std::thread_local! {
         pub static DEPTH: AtomicUsize = AtomicUsize::new(0);
     }
+    static FILE: OnceCell<File> = OnceCell::new();
 
     /// Measure the time between when this value is allocated
     /// and when it is dropped.
     pub struct Timer {
         name: String,
         start: std::time::Instant,
+    }
+
+    #[derive(Debug, Serialize)]
+    struct TimerInfo {
+        name: String,
+        elapsed: u128,
     }
 
     impl Timer {
@@ -37,6 +49,43 @@ pub mod profiling {
             let pad = "   ".repeat(depth - 1);
 
             tracing::info!("{}⏳ {} - elapsed: {}ms", pad, self.name, elapsed);
+            let info = TimerInfo {
+                name: self.name.clone(),
+                elapsed,
+            };
+            to_json_format(info);
+        }
+    }
+
+    pub fn open_or_create_profile_file(file_name: &Path) {
+        let file = OpenOptions::new()
+            .write(true)
+            .append(true)
+            .create(true)
+            .open(file_name)
+            .unwrap();
+
+        match FILE.set(file) {
+            Ok(_) => tracing::trace!("profile file created at: {file_name:#?}"),
+            Err(e) => tracing::error!("profile file was already set with: {:#?}", e.metadata()),
+        }
+    }
+
+    fn to_json_format(info: TimerInfo) {
+        use std::io::prelude::*;
+
+        let json_str = match serde_json::to_string(&info) {
+            Ok(value) => value,
+            Err(_) => format!("{info:?}"), // Fallback to debug printing
+        };
+
+        match FILE.get() {
+            Some(mut f) => {
+                if let Err(e) = writeln!(f, "{}", json_str) {
+                    tracing::error!("Couldn't write to file: {}", e);
+                }
+            }
+            None => tracing::debug!("File for profiling is not set"),
         }
     }
 }
