4c5ad90ffb07e12aa5898f0efddf77ff1729d6a3
Soares Chen <soares.chen@maybevoid.com>
2023-07-05 02:05:38 +0200

    Define CreateClient traits

---
+++ crates/relayer-components/src/chain/traits/client/create.rs
@@ -0,0 +1,42 @@
+use async_trait::async_trait;
+
+use crate::chain::traits::types::ibc::HasIbcChainTypes;
+use crate::core::traits::error::HasErrorType;
+use crate::core::traits::sync::Async;
+use crate::std_prelude::*;
+
+pub trait HasCreateClientOptionsType<Counterparty>: HasIbcChainTypes<Counterparty> {
+    type CreateClientOptions: Async;
+}
+
+pub trait HasCreateClientPayloadType<Counterparty>: HasIbcChainTypes<Counterparty> {
+    type CreateClientPayload: Async;
+}
+
+pub trait HasCreateClientEvent<Counterparty>: HasIbcChainTypes<Counterparty> {
+    type CreateClientEvent: Async;
+
+    fn try_extract_create_client_event(event: Self::Event) -> Option<Self::CreateClientEvent>;
+
+    fn create_client_event_client_id(event: &Self::CreateClientEvent) -> &Self::ClientId;
+}
+
+#[async_trait]
+pub trait CanBuildCreateClientPayload<Counterparty>:
+    HasCreateClientOptionsType<Counterparty> + HasCreateClientPayloadType<Counterparty> + HasErrorType
+{
+    async fn build_create_client_payload(
+        create_client_options: &Self::CreateClientOptions,
+    ) -> Result<Self::CreateClientPayload, Self::Error>;
+}
+
+#[async_trait]
+pub trait CanBuildCreateClientMessage<Counterparty>:
+    HasIbcChainTypes<Counterparty> + HasErrorType
+where
+    Counterparty: HasCreateClientPayloadType<Self>,
+{
+    async fn build_create_client_message(
+        counterparty_payload: Counterparty::CreateClientPayload,
+    ) -> Result<Self::Message, Self::Error>;
+}
