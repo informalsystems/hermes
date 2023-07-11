389ceae0ac6b21793b06bceeedbbf15b42d68e3e
Luca Joss <luca@informal.systems>
2023-07-11 08:21:16 +0200

    Remove serde-with dependency

--- crates/relayer/src/chain/cosmos/types/app_state.rs
+++ crates/relayer/src/chain/cosmos/types/app_state.rs
@@ -1,10 +1,9 @@
 //! Structure used to parse queried Genesis state using
 //! /genesis RPC endpoint.
 
+use serde::Deserialize as SerdeDeserialize;
 use serde_derive::Deserialize;
 use serde_derive::Serialize;
-use serde_with::serde_as;
-use serde_with::DisplayFromStr;
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct GenesisAppState {
@@ -30,9 +29,19 @@ struct ConnectionGenesisConfig {
     params: ConnectionGenesisParams,
 }
 
-#[serde_as]
 #[derive(Debug, Deserialize, Serialize)]
 struct ConnectionGenesisParams {
-    #[serde_as(as = "DisplayFromStr")]
+    #[serde(deserialize_with = "deserialize_max_expected_per_block")]
     max_expected_time_per_block: u64,
+}
+
+fn deserialize_max_expected_per_block<'de, T, D>(de: D) -> Result<T, D::Error>
+where
+    D: serde::Deserializer<'de>,
+    T: std::str::FromStr,
+    <T as std::str::FromStr>::Err: std::fmt::Display,
+{
+    String::deserialize(de)?
+        .parse()
+        .map_err(serde::de::Error::custom)
 }
