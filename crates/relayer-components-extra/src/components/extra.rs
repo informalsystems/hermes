7f6f5d692e89a40a9e99df748a68f215b14bcd5d
Soares Chen <soares.chen@maybevoid.com>
2023-08-08 12:31:37 +0200

    Use components pattern for PacketRelayer

--- crates/relayer-components-extra/src/components/extra.rs
+++ crates/relayer-components-extra/src/components/extra.rs
@@ -1,10 +1,15 @@
 use core::marker::PhantomData;
 
+use crate::relay::impls::packet_relayers::retry::RetryRelayer;
 use crate::std_prelude::*;
 use crate::telemetry::impls::status::ChainStatusTelemetryQuerier;
 use ibc_relayer_components::relay::impls::client::update::BuildUpdateClientMessages;
 use ibc_relayer_components::relay::impls::messages::skip_update_client::SkipUpdateClient;
 use ibc_relayer_components::relay::impls::messages::wait_update_client::WaitUpdateClient;
+use ibc_relayer_components::relay::impls::packet_relayers::general::filter_relayer::FilterRelayer;
+use ibc_relayer_components::relay::impls::packet_relayers::general::full_relay::FullCycleRelayer;
+use ibc_relayer_components::relay::impls::packet_relayers::general::lock::LockPacketRelayer;
+use ibc_relayer_components::relay::impls::packet_relayers::general::log::LoggerRelayer;
 
 pub struct ExtraComponents<BaseComponents>(pub PhantomData<BaseComponents>);
 
@@ -16,4 +21,9 @@ ibc_relayer_components::derive_update_client_message_builder!(
 ibc_relayer_components::derive_chain_status_querier!(
     ExtraComponents<BaseComponents>,
     ChainStatusTelemetryQuerier<BaseComponents>,
+);
+
+ibc_relayer_components::derive_packet_relayer!(
+    ExtraComponents<BaseComponents>,
+    LockPacketRelayer<LoggerRelayer<FilterRelayer<RetryRelayer<FullCycleRelayer>>>>,
 );
--- crates/relayer-components/src/components/default.rs
+++ crates/relayer-components/src/components/default.rs
@@ -3,13 +3,22 @@ use core::marker::PhantomData;
 use crate::relay::impls::client::update::BuildUpdateClientMessages;
 use crate::relay::impls::messages::skip_update_client::SkipUpdateClient;
 use crate::relay::impls::messages::wait_update_client::WaitUpdateClient;
+use crate::relay::impls::packet_relayers::general::filter_relayer::FilterRelayer;
+use crate::relay::impls::packet_relayers::general::full_relay::FullCycleRelayer;
+use crate::relay::impls::packet_relayers::general::lock::LockPacketRelayer;
+use crate::relay::impls::packet_relayers::general::log::LoggerRelayer;
 use crate::std_prelude::*;
 
 pub struct DefaultComponents<BaseComponents>(pub PhantomData<BaseComponents>);
+
+crate::derive_chain_status_querier!(DefaultComponents<BaseComponents>, BaseComponents);
 
 crate::derive_update_client_message_builder!(
     DefaultComponents<BaseComponents>,
     SkipUpdateClient<WaitUpdateClient<BuildUpdateClientMessages>>,
 );
 
-crate::derive_chain_status_querier!(DefaultComponents<BaseComponents>, BaseComponents,);
+crate::derive_packet_relayer!(
+    DefaultComponents<BaseComponents>,
+    LockPacketRelayer<LoggerRelayer<FilterRelayer<FullCycleRelayer>>>,
+);
