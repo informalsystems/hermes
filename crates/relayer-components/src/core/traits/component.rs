29aaefe41354bb1d0276626b4955d52817a7dc16
Soares Chen <soares.chen@maybevoid.com>
2023-08-15 13:15:08 +0200

    Allow delegate_components! to match without trailing comma

--- crates/relayer-components/src/core/traits/component.rs
+++ crates/relayer-components/src/core/traits/component.rs
@@ -23,8 +23,8 @@ macro_rules! delegate_components {
     ( [$(,)?], $target:ident $( < $( $param:ident ),* $(,)? > )?, $forwarded:ty $(,)? ) => {
 
     };
-    ( [$name:ty, $($rest:tt)*], $target:ident $( < $( $param:ident ),* $(,)? > )?, $forwarded:ty $(,)?  ) => {
+    ( [$name:ty $(, $($rest:tt)* )?], $target:ident $( < $( $param:ident ),* $(,)? > )?, $forwarded:ty $(,)?  ) => {
         $crate::delegate_component!($name, $target $( < $( $param ),* > )*, $forwarded);
-        $crate::delegate_components!([ $($rest)* ], $target $( < $( $param ),* > )*, $forwarded);
+        $crate::delegate_components!([ $( $($rest)* )? ], $target $( < $( $param ),* > )*, $forwarded);
     };
 }
