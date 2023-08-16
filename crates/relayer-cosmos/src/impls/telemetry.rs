b0a22e10ccdf67f75146611a3fe34ec1f157d384
Soares Chen <soares.chen@maybevoid.com>
2023-08-16 05:28:01 +0200

    Slightly improve telemetry

--- crates/relayer-cosmos/src/impls/telemetry.rs
+++ crates/relayer-cosmos/src/impls/telemetry.rs
@@ -8,6 +8,7 @@ use crate::types::telemetry::CosmosTelemetry;
 
 impl HasLabel for CosmosTelemetry {
     type Label = KeyValue;
+
     fn new_label(key: &str, value: &str) -> Self::Label {
         KeyValue::new(key.to_string(), value.to_string())
     }
@@ -26,24 +27,30 @@ impl HasMetric<TelemetryCounter> for CosmosTelemetry {
         description: Option<&str>,
         unit: Option<Self::Unit>,
     ) {
-        let mut telemetry_state = self.telemetry_state.lock().unwrap();
-        if let Some(metric) = telemetry_state.counters.get(name) {
+        let mut counters = self.counters.lock().unwrap();
+
+        if let Some(metric) = counters.get(name) {
             metric.add(value, labels);
         } else {
-            let builder = telemetry_state.meter.u64_counter(name);
+            let builder = self.meter.u64_counter(name);
+
             let builder = if let Some(description) = description {
                 builder.with_description(description)
             } else {
                 builder
             };
+
             let builder = if let Some(unit) = unit {
                 builder.with_unit(unit)
             } else {
                 builder
             };
+
             let metric = builder.init();
+
             metric.add(value, labels);
-            telemetry_state.counters.insert(name.to_string(), metric);
+
+            counters.insert(name.to_string(), metric);
         }
     }
 }
@@ -61,26 +68,30 @@ impl HasMetric<TelemetryValueRecorder> for CosmosTelemetry {
         description: Option<&str>,
         unit: Option<Self::Unit>,
     ) {
-        let mut telemetry_state = self.telemetry_state.lock().unwrap();
-        if let Some(metric) = telemetry_state.value_recorders.get(name) {
+        let mut value_recorders = self.value_recorders.lock().unwrap();
+
+        if let Some(metric) = value_recorders.get(name) {
             metric.record(value, labels);
         } else {
-            let builder = telemetry_state.meter.u64_value_recorder(name);
+            let builder = self.meter.u64_value_recorder(name);
+
             let builder = if let Some(description) = description {
                 builder.with_description(description)
             } else {
                 builder
             };
+
             let builder = if let Some(unit) = unit {
                 builder.with_unit(unit)
             } else {
                 builder
             };
+
             let metric = builder.init();
+
             metric.record(value, labels);
-            telemetry_state
-                .value_recorders
-                .insert(name.to_string(), metric);
+
+            value_recorders.insert(name.to_string(), metric);
         }
     }
 }
@@ -98,26 +109,30 @@ impl HasMetric<TelemetryUpDownCounter> for CosmosTelemetry {
         description: Option<&str>,
         unit: Option<Self::Unit>,
     ) {
-        let mut telemetry_state = self.telemetry_state.lock().unwrap();
-        if let Some(metric) = telemetry_state.updown_counters.get(name) {
+        let mut updown_counters = self.updown_counters.lock().unwrap();
+
+        if let Some(metric) = updown_counters.get(name) {
             metric.add(value, labels);
         } else {
-            let builder = telemetry_state.meter.i64_up_down_counter(name);
+            let builder = self.meter.i64_up_down_counter(name);
+
             let builder = if let Some(description) = description {
                 builder.with_description(description)
             } else {
                 builder
             };
+
             let builder = if let Some(unit) = unit {
                 builder.with_unit(unit)
             } else {
                 builder
             };
+
             let metric = builder.init();
+
             metric.add(value, labels);
-            telemetry_state
-                .updown_counters
-                .insert(name.to_string(), metric);
+
+            updown_counters.insert(name.to_string(), metric);
         }
     }
 }
--- crates/relayer-cosmos/src/types/telemetry.rs
+++ crates/relayer-cosmos/src/types/telemetry.rs
@@ -7,33 +7,24 @@ use opentelemetry::{
     metrics::{Counter, Meter, UpDownCounter, ValueRecorder},
 };
 
-pub struct TelemetryState {
-    pub meter: Meter,
-    pub counters: HashMap<String, Counter<u64>>,
-    pub value_recorders: HashMap<String, ValueRecorder<u64>>,
-    pub updown_counters: HashMap<String, UpDownCounter<i64>>,
-}
-
 #[derive(Clone)]
 pub struct CosmosTelemetry {
-    pub telemetry_state: Arc<Mutex<TelemetryState>>,
-}
+    pub meter: Arc<Meter>,
 
-impl CosmosTelemetry {
-    pub fn new(telemetry_state: TelemetryState) -> Self {
-        Self {
-            telemetry_state: Arc::new(Mutex::new(telemetry_state)),
-        }
-    }
+    pub counters: Arc<Mutex<HashMap<String, Counter<u64>>>>,
+
+    pub value_recorders: Arc<Mutex<HashMap<String, ValueRecorder<u64>>>>,
+
+    pub updown_counters: Arc<Mutex<HashMap<String, UpDownCounter<i64>>>>,
 }
 
 impl Default for CosmosTelemetry {
     fn default() -> Self {
-        Self::new(TelemetryState {
-            meter: global::meter("hermes"),
-            counters: HashMap::new(),
-            value_recorders: HashMap::new(),
-            updown_counters: HashMap::new(),
-        })
+        Self {
+            meter: Arc::new(global::meter("hermes")),
+            counters: Arc::new(Mutex::new(HashMap::new())),
+            value_recorders: Arc::new(Mutex::new(HashMap::new())),
+            updown_counters: Arc::new(Mutex::new(HashMap::new())),
+        }
     }
 }
