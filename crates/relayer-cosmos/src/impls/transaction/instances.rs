7bf368994c0e99f9be72df85e1a33519a8e2a3d2
Soares Chen <soares.chen@maybevoid.com>
2023-08-16 16:04:35 +0200

    Add proof for CosmosTxContext implementing CanSendMessages

---
+++ crates/relayer-cosmos/src/impls/transaction/instances.rs
@@ -0,0 +1,21 @@
+use alloc::sync::Arc;
+use async_trait::async_trait;
+use ibc_relayer_components::chain::traits::message_sender::{CanSendMessages, MessageSender};
+use tendermint::abci::Event as AbciEvent;
+
+use crate::contexts::transaction::CosmosTxContext;
+use crate::traits::message::CosmosMessage;
+use crate::types::error::Error;
+
+pub struct CosmosTxInstances;
+
+// Proof that CosmosTxContext implements [`CanSendMessages`].
+#[async_trait]
+impl MessageSender<CosmosTxContext> for CosmosTxInstances {
+    async fn send_messages(
+        tx_context: &CosmosTxContext,
+        messages: Vec<Arc<dyn CosmosMessage>>,
+    ) -> Result<Vec<Vec<Arc<AbciEvent>>>, Error> {
+        tx_context.send_messages(messages).await
+    }
+}
--- crates/relayer-cosmos/src/impls/transaction/mod.rs
+++ crates/relayer-cosmos/src/impls/transaction/mod.rs
@@ -2,6 +2,7 @@ pub mod component;
 pub mod components;
 pub mod error;
 pub mod fields;
+pub mod instances;
 pub mod log;
 pub mod parse;
 pub mod types;
