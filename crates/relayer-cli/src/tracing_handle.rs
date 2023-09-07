d201d52a14ff411d3484c55e65ae7ec8ac2c42cc
Romain Ruetschi <106849+romac@users.noreply.github.com>
2023-09-07 13:24:32 +0200

    Fix for server hanging

--- crates/relayer-cli/src/tracing_handle.rs
+++ crates/relayer-cli/src/tracing_handle.rs
@@ -1,6 +1,6 @@
 use std::io;
 
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader};
 use tokio::net::TcpListener;
 use tokio::net::TcpStream;
 use tracing_subscriber::filter;
@@ -21,14 +21,17 @@ pub async fn spawn_reload_handler<S>(
     let listener = TcpListener::bind(("0.0.0.0", config.port)).await?;
 
     loop {
-        let (mut socket, _) = listener.accept().await?;
+        let (socket, _) = listener.accept().await?;
+        let mut reader = BufReader::new(socket);
 
         let mut buffer = String::new();
-        socket.read_to_string(&mut buffer).await?;
+        reader.read_line(&mut buffer).await?;
+
+        let mut socket = reader.into_inner();
 
         let cmd = buffer.trim_end();
         let _ = reload_handle.reload(filter::EnvFilter::new(cmd));
-        let success_msg = format!("Successfully set tracing filter to `{}`\n", cmd);
+        let success_msg = format!("Successfully set tracing filter to `{}`", cmd);
         socket.write_all(success_msg.as_bytes()).await?;
     }
 }
@@ -37,6 +40,7 @@ pub async fn send_command(cmd: &str, port: u16) -> io::Result<String> {
     let mut stream = TcpStream::connect(("127.0.0.1", port)).await?;
 
     stream.write_all(cmd.as_bytes()).await?;
+    stream.write_all(b"\n").await?;
 
     let mut buffer = String::new();
     stream.read_to_string(&mut buffer).await?;
