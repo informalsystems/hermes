1a2c24c11ed1b009412465612d5daf13faa156f4
Farhad Shabani <farhad.shabani@gmail.com>
2023-09-12 16:59:41 -0700

    fix: test_update_client should query dst_chain to see client_state updated

--- crates/relayer-cosmos-mock/src/tests/ics02_client.rs
+++ crates/relayer-cosmos-mock/src/tests/ics02_client.rs
@@ -1,7 +1,6 @@
 use crate::contexts::basecoin::MockBasecoin;
 use crate::contexts::chain::MockCosmosContext;
 use crate::tests::init::binary_setup;
-use crate::traits::endpoint::BasecoinEndpoint;
 use crate::types::error::Error;
 
 use basecoin_store::impls::InMemoryStore;
@@ -9,6 +8,8 @@ use ibc::core::ics24_host::identifier::ClientId;
 use ibc::core::ValidationContext;
 use ibc_relayer_components::chain::traits::client::client_state::CanQueryClientState;
 use ibc_relayer_components::chain::traits::client::create::CanBuildCreateClientPayload;
+use ibc_relayer_components::chain::traits::components::chain_status_querier::CanQueryChainStatus;
+use ibc_relayer_components::chain::traits::components::message_sender::CanSendMessages;
 use ibc_relayer_components::relay::traits::components::update_client_message_builder::CanBuildUpdateClientMessage;
 use ibc_relayer_components::relay::traits::target::DestinationTarget;
 use ibc_relayer_components::runtime::traits::sleep::CanSleep;
@@ -25,7 +26,8 @@ async fn test_create_client() -> Result<(), Error> {
 
     relayer
         .dst_chain()
-        .submit_messages(vec![msg_create_client])?;
+        .send_messages(vec![msg_create_client])
+        .await?;
 
     assert!(relayer
         .dst_chain()
@@ -48,25 +50,26 @@ async fn test_update_client() -> Result<(), Error> {
 
     relayer
         .dst_chain()
-        .submit_messages(vec![msg_create_client])?;
+        .send_messages(vec![msg_create_client])
+        .await?;
 
     relayer
         .runtime()
         .sleep(tokio::time::Duration::from_millis(200))
         .await;
 
-    let src_current_height = relayer.src_chain().get_current_height();
+    let src_current_height = relayer.src_chain().query_chain_status().await?.height;
 
     let msg_update_client = relayer
         .build_update_client_messages(DestinationTarget, &src_current_height)
         .await?;
 
-    relayer.dst_chain().submit_messages(msg_update_client)?;
+    relayer.dst_chain().send_messages(msg_update_client).await?;
 
     let latest_client_state =
         <MockCosmosContext<MockBasecoin<InMemoryStore>> as CanQueryClientState<
             MockCosmosContext<MockBasecoin<InMemoryStore>>,
-        >>::query_client_state(relayer.src_chain(), &ClientId::default())
+        >>::query_client_state(relayer.dst_chain(), &ClientId::default())
         .await?;
 
     assert_eq!(latest_client_state.latest_height, src_current_height);
