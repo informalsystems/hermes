8241fd9a531d46d73440e8169c265a9b240bc73b
Romain Ruetschi <106849+romac@users.noreply.github.com>
2023-09-13 12:46:14 +0200

    Update ICS misbehaviour test to use a different wallet for the `evidence` command

---
+++ ci/double-sign/double_sign_test.sh
@@ -0,0 +1,87 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# --- Variables ---
+
+HERMES="cargo run --bin hermes -q --"
+HERMES_LOG="hermes.log"
+DEV_ENV="../../scripts/dev-env"
+CHAIN_PREFIX="127.0.0.1"
+
+# --- Helpers ---
+
+warn() {
+    echo "WARNING: $*"
+}
+
+info() {
+    echo "❯ $*"
+}
+
+# --- Main ---
+
+info "Starting chains"
+"$DEV_ENV" config.toml ibc-0 ibc-1 --non-interactive
+
+info "Waiting for the chains to produce a few blocks..."
+sleep 10
+
+info "Spawning duplicate chain ibc-1"
+cp -r data/ibc-1 data/sybil
+echo '{"height": "0","round": 0,"step": 0,"signature":"","signbytes":""}' > data/sybil/data/priv_validator_state.json
+echo '{"priv_key":{"type":"tendermint/PrivKeyEd25519","value":"tj55by/yYwruSz4NxsOG9y9k2WrPvKLXKQdz/9jL9Uptmi647OYpcisjwf92TyA+wCUYVDOgW7D53Q+638l9/w=="}}' > data/sybil/config/node_key.json
+
+ARGS="--address tcp://$CHAIN_PREFIX:29655 \
+    --rpc.laddr tcp://$CHAIN_PREFIX:29658 \
+    --grpc.address $CHAIN_PREFIX:29091 \
+    --p2p.laddr tcp://$CHAIN_PREFIX:29656 \
+    --log_level info \
+    --grpc-web.enable=false"
+
+gaiad $ARGS --home data/sybil start &> data/sybil/logs &
+
+# info "Starting Hermes for ibc-0 and ibc-1"
+# $HERMES --config config.toml start > "$HERMES_LOG" 2>&1 &
+# HERMES_PID=$!
+#
+# info "Waiting for Hermes to start"
+# sleep 10
+#
+# info "Update client on ibc-0 against the forked chain ibc-1-f"
+# $HERMES --config config_fork.toml update client --client 07-tendermint-0 --host-chain ibc-0
+#
+# info "Wait for chain ibc-1 to stop..."
+# sleep 5
+#
+# info "Killing Hermes"
+# kill -9 "$HERMES_PID"
+#
+# echo ""
+# info "--------------------------------------------------"
+# info "Hermes log:"
+# info "--------------------------------------------------"
+# cat "$HERMES_LOG"
+# info "--------------------------------------------------"
+# echo ""
+#
+# if grep -q "Evidence succesfully submitted" "$HERMES_LOG"; then
+#     warn "Misbehaviour detection failed!"
+#     exit 1
+# else
+#     info "Misbehaviour detected and submitted successfully!"
+# fi
+#
+# STOPPED_HEIGHT="$(curl -s http://localhost:$IBC_1_RPC_PORT/status | jq -r .result.sync_info.latest_block_height)"
+#
+# info "Chain ibc-1 stopped at height $STOPPED_HEIGHT"
+#
+# info "Fetch evidence from block $STOPPED_HEIGHT on ibc-1"
+# EVIDENCE="$(curl -s "http://localhost:$IBC_1_RPC_PORT/block?height=$STOPPED_HEIGHT" | jq .result.block.evidence)"
+#
+# info "Found evidence at height $STOPPED_HEIGHT: $EVIDENCE"
+#
+# if [ "$EVIDENCE" = "null" ]; then
+#     warn "No evidence found in the latest block on ibc-1"
+#     exit 1
+# fi
--- ci/misbehaviour-ics/misbehaviour_test.sh
+++ ci/misbehaviour-ics/misbehaviour_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -eu
+set -eux
 
 diag() {
   echo ">>
@@ -28,8 +28,9 @@ PROV_NODE_DIR=${HOME_DIR}/provider-${MONIKER}
 CONS_NODE_DIR=${HOME_DIR}/consumer-${MONIKER}
 CONS_FORK_NODE_DIR=${HOME_DIR}/consumer-fork-${MONIKER}
 
-# Coordinator key
-PROV_KEY=${MONIKER}-key
+# Coordinator keys
+PROV_KEY1=${MONIKER}-key1
+PROV_KEY2=${MONIKER}-key2
 
 # Clean start
 pkill -f interchain-security-pd &> /dev/null || true
@@ -49,18 +50,22 @@ jq ".app_state.gov.voting_params.voting_period = \"3s\" | .app_state.staking.par
 
 sleep 1
 
-# Create account keypair
-interchain-security-pd keys add $PROV_KEY --home ${PROV_NODE_DIR} --keyring-backend test --output json > ${PROV_NODE_DIR}/${PROV_KEY}.json 2>&1
+# Create account keypairs
+interchain-security-pd keys add $PROV_KEY1 --home ${PROV_NODE_DIR} --keyring-backend test --output json > ${PROV_NODE_DIR}/${PROV_KEY1}.json 2>&1
+interchain-security-pd keys add $PROV_KEY2 --home ${PROV_NODE_DIR} --keyring-backend test --output json > ${PROV_NODE_DIR}/${PROV_KEY2}.json 2>&1
 sleep 1
 
-# Add stake to user
-PROV_ACCOUNT_ADDR=$(jq -r '.address' ${PROV_NODE_DIR}/${PROV_KEY}.json)
-interchain-security-pd add-genesis-account $PROV_ACCOUNT_ADDR $USER_COINS --home ${PROV_NODE_DIR} --keyring-backend test
+# Add stake to users
+PROV_ACCOUNT_ADDR1=$(jq -r '.address' ${PROV_NODE_DIR}/${PROV_KEY1}.json)
+interchain-security-pd add-genesis-account $PROV_ACCOUNT_ADDR1 $USER_COINS --home ${PROV_NODE_DIR} --keyring-backend test
+PROV_ACCOUNT_ADDR2=$(jq -r '.address' ${PROV_NODE_DIR}/${PROV_KEY2}.json)
+interchain-security-pd add-genesis-account $PROV_ACCOUNT_ADDR2 $USER_COINS --home ${PROV_NODE_DIR} --keyring-backend test
 sleep 1
 
 
 # Stake 1/1000 user's coins
-interchain-security-pd gentx $PROV_KEY $STAKE --chain-id provider --home ${PROV_NODE_DIR} --keyring-backend test --moniker $MONIKER
+interchain-security-pd gentx $PROV_KEY1 $STAKE --chain-id provider --home ${PROV_NODE_DIR} --keyring-backend test --moniker $MONIKER
+# interchain-security-pd gentx $PROV_KEY2 $STAKE --chain-id provider --home ${PROV_NODE_DIR} --keyring-backend test --moniker $MONIKER
 sleep 1
 
 interchain-security-pd collect-gentxs --home ${PROV_NODE_DIR} --gentx-dir ${PROV_NODE_DIR}/config/gentx/
@@ -104,30 +109,35 @@ tee ${PROV_NODE_DIR}/consumer-proposal.json<<EOF
 }
 EOF
 
-interchain-security-pd keys show $PROV_KEY --keyring-backend test --home ${PROV_NODE_DIR}
+interchain-security-pd keys show $PROV_KEY1 --keyring-backend test --home ${PROV_NODE_DIR}
+interchain-security-pd keys show $PROV_KEY2 --keyring-backend test --home ${PROV_NODE_DIR}
 
 # Submit consumer chain proposal
-interchain-security-pd tx gov submit-proposal consumer-addition ${PROV_NODE_DIR}/consumer-proposal.json --chain-id provider --from $PROV_KEY --home ${PROV_NODE_DIR} --node tcp://${NODE_IP}:26658  --keyring-backend test -b block -y
+interchain-security-pd tx gov submit-proposal consumer-addition ${PROV_NODE_DIR}/consumer-proposal.json --chain-id provider --from $PROV_KEY1 --home ${PROV_NODE_DIR} --node tcp://${NODE_IP}:26658  --keyring-backend test -b block -y
 
 sleep 1
 
 # Vote yes to proposal
-interchain-security-pd tx gov vote 1 yes --from $PROV_KEY --chain-id provider --home ${PROV_NODE_DIR} -b block -y --keyring-backend test
+interchain-security-pd tx gov vote 1 yes --from $PROV_KEY1 --chain-id provider --home ${PROV_NODE_DIR} -b block -y --keyring-backend test
+# interchain-security-pd tx gov vote 1 yes --from $PROV_KEY2 --chain-id provider --home ${PROV_NODE_DIR} -b block -y --keyring-backend test
 
 sleep 3
 
 ## CONSUMER CHAIN ##
 
 # Build genesis file and node directory structure
-interchain-security-cd init $MONIKER --chain-id consumer  --home  ${CONS_NODE_DIR}
+interchain-security-cd init $MONIKER --chain-id consumer --home  ${CONS_NODE_DIR}
 sleep 1
 
-# Create user account keypair
-interchain-security-cd keys add $PROV_KEY --home  ${CONS_NODE_DIR} --keyring-backend test --output json > ${CONS_NODE_DIR}/${PROV_KEY}.json 2>&1
+# Create user account keypairs
+interchain-security-cd keys add $PROV_KEY1 --home ${CONS_NODE_DIR} --keyring-backend test --output json > ${CONS_NODE_DIR}/${PROV_KEY1}.json 2>&1
+interchain-security-cd keys add $PROV_KEY2 --home ${CONS_NODE_DIR} --keyring-backend test --output json > ${CONS_NODE_DIR}/${PROV_KEY2}.json 2>&1
 
-# Add stake to user account
-CONS_ACCOUNT_ADDR=$(jq -r '.address' ${CONS_NODE_DIR}/${PROV_KEY}.json)
-interchain-security-cd add-genesis-account $CONS_ACCOUNT_ADDR 1000000000stake --home ${CONS_NODE_DIR}
+# Add stake to user accounts
+CONS_ACCOUNT_ADDR1=$(jq -r '.address' ${CONS_NODE_DIR}/${PROV_KEY1}.json)
+interchain-security-cd add-genesis-account $CONS_ACCOUNT_ADDR1 1000000000stake --home ${CONS_NODE_DIR}
+CONS_ACCOUNT_ADDR2=$(jq -r '.address' ${CONS_NODE_DIR}/${PROV_KEY2}.json)
+interchain-security-cd add-genesis-account $CONS_ACCOUNT_ADDR2 1000000000stake --home ${CONS_NODE_DIR}
 
 # Add consumer genesis states to genesis file
 interchain-security-pd query provider consumer-genesis consumer --home ${PROV_NODE_DIR} -o json > consumer_gen.json
@@ -146,7 +156,7 @@ cp ${PROV_NODE_DIR}/config/node_key.json ${CONS_NODE_DIR}/config/node_key.json
 sed -i -r "/node =/ s/= .*/= \"tcp:\/\/${NODE_IP}:26648\"/" ${CONS_NODE_DIR}/config/client.toml
 
 # Start gaia
-interchain-security-cd start --home  ${CONS_NODE_DIR} \
+interchain-security-cd start --home ${CONS_NODE_DIR} \
         --rpc.laddr tcp://${NODE_IP}:26648 \
         --grpc.address ${NODE_IP}:9081 \
         --address tcp://${NODE_IP}:26645 \
@@ -227,9 +237,10 @@ $HERMES --config $HOME_DIR/config.toml keys delete --chain consumer --all
 $HERMES --config $HOME_DIR/config.toml keys delete --chain provider --all
 
 # Restore keys to hermes relayer
-$HERMES --config $HOME_DIR/config.toml keys add --key-file  ${CONS_NODE_DIR}/${PROV_KEY}.json --chain consumer
-$HERMES --config $HOME_DIR/config.toml keys add --key-file  ${PROV_NODE_DIR}/${PROV_KEY}.json --chain provider
-
+$HERMES --config $HOME_DIR/config.toml keys add --key-name relayer  --key-file ${CONS_NODE_DIR}/${PROV_KEY1}.json --chain consumer
+$HERMES --config $HOME_DIR/config.toml keys add --key-name evidence --key-file ${CONS_NODE_DIR}/${PROV_KEY2}.json --chain consumer
+$HERMES --config $HOME_DIR/config.toml keys add --key-name relayer  --key-file ${PROV_NODE_DIR}/${PROV_KEY1}.json --chain provider
+$HERMES --config $HOME_DIR/config.toml keys add --key-name evidence --key-file ${PROV_NODE_DIR}/${PROV_KEY2}.json --chain provider
 
 sleep 5
 
@@ -255,16 +266,25 @@ $HERMES --config $HOME_DIR/config.toml start &> $HOME_DIR/hermes-logs &
 interchain-security-pd q tendermint-validator-set --home ${PROV_NODE_DIR}
 interchain-security-cd q tendermint-validator-set --home ${CONS_NODE_DIR}
 
-DELEGATIONS=$(interchain-security-pd q staking delegations $PROV_ACCOUNT_ADDR --home ${PROV_NODE_DIR} -o json)
+DELEGATIONS1=$(interchain-security-pd q staking delegations $PROV_ACCOUNT_ADDR1 --home ${PROV_NODE_DIR} -o json)
+OPERATOR_ADDR1=$(echo $DELEGATIONS1 | jq -r '.delegation_responses[0].delegation.validator_address')
 
-OPERATOR_ADDR=$(echo $DELEGATIONS | jq -r '.delegation_responses[0].delegation.validator_address')
+# DELEGATIONS2=$(interchain-security-pd q staking delegations $PROV_ACCOUNT_ADDR2 --home ${PROV_NODE_DIR} -o json)
+# OPERATOR_ADDR2=$(echo $DELEGATIONS2 | jq -r '.delegation_responses[0].delegation.validator_address')
 
-interchain-security-pd tx staking delegate $OPERATOR_ADDR 1000000stake \
-       	--from $PROV_KEY \
+interchain-security-pd tx staking delegate $OPERATOR_ADDR1 1000000stake \
+       	--from $PROV_KEY1 \
        	--keyring-backend test \
        	--home ${PROV_NODE_DIR} \
        	--chain-id provider \
 	-y -b block
+
+# interchain-security-pd tx staking delegate $OPERATOR_ADDR2 1000000stake \
+#        	--from $PROV_KEY2 \
+#        	--keyring-backend test \
+#        	--home ${PROV_NODE_DIR} \
+#        	--chain-id provider \
+# 	-y -b block
 
 sleep 13
 
@@ -351,7 +371,7 @@ cp -r ${CONS_NODE_DIR} ${CONS_FORK_NODE_DIR}
 sed -i -r "/node =/ s/= .*/= \"tcp:\/\/${NODE_IP}:26638\"/" ${CONS_FORK_NODE_DIR}/config/client.toml
 
 # Start gaia
-interchain-security-cd start --home  ${CONS_FORK_NODE_DIR} \
+interchain-security-cd start --home ${CONS_FORK_NODE_DIR} \
         --rpc.laddr tcp://${NODE_IP}:26638 \
         --grpc.address ${NODE_IP}:9071 \
         --address tcp://${NODE_IP}:26635 \
