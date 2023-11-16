341f356a880e367d8f147152a21fd9a159c9ef40
Luca Joss <luca@informal.systems>
2023-11-16 09:26:26 +0100

    Improve assertion for client upgrade tests

--- tools/integration-test/src/tests/client_upgrade.rs
+++ tools/integration-test/src/tests/client_upgrade.rs
@@ -15,6 +15,10 @@
 use http::Uri;
 use std::str::FromStr;
 
+use ibc_relayer::chain::requests::IncludeProof;
+use ibc_relayer::chain::requests::QueryClientStateRequest;
+use ibc_relayer::chain::requests::QueryHeight;
+use ibc_relayer::client_state::AnyClientState;
 use ibc_relayer::upgrade_chain::{build_and_send_ibc_upgrade_proposal, UpgradePlanOptions};
 use ibc_relayer_types::core::ics02_client::height::Height;
 use ibc_test_framework::chain::config::{set_max_deposit_period, set_voting_period};
@@ -70,27 +74,13 @@ impl BinaryChainTest for ClientUpgradeTest {
         _relayer: ibc_test_framework::prelude::RelayerDriver,
         chains: ibc_test_framework::prelude::ConnectedChains<ChainA, ChainB>,
     ) -> Result<(), ibc_test_framework::prelude::Error> {
+        let upgraded_chain_id = ChainId::new("upgradedibc".to_owned(), 1);
         let fee_denom_a: MonoTagged<ChainA, Denom> =
             MonoTagged::new(Denom::base(&config.native_tokens[0]));
         let foreign_clients = chains.clone().foreign_clients;
 
-        let upgraded_chain_id = chains.chain_id_a().0.clone();
-
-        let src_client_id = foreign_clients.client_id_b().0.clone();
-
-        let gov_account = chains.node_a.chain_driver().query_auth_module("gov")?;
-
         // Create and send an chain upgrade proposal
-        let opts = UpgradePlanOptions {
-            src_client_id,
-            amount: 10000000u64,
-            denom: fee_denom_a.to_string(),
-            height_offset: DELTA_HEIGHT,
-            upgraded_chain_id,
-            upgraded_unbonding_period: None,
-            upgrade_plan_name: "plan".to_string(),
-            gov_account,
-        };
+        let opts = create_upgrade_plan(config, &chains, &upgraded_chain_id)?;
 
         build_and_send_ibc_upgrade_proposal(
             chains.handle_a().clone(),
@@ -142,11 +132,23 @@ impl BinaryChainTest for ClientUpgradeTest {
         std::thread::sleep(WAIT_CHAIN_UPGRADE);
 
         // Trigger the client upgrade
-        let outcome = foreign_clients.client_a_to_b.upgrade(client_upgrade_height);
+        // The error is ignored as the client state will be asserted afterwards.
+        let _ = foreign_clients.client_a_to_b.upgrade(client_upgrade_height);
 
-        assert!(outcome.is_ok(), "{outcome:?}");
+        let (state, _) = chains.handle_b().query_client_state(
+            QueryClientStateRequest {
+                client_id: foreign_clients.client_a_to_b.id().clone(),
+                height: QueryHeight::Latest,
+            },
+            IncludeProof::No,
+        )?;
 
-        Ok(())
+        match state {
+            AnyClientState::Tendermint(client_state) => {
+                assert_eq!(client_state.chain_id, upgraded_chain_id);
+                Ok(())
+            }
+        }
     }
 }
 
@@ -167,7 +169,7 @@ impl BinaryChainTest for InvalidClientUpgradeTest {
         chains: ibc_test_framework::prelude::ConnectedChains<ChainA, ChainB>,
     ) -> Result<(), ibc_test_framework::prelude::Error> {
         let arbitrary_height = 5u64;
-        let foreign_clients = chains.foreign_clients;
+        let foreign_clients = &chains.foreign_clients;
 
         // Wait for the chain to reach a given height
         let client_upgrade_height = Height::new(
@@ -180,11 +182,23 @@ impl BinaryChainTest for InvalidClientUpgradeTest {
         std::thread::sleep(Duration::from_secs(2));
 
         // Trigger the client upgrade
-        let outcome = foreign_clients.client_a_to_b.upgrade(client_upgrade_height);
+        // The error is ignored as the client state will be asserted afterwards.
+        let _ = foreign_clients.client_a_to_b.upgrade(client_upgrade_height);
 
-        assert!(outcome.is_err(), "{outcome:?}");
+        let (state, _) = chains.handle_b().query_client_state(
+            QueryClientStateRequest {
+                client_id: foreign_clients.client_a_to_b.id().clone(),
+                height: QueryHeight::Latest,
+            },
+            IncludeProof::No,
+        )?;
 
-        Ok(())
+        match state {
+            AnyClientState::Tendermint(client_state) => {
+                assert_eq!(client_state.chain_id, chains.handle_a().id());
+                Ok(())
+            }
+        }
     }
 }
 
@@ -200,27 +214,13 @@ impl BinaryChainTest for HeightTooHighClientUpgradeTest {
         _relayer: ibc_test_framework::prelude::RelayerDriver,
         chains: ibc_test_framework::prelude::ConnectedChains<ChainA, ChainB>,
     ) -> Result<(), ibc_test_framework::prelude::Error> {
+        let upgraded_chain_id = ChainId::new("upgradedibc".to_owned(), 1);
         let fee_denom_a: MonoTagged<ChainA, Denom> =
             MonoTagged::new(Denom::base(&config.native_tokens[0]));
         let foreign_clients = chains.clone().foreign_clients;
 
-        let upgraded_chain_id = chains.chain_id_a().0.clone();
-
-        let src_client_id = foreign_clients.client_id_b().0.clone();
-
-        let gov_account = chains.node_a.chain_driver().query_auth_module("gov")?;
-
         // Create and send an chain upgrade proposal
-        let opts = UpgradePlanOptions {
-            src_client_id,
-            amount: 10000000u64,
-            denom: fee_denom_a.to_string(),
-            height_offset: DELTA_HEIGHT,
-            upgraded_chain_id,
-            upgraded_unbonding_period: None,
-            upgrade_plan_name: "plan".to_string(),
-            gov_account,
-        };
+        let opts = create_upgrade_plan(config, &chains, &upgraded_chain_id)?;
 
         build_and_send_ibc_upgrade_proposal(
             chains.handle_a().clone(),
@@ -275,13 +275,25 @@ impl BinaryChainTest for HeightTooHighClientUpgradeTest {
         std::thread::sleep(WAIT_CHAIN_UPGRADE);
 
         // Trigger the client upgrade using client_upgrade_height + 1.
-        let outcome = foreign_clients
+        // The error is ignored as the client state will be asserted afterwards.
+        let _ = foreign_clients
             .client_a_to_b
             .upgrade(client_upgrade_height.increment());
 
-        assert!(outcome.is_err(), "{outcome:?}");
+        let (state, _) = chains.handle_b().query_client_state(
+            QueryClientStateRequest {
+                client_id: foreign_clients.client_a_to_b.id().clone(),
+                height: QueryHeight::Latest,
+            },
+            IncludeProof::No,
+        )?;
 
-        Ok(())
+        match state {
+            AnyClientState::Tendermint(client_state) => {
+                assert_eq!(client_state.chain_id, chains.handle_a().id());
+                Ok(())
+            }
+        }
     }
 }
 
@@ -297,27 +309,12 @@ impl BinaryChainTest for HeightTooLowClientUpgradeTest {
         _relayer: ibc_test_framework::prelude::RelayerDriver,
         chains: ibc_test_framework::prelude::ConnectedChains<ChainA, ChainB>,
     ) -> Result<(), ibc_test_framework::prelude::Error> {
+        let upgraded_chain_id = ChainId::new("upgradedibc".to_owned(), 1);
         let fee_denom_a: MonoTagged<ChainA, Denom> =
             MonoTagged::new(Denom::base(&config.native_tokens[0]));
         let foreign_clients = chains.clone().foreign_clients;
 
-        let upgraded_chain_id = chains.chain_id_a().0.clone();
-
-        let src_client_id = foreign_clients.client_id_b().0.clone();
-
-        let gov_account = chains.node_a.chain_driver().query_auth_module("gov")?;
-
-        // Create and send an chain upgrade proposal
-        let opts = UpgradePlanOptions {
-            src_client_id,
-            amount: 10000000u64,
-            denom: fee_denom_a.to_string(),
-            height_offset: DELTA_HEIGHT,
-            upgraded_chain_id,
-            upgraded_unbonding_period: None,
-            upgrade_plan_name: "plan".to_string(),
-            gov_account,
-        };
+        let opts = create_upgrade_plan(config, &chains, &upgraded_chain_id)?;
 
         build_and_send_ibc_upgrade_proposal(
             chains.handle_a().clone(),
@@ -374,16 +371,53 @@ impl BinaryChainTest for HeightTooLowClientUpgradeTest {
         std::thread::sleep(WAIT_CHAIN_UPGRADE);
 
         // Trigger the client upgrade using client_upgrade_height - 1.
-        let outcome = foreign_clients.client_a_to_b.upgrade(
+        // The error is ignored as the client state will be asserted afterwards.
+        let _ = foreign_clients.client_a_to_b.upgrade(
             client_upgrade_height
                 .decrement()
                 .map_err(handle_generic_error)?,
         );
 
-        assert!(outcome.is_err(), "{outcome:?}");
+        let (state, _) = chains.handle_b().query_client_state(
+            QueryClientStateRequest {
+                client_id: foreign_clients.client_a_to_b.id().clone(),
+                height: QueryHeight::Latest,
+            },
+            IncludeProof::No,
+        )?;
 
-        Ok(())
+        match state {
+            AnyClientState::Tendermint(client_state) => {
+                assert_eq!(client_state.chain_id, chains.handle_a().id());
+                Ok(())
+            }
+        }
     }
+}
+
+fn create_upgrade_plan<ChainA: ChainHandle, ChainB: ChainHandle>(
+    config: &ibc_test_framework::prelude::TestConfig,
+    chains: &ibc_test_framework::prelude::ConnectedChains<ChainA, ChainB>,
+    upgraded_chain_id: &ChainId,
+) -> Result<UpgradePlanOptions, Error> {
+    let fee_denom_a: MonoTagged<ChainA, Denom> =
+        MonoTagged::new(Denom::base(&config.native_tokens[0]));
+    let foreign_clients = chains.clone().foreign_clients;
+
+    let src_client_id = foreign_clients.client_id_b().0.clone();
+
+    let gov_account = chains.node_a.chain_driver().query_auth_module("gov")?;
+    // Create and send an chain upgrade proposal
+    Ok(UpgradePlanOptions {
+        src_client_id,
+        amount: 10000000u64,
+        denom: fee_denom_a.to_string(),
+        height_offset: DELTA_HEIGHT,
+        upgraded_chain_id: upgraded_chain_id.clone(),
+        upgraded_unbonding_period: None,
+        upgrade_plan_name: "plan".to_string(),
+        gov_account,
+    })
 }
 
 impl HasOverrides for ClientUpgradeTest {
