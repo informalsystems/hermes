bf6cda65973c2f67c977f96af05e271eda4d4f2b
Luca Joss <luca@informal.systems>
2023-11-24 14:49:11 +0100

    Update guide with new metric

--- crates/relayer/src/foreign_client.rs
+++ crates/relayer/src/foreign_client.rs
@@ -1146,7 +1146,6 @@ impl<DstChain: ChainHandle, SrcChain: ChainHandle> ForeignClient<DstChain, SrcCh
 
             // If the client already stores a consensus state for the target height,
             // there is no need to update the client
-
             telemetry!(
                 client_updates_skipped,
                 &self.src_chain.id(),
