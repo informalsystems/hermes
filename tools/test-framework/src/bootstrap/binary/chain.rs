3450b7f83944ec09fbaf44f37d15916d5c49be86
Luca Joss <luca@informal.systems>
2023-12-12 17:24:57 +0100

    Add wait before spawning chain handles

--- tools/test-framework/src/bootstrap/binary/chain.rs
+++ tools/test-framework/src/bootstrap/binary/chain.rs
@@ -67,6 +67,9 @@ pub fn bootstrap_chains_with_full_nodes(
 
     let registry = new_registry(config.clone());
 
+    // Wait before spawning the chain handle
+    std::thread::sleep(Duration::from_secs(10));
+
     // Pass in unique closure expressions `||{}` as the first argument so that
     // the returned chains are considered different types by Rust.
     // See [`spawn_chain_handle`] for more details.
