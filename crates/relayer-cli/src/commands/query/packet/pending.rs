0afbb11f310f447566e59c941756a17303f8dd74
Anca Zamfir <ancazamfir@users.noreply.github.com>
2024-01-12 10:23:47 +0100

    Add list of sequence numbers to clear pending CLI (#3751)
    * Add list of sequence numbers to clear pending CLI
    
    * With sequences option only clear the packets from the specified chain and not counterparty
    
    * Fix clippy, add sequence test
    
    * Update guide template
    
    * Allow specifying sequences to clear as a list of ranges (#3756)
    
    * Allow specifying sequences to clear as a list of ranges
    
    * Make the ranges inclusive
    
    * Update guide templates
    
    * Add packet-sequences to the packet tx CLIs.
    
    * Align and wording
    
    * Update guide templates
    
    * Improve output of `query packet pending` command
    
    * Add integration test for clearing packets by sequence number ranges
    
    * Add changelog entry
    
    ---------
    
    Co-authored-by: Romain Ruetschi <romain@informal.systems>

--- crates/relayer-cli/src/commands/query/packet/pending.rs
+++ crates/relayer-cli/src/commands/query/packet/pending.rs
@@ -1,3 +1,5 @@
+use core::fmt;
+
 use abscissa_core::clap::Parser;
 use abscissa_core::{Command, Runnable};
 use serde::Serialize;
@@ -5,7 +7,7 @@ use serde::Serialize;
 use ibc_relayer::chain::counterparty::{
     channel_on_destination, pending_packet_summary, PendingPackets,
 };
-use ibc_relayer::chain::handle::BaseChainHandle;
+use ibc_relayer::chain::handle::{BaseChainHandle, ChainHandle};
 use ibc_relayer_types::core::ics24_host::identifier::{ChainId, ChannelId, PortId};
 
 use crate::cli_utils::spawn_chain_counterparty;
@@ -19,8 +21,15 @@ use super::util::CollatedPendingPackets;
 /// at both ends of a channel.
 #[derive(Debug, Serialize)]
 struct Summary<P> {
+    /// Source chain
+    src_chain: ChainId,
+
+    /// Destination chain
+    dst_chain: ChainId,
+
     /// The packets sent on the source chain as identified by the command.
     src: P,
+
     /// The packets sent on the counterparty chain.
     dst: P,
 }
@@ -28,9 +37,44 @@ struct Summary<P> {
 impl Summary<PendingPackets> {
     fn collate(self) -> Summary<CollatedPendingPackets> {
         Summary {
+            src_chain: self.src_chain,
+            dst_chain: self.dst_chain,
+
             src: CollatedPendingPackets::new(self.src),
             dst: CollatedPendingPackets::new(self.dst),
         }
+    }
+}
+
+impl fmt::Display for Summary<CollatedPendingPackets> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "Summary of pending packets:")?;
+
+        writeln!(f, "Packets pending on source chain ({}):", self.src_chain)?;
+        writeln!(f, "  Unreceived packets:")?;
+        for seq in &self.src.unreceived_packets {
+            writeln!(f, "    {}", seq)?;
+        }
+        writeln!(f, "  Unreceived acks:")?;
+        for seq in &self.src.unreceived_acks {
+            writeln!(f, "    {}", seq)?;
+        }
+
+        writeln!(
+            f,
+            "Packets pending on destination chain ({}):",
+            self.dst_chain
+        )?;
+        writeln!(f, "  Unreceived packets:")?;
+        for seq in &self.dst.unreceived_packets {
+            writeln!(f, "    {}", seq)?;
+        }
+        writeln!(f, "  Unreceived acks:")?;
+        for seq in &self.dst.unreceived_acks {
+            writeln!(f, "    {}", seq)?;
+        }
+
+        Ok(())
     }
 }
 
@@ -102,6 +146,8 @@ impl QueryPendingPacketsCmd {
             .map_err(Error::supervisor)?;
 
         Ok(Summary {
+            src_chain: chains.src.id(),
+            dst_chain: chains.dst.id(),
             src: src_summary,
             dst: dst_summary,
         })
@@ -114,7 +160,7 @@ impl Runnable for QueryPendingPacketsCmd {
 
         match self.execute() {
             Ok(summary) if json() => Output::success(summary).exit(),
-            Ok(summary) => Output::success(summary.collate()).exit(),
+            Ok(summary) => Output::success_msg(summary.collate().to_string()).exit(),
             Err(e) => Output::error(e).exit(),
         }
     }
