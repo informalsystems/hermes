7f630c02e73faa8038470668ab86cce4ca2d3748
Luca Joss <luca@informal.systems>
2024-01-26 13:17:22 +0100

    Add case where Channel upgrade Try event is received by source chain while dst chain is open not upgrading

--- crates/relayer/src/channel.rs
+++ crates/relayer/src/channel.rs
@@ -928,6 +928,47 @@ impl<ChainA: ChainHandle, ChainB: ChainHandle> Channel<ChainA, ChainB> {
                     }
                 }
             }
+            (State::Flushing, State::Open(UpgradeState::NotUpgrading)) => {
+                let src_latest_height = self
+                    .src_chain()
+                    .query_latest_height()
+                    .map_err(|e| ChannelError::chain_query(self.src_chain().id(), e))?;
+                let dst_latest_height = self
+                    .dst_chain()
+                    .query_latest_height()
+                    .map_err(|e| ChannelError::chain_query(self.dst_chain().id(), e))?;
+                let (error_receipt, _) = self
+                    .dst_chain()
+                    .query_upgrade_error(
+                        QueryUpgradeErrorRequest {
+                            port_id: self.dst_port_id().to_string(),
+                            channel_id: self.dst_channel_id().unwrap().to_string(),
+                        },
+                        dst_latest_height,
+                    )
+                    .map_err(|e| ChannelError::chain_query(self.dst_chain().id(), e))?;
+
+                let (channel_end, _) = self
+                    .src_chain()
+                    .query_channel(
+                        QueryChannelRequest {
+                            port_id: self.src_port_id().clone(),
+                            channel_id: self.src_channel_id().unwrap().clone(),
+                            height: QueryHeight::Specific(src_latest_height),
+                        },
+                        IncludeProof::Yes,
+                    )
+                    .map_err(|e| ChannelError::query(self.dst_chain().id(), e))?;
+
+                if error_receipt.sequence == channel_end.upgrade_sequence {
+                    Some(self.flipped().build_chan_upgrade_cancel_and_send()?)
+                } else {
+                    match self.build_chan_upgrade_ack_and_send()? {
+                        Some(event) => Some(event),
+                        None => Some(self.flipped().build_chan_upgrade_cancel_and_send()?),
+                    }
+                }
+            }
             (State::Open(UpgradeState::Upgrading), State::Flushcomplete) => {
                 Some(self.build_chan_upgrade_open_and_send()?)
             }
