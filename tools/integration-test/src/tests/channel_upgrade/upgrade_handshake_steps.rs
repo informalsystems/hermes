85006dd6fe39d6a7ed3df61c7961c662777acc38
Carlos Rodriguez <carlos@interchain.io>
2024-01-30 14:45:49 +0100

    add test where upgrade times out during packet ack

--- tools/integration-test/src/tests/channel_upgrade/upgrade_handshake_steps.rs
+++ tools/integration-test/src/tests/channel_upgrade/upgrade_handshake_steps.rs
@@ -24,6 +24,9 @@
 //! - `ChannelUpgradeHandshakeInitiateNewUpgrade` tests that the channel worker will
 //!   finish the upgrade handshake if the side that moved to `OPEN` initiates a
 //!   new upgrade before the counterparty moved to `OPEN`.
+//! 
+//! - `ChannelUpgradeHandshakeTimeoutOnPacketAck` tests that the channel worker will cancel the
+//!   upgrade handshake if the chain acknowledges a packet after the upgrade timeout expired.
 
 use ibc_relayer::chain::requests::{IncludeProof, QueryChannelRequest, QueryHeight};
 use ibc_relayer_types::core::ics04_channel::channel::{State as ChannelState, UpgradeState};
@@ -79,6 +82,11 @@ fn test_channel_upgrade_handshake_flush_packets() -> Result<(), Error> {
 #[test]
 fn test_channel_upgrade_handshake_initiate_new_upgrade() -> Result<(), Error> {
     run_binary_channel_test(&ChannelUpgradeHandshakeInitiateNewUpgrade)
+}
+
+#[test]
+fn test_channel_upgrade_handshake_timeout_on_packet_ack() -> Result<(), Error> {
+    run_binary_channel_test(&ChannelUpgradeHandshakeTimeoutOnPacketAck)
 }
 
 const MAX_DEPOSIT_PERIOD: &str = "10s";
@@ -1432,6 +1440,195 @@ impl BinaryChannelTest for ChannelUpgradeHandshakeInitiateNewUpgrade {
     }
 }
 
+struct ChannelUpgradeHandshakeTimeoutOnPacketAck;
+
+impl BinaryChannelTest for ChannelUpgradeHandshakeTimeoutOnPacketAck {
+    fn run<ChainA: ChainHandle, ChainB: ChainHandle>(
+        &self,
+        _config: &TestConfig,
+        relayer: RelayerDriver,
+        chains: ConnectedChains<ChainA, ChainB>,
+        channels: ConnectedChannel<ChainA, ChainB>,
+    ) -> Result<(), Error> {
+        info!("Check that channels are both in OPEN State");
+
+        assert_eventually_channel_established(
+            &chains.handle_b,
+            &chains.handle_a,
+            &channels.channel_id_b.as_ref(),
+            &channels.port_b.as_ref(),
+        )?;
+
+        let channel_end_a = chains
+            .handle_a
+            .query_channel(
+                QueryChannelRequest {
+                    port_id: channels.port_a.0.clone(),
+                    channel_id: channels.channel_id_a.0.clone(),
+                    height: QueryHeight::Latest,
+                },
+                IncludeProof::No,
+            )
+            .map(|(channel_end, _)| channel_end)
+            .map_err(|e| eyre!("Error querying ChannelEnd A: {e}"))?;
+
+        let channel_end_b = chains
+            .handle_b
+            .query_channel(
+                QueryChannelRequest {
+                    port_id: channels.port_b.0.clone(),
+                    channel_id: channels.channel_id_b.0.clone(),
+                    height: QueryHeight::Latest,
+                },
+                IncludeProof::No,
+            )
+            .map(|(channel_end, _)| channel_end)
+            .map_err(|e| eyre!("Error querying ChannelEnd B: {e}"))?;
+
+        let old_version = channel_end_a.version;
+        let old_ordering = channel_end_a.ordering;
+        let old_connection_hops_a = channel_end_a.connection_hops;
+        let old_connection_hops_b = channel_end_b.connection_hops;
+
+        let channel = channels.channel;
+        let new_version = Version::ics20_with_fee();
+
+        let old_attrs = ChannelUpgradableAttributes::new(
+            old_version.clone(),
+            old_version.clone(),
+            old_ordering,
+            old_connection_hops_a.clone(),
+            old_connection_hops_b.clone(),
+            Sequence::from(1),
+        );
+
+        info!("Will update channel params to set a short upgrade timeout...");
+
+        // the upgrade timeout should be long enough for chain a
+        // to complete Ack successfully so that it goes into `FLUSHING`
+        chains.node_b.chain_driver().update_channel_params(
+            80000000000,
+            chains.handle_b().get_signer().unwrap().as_ref(),
+            "1",
+        )?;
+
+        info!("Will initialise upgrade handshake with governance proposal...");
+
+        chains.node_a.chain_driver().initialise_channel_upgrade(
+            channel.src_port_id().as_str(),
+            channel.src_channel_id().unwrap().as_str(),
+            old_ordering.as_str(),
+            old_connection_hops_a.first().unwrap().as_str(),
+            &serde_json::to_string(&new_version.0).unwrap(),
+            chains.handle_a().get_signer().unwrap().as_ref(),
+            "1",
+        )?;
+
+        info!("Check that the step ChanUpgradeInit was correctly executed...");
+
+        assert_eventually_channel_upgrade_init(
+            &chains.handle_a,
+            &chains.handle_b,
+            &channels.channel_id_a.as_ref(),
+            &channels.port_a.as_ref(),
+            &old_attrs,
+        )?;
+
+        // send a IBC transfer message from chain a to chain b
+        // so that we have an in-flight packet and chain a
+        // will move to `FLUSHING` during Ack
+        let denom_a = chains.node_a.denom();
+        let wallet_a = chains.node_a.wallets().user1().cloned();
+        let wallet_b = chains.node_b.wallets().user1().cloned();
+        let a_to_b_amount = random_u128_range(1000, 5000);
+
+        info!(
+            "Sending IBC transfer from chain {} to chain {} with amount of {} {}",
+            chains.chain_id_a(),
+            chains.chain_id_b(),
+            a_to_b_amount,
+            denom_a
+        );
+
+        chains.node_a.chain_driver().ibc_transfer_token_with_memo_and_timeout(
+            &channels.port_a.as_ref(),
+            &channels.channel_id_a.as_ref(),
+            &wallet_a.as_ref(),
+            &wallet_b.address(),
+            &denom_a.with_amount(a_to_b_amount).as_ref(),
+            None,
+            Some(Duration::from_secs(600)),
+        )?;
+
+        info!("Will run ChanUpgradeTry step...");
+
+        channel.build_chan_upgrade_try_and_send()?;
+
+        info!("Check that the step ChanUpgradeTry was correctly executed...");
+
+        assert_eventually_channel_upgrade_try(
+            &chains.handle_b,
+            &chains.handle_a,
+            &channels.channel_id_b.as_ref(),
+            &channels.port_b.as_ref(),
+            &old_attrs.flipped(),
+        )?;
+
+        info!("Will run ChanUpgradeAck step...");
+
+        channel.flipped().build_chan_upgrade_ack_and_send()?;
+
+        info!("Check that the step ChanUpgradeAck was correctly executed...");
+
+        // channel a is `FLUSHING` because the packet
+        // from a to b has not been cleared yet
+        assert_eventually_channel_upgrade_ack(
+            &chains.handle_a,
+            &chains.handle_b,
+            &channels.channel_id_a.as_ref(),
+            &channels.port_a.as_ref(),
+            ChannelState::Flushing,
+            ChannelState::Flushing,
+            &old_attrs,
+        )?;
+
+        // wait enough time so that timeout expires while chain a is in FLUSHING
+        // and when packet lifecycle completes with acknowledge packet on chain a
+        // it will abort the upgrade
+        sleep(Duration::from_nanos(60000000000));
+
+        info!("Check that the channel upgrade aborted...");
+
+        // start supervisor to clear in-flight packets
+        // and move channel ends to `FLUSH_COMPLETE`
+        relayer.with_supervisor(|| {
+            let ibc_denom_b = derive_ibc_denom(
+                &channels.port_b.as_ref(),
+                &channels.channel_id_b.as_ref(),
+                &denom_a,
+            )?;
+
+            chains.node_b.chain_driver().assert_eventual_wallet_amount(
+                &wallet_b.address(),
+                &ibc_denom_b.with_amount(a_to_b_amount).as_ref(),
+            )?;
+
+            // This will assert that both channel ends are eventually
+            // in Open state, and that the fields targeted by the upgrade
+            // have NOT been correctly updated, because chain a aborted the upgrade
+            assert_eventually_channel_upgrade_cancel(
+                &chains.handle_a,
+                &chains.handle_b,
+                &channels.channel_id_a.as_ref(),
+                &channels.port_a.as_ref(),
+                &old_attrs,
+            )?;
+
+            Ok(())
+        })
+    }
+}
+
 impl HasOverrides for ChannelUpgradeManualHandshake {
     type Overrides = ChannelUpgradeTestOverrides;
 
@@ -1489,6 +1686,14 @@ impl HasOverrides for ChannelUpgradeHandshakeFlushPackets {
 }
 
 impl HasOverrides for ChannelUpgradeHandshakeInitiateNewUpgrade {
+    type Overrides = ChannelUpgradeTestOverrides;
+
+    fn get_overrides(&self) -> &ChannelUpgradeTestOverrides {
+        &ChannelUpgradeTestOverrides
+    }
+}
+
+impl HasOverrides for ChannelUpgradeHandshakeTimeoutOnPacketAck {
     type Overrides = ChannelUpgradeTestOverrides;
 
     fn get_overrides(&self) -> &ChannelUpgradeTestOverrides {
