f21513d8ff42eb21cf75e7341cdc515b144fc52c
Romain Ruetschi <106849+romac@users.noreply.github.com>
2024-02-08 11:55:30 +0100

    Update ibc-proto to v0.42.0

--- crates/relayer-types/src/applications/ics27_ica/msgs/register.rs
+++ crates/relayer-types/src/applications/ics27_ica/msgs/register.rs
@@ -4,6 +4,7 @@ use ibc_proto::ibc::applications::interchain_accounts::controller::v1::MsgRegist
 use ibc_proto::Protobuf;
 
 use crate::applications::ics27_ica::error::Error;
+use crate::core::ics04_channel::channel::Ordering;
 use crate::core::ics04_channel::version::Version;
 use crate::core::ics24_host::error::ValidationError;
 use crate::core::ics24_host::identifier::ConnectionId;
@@ -18,6 +19,7 @@ pub struct MsgRegisterInterchainAccount {
     pub owner: Signer,
     pub connection_id: ConnectionId,
     pub version: Version,
+    pub ordering: Ordering,
 }
 
 impl Msg for MsgRegisterInterchainAccount {
@@ -46,6 +48,8 @@ impl TryFrom<RawMsgRegisterInterchainAccount> for MsgRegisterInterchainAccount {
                 .parse()
                 .map_err(Error::invalid_connection_identifier)?,
             version: value.version.into(),
+            ordering: Ordering::from_i32(value.ordering)
+                .map_err(|_| Error::invalid_ordering(value.ordering))?,
         })
     }
 }
@@ -56,6 +60,7 @@ impl From<MsgRegisterInterchainAccount> for RawMsgRegisterInterchainAccount {
             owner: value.owner.to_string(),
             connection_id: value.connection_id.to_string(),
             version: value.version.to_string(),
+            ordering: value.ordering as i32,
         }
     }
 }
