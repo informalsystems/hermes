5154f01f08aba7328335928c62b2aa74be644e65
Romain Ruetschi <romain@informal.systems>
2024-02-28 12:30:55 +0100

    Improve reliability of compatibility check (#3835)

--- crates/relayer/src/chain/cosmos/compatibility.rs
+++ crates/relayer/src/chain/cosmos/compatibility.rs
@@ -24,12 +24,18 @@ const IBC_GO_MODULE_VERSION_REQ: &str = ">=4.1.1, <9";
 
 #[derive(Error, Debug)]
 pub enum Diagnostic {
+    #[error("SDK module version not found, required {requirements}")]
+    MissingSdkModuleVersion { requirements: String },
+
+    #[error("IBC-Go module version not found, required {requirements}")]
+    MissingIbcGoModuleVersion { requirements: String },
+
     #[error(
         "SDK module at version '{found}' does not meet compatibility requirements {requirements}"
     )]
     MismatchingSdkModuleVersion { requirements: String, found: String },
 
-    #[error("Ibc-Go module at version '{found}' does not meet compatibility requirements {requirements}")]
+    #[error("IBC-Go module at version '{found}' does not meet compatibility requirements {requirements}")]
     MismatchingIbcGoModuleVersion { requirements: String, found: String },
 }
 
@@ -44,40 +50,44 @@ pub enum Diagnostic {
 /// Sdk module by name, as well as the constants
 /// [`SDK_MODULE_VERSION_REQ`] and [`IBC_GO_MODULE_VERSION_REQ`]
 /// for establishing compatibility requirements.
-pub(crate) fn run_diagnostic(v: &version::Specs) -> Result<(), Diagnostic> {
-    debug!("running diagnostic on version info {}", v);
+pub(crate) fn run_diagnostic(specs: &version::Specs) -> Result<(), Diagnostic> {
+    debug!("running diagnostic on version specs: {specs}");
 
-    sdk_diagnostic(&v.cosmos_sdk)?;
-    ibc_go_diagnostic(v.ibc_go.as_ref())?;
+    sdk_diagnostic(specs.cosmos_sdk.as_ref())?;
+    ibc_go_diagnostic(specs.ibc_go.as_ref())?;
 
     Ok(())
 }
 
-fn sdk_diagnostic(version: &semver::Version) -> Result<(), Diagnostic> {
+fn sdk_diagnostic(version: Option<&semver::Version>) -> Result<(), Diagnostic> {
     // Parse the SDK requirements into a semver
     let sdk_reqs = semver::VersionReq::parse(SDK_MODULE_VERSION_REQ)
         .expect("parsing the SDK module requirements into semver");
 
-    // Finally, check the version requirements
-    match sdk_reqs.matches(version) {
-        true => Ok(()),
-        false => Err(Diagnostic::MismatchingSdkModuleVersion {
+    match version {
+        None => Err(Diagnostic::MissingSdkModuleVersion {
             requirements: SDK_MODULE_VERSION_REQ.to_string(),
-            found: version.to_string(),
         }),
+
+        Some(version) => match sdk_reqs.matches(version) {
+            true => Ok(()),
+            false => Err(Diagnostic::MismatchingSdkModuleVersion {
+                requirements: SDK_MODULE_VERSION_REQ.to_string(),
+                found: version.to_string(),
+            }),
+        },
     }
 }
 
-fn ibc_go_diagnostic(version_info: Option<&semver::Version>) -> Result<(), Diagnostic> {
+fn ibc_go_diagnostic(version: Option<&semver::Version>) -> Result<(), Diagnostic> {
     // Parse the IBC-go module requirements into a semver
     let ibc_reqs = semver::VersionReq::parse(IBC_GO_MODULE_VERSION_REQ)
         .expect("parsing the IBC-Go module requirements into semver");
 
-    // Find the Ibc-Go module
-    match version_info {
-        // If binary lacks the ibc-go dependency it is _not_ an error,
-        // we support chains without the standalone ibc-go module.
-        None => Ok(()),
+    match version {
+        None => Err(Diagnostic::MissingIbcGoModuleVersion {
+            requirements: IBC_GO_MODULE_VERSION_REQ.to_string(),
+        }),
         Some(version) => match ibc_reqs.matches(version) {
             true => Ok(()),
             false => Err(Diagnostic::MismatchingIbcGoModuleVersion {
