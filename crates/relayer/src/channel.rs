150ae6cf89734bc99de363083585e9bb19001b1f
Romain Ruetschi <romain@informal.systems>
2024-03-18 14:04:23 +0100

    Use constant backoff in handshake retry strategy

--- crates/relayer/src/channel.rs
+++ crates/relayer/src/channel.rs
@@ -55,13 +55,13 @@ pub mod channel_handshake_retry {
 
     /// Defines the increment in delay between subsequent retries.
     /// A value of `0` will make the retry delay constant.
-    const DELAY_INCREMENT: Duration = Duration::from_secs(1);
+    const DELAY_INCREMENT: Duration = Duration::from_secs(0);
 
     /// Maximum number of retries
     const MAX_RETRIES: u32 = 10;
 
     /// The default retry strategy.
-    /// We retry with a growing backoff strategy. The strategy is parametrized by the
+    /// We retry with a constant backoff strategy. The strategy is parametrized by the
     /// maximum block time expressed as a `Duration`.
     pub fn default_strategy(max_block_time: Duration) -> impl Iterator<Item = Duration> {
         let retry_delay = max_block_time / PER_BLOCK_RETRIES;
