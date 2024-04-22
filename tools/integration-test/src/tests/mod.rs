bdbbef3c8d59942a8e5c7468911131fa35661609
Luca Joss <43531661+ljoss17@users.noreply.github.com>
2024-04-22 11:54:45 +0200

    Update Juno version used in CI (#3962)
    * Update Juno version used in CI and run ica tests with Juno
    
    * Add changelog entry
    
    ---------
    
    Signed-off-by: Romain Ruetschi <romain@informal.systems>
    Co-authored-by: Romain Ruetschi <romain@informal.systems>

--- tools/integration-test/src/tests/mod.rs
+++ tools/integration-test/src/tests/mod.rs
@@ -10,7 +10,7 @@ pub mod client_expiration;
 pub mod client_filter;
 pub mod client_refresh;
 pub mod client_settings;
-#[cfg(not(any(feature = "celestia", feature = "juno")))]
+#[cfg(not(feature = "celestia"))]
 pub mod client_upgrade;
 pub mod connection_delay;
 pub mod consensus_states;
