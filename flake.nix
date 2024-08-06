184466f27b0547f3769283bd0e465d46db4cbaf8
Luca Joss <luca@informal.systems>
2024-08-06 14:54:16 +0200

    Update Nix flake to use branch with Neutron v4.1.0

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = github:nixos/nixpkgs/nixpkgs-unstable;
     flake-utils.url = github:numtide/flake-utils;
-    cosmos-nix.url = github:informalsystems/cosmos.nix;
+    cosmos-nix.url = github:informalsystems/cosmos.nix/luca_joss/update-neutron-to-v4;
   };
 
   outputs = inputs: let
