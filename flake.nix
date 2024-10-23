de45cbf23782c63195df449dd437d15c99772053
Luca Joss <luca@informal.systems>
2024-10-23 13:14:01 +0200

    Use Cosmos Nix main branch

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    cosmos-nix.url = "github:informalsystems/cosmos.nix/luca_joss/add-gaia-v20-alpha";
+    cosmos-nix.url = "github:informalsystems/cosmos.nix";
   };
 
   outputs = inputs: let
