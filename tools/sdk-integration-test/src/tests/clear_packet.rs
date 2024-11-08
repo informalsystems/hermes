3e6bc7c4f9ae261a2c64beec8b76c76d50bfb303
Luca Joss <luca@informal.systems>
2024-11-08 11:03:28 +0100

    Improve SDK test bootstrap and add packet clearing test

--- tools/sdk-integration-test/src/tests/clear_packet.rs
+++ tools/sdk-integration-test/src/tests/clear_packet.rs
@@ -1,5 +1,4 @@
 use hermes_cosmos_integration_tests::contexts::binary_channel::test_driver::CosmosBinaryChannelTestDriver;
-use hermes_cosmos_test_components::chain::types::amount::Amount;
 use hermes_error::types::Error;
 use hermes_relayer_components::multi::types::index::Index;
 use hermes_relayer_components::multi::types::index::Twindex;
@@ -17,19 +16,28 @@ use hermes_test_components::driver::traits::channel_at::HasChannelAt;
 use ibc_test_framework::prelude::RelayerDriver;
 use ibc_test_framework::prelude::*;
 
+use crate::tests::bootstrap::bootstrap_and_run_test;
 use crate::tests::bootstrap::BoxFuture;
-
-use super::bootstrap::bootstrap_and_run_test;
 
 #[test]
 fn run_test() -> Result<(), Error> {
     bootstrap_and_run_test::<_, BoxFuture<'_, Result<(), Error>>>(
-        |setup, relay_driver| Box::pin(test_ibc_transfer(setup, relay_driver)),
-        |_| {},
+        |setup, relay_driver| Box::pin(test_clear_packet_no_scan(setup, relay_driver)),
+        |config| {
+            // Disabling the client workers and clear_on_start should make the relayer not
+            // relay any packet it missed before starting.
+            config.mode.clients.enabled = false;
+            config.mode.connections.enabled = false;
+            config.mode.channels.enabled = false;
+
+            config.mode.packets.enabled = true;
+            config.mode.packets.clear_on_start = false;
+            config.mode.packets.clear_interval = 10;
+        },
     )
 }
 
-async fn test_ibc_transfer(
+async fn test_clear_packet_no_scan(
     setup: &CosmosBinaryChannelTestDriver,
     relay_driver: RelayerDriver,
 ) -> Result<(), Error> {
@@ -45,29 +53,15 @@ async fn test_ibc_transfer(
     let channel_id_b = setup.channel_id_at(Twindex::<1, 0>);
     let port_id_b = setup.port_id_at(Twindex::<1, 0>);
 
-    // Missing native token for chains
     let denom_a = chain_driver_a.denom_at(TransferDenom, Index::<0>);
+    // fee denom
 
     let wallet_a = &chain_driver_a.user_wallet_a;
     let wallet_b = &chain_driver_b.user_wallet_a;
-    let wallet_c = &chain_driver_a.user_wallet_b;
 
     let balance_a = chain_a.query_balance(&wallet_a.address, denom_a).await?;
 
     let a_to_b_amount = chain_driver_a.random_amount(1000, &balance_a).await;
-
-    let expected_balance_a = hermes_cosmos_relayer::contexts::chain::CosmosChain::subtract_amount(
-        &balance_a,
-        &a_to_b_amount,
-    )?;
-    let _handle = tokio::task::spawn_blocking(move || relay_driver.spawn_supervisor().unwrap());
-
-    tokio::time::sleep(core::time::Duration::from_secs(10)).await;
-
-    info!(
-        "Sending IBC transfer from chain {} to chain {} with amount of {} {}",
-        chain_a.chain_id, chain_b.chain_id, a_to_b_amount, denom_a
-    );
 
     <hermes_cosmos_relayer::contexts::chain::CosmosChain as CanIbcTransferToken<
         hermes_cosmos_relayer::contexts::chain::CosmosChain,
@@ -81,80 +75,83 @@ async fn test_ibc_transfer(
     )
     .await?;
 
-    info!(
-        "Waiting for user on chain B to receive IBC transferred amount of {}",
-        a_to_b_amount
-    );
-    tokio::time::sleep(core::time::Duration::from_secs(20)).await;
+    tokio::time::sleep(Duration::from_secs(25)).await;
+
+    let _handle = tokio::task::spawn_blocking(move || relay_driver.spawn_supervisor().unwrap());
+
+    info!("Assert clear on start does not trigger");
+
+    let expected_balance_a = hermes_cosmos_relayer::contexts::chain::CosmosChain::subtract_amount(
+        &balance_a,
+        &a_to_b_amount,
+    )?;
+
+    let zero_coin = hermes_cosmos_relayer::contexts::chain::CosmosChain::subtract_amount(
+        &a_to_b_amount,
+        &a_to_b_amount,
+    )?;
+
+    let expected_balance_b =
+        <hermes_cosmos_relayer::contexts::chain::CosmosChain as CanConvertIbcTransferredAmount<
+            hermes_cosmos_relayer::contexts::chain::CosmosChain,
+        >>::ibc_transfer_amount_from(&zero_coin, channel_id_b, port_id_b)?;
 
     chain_a
         .assert_eventual_amount(&wallet_a.address, &expected_balance_a)
         .await?;
 
-    let expected_balance_b =
-        <hermes_cosmos_relayer::contexts::chain::CosmosChain as CanConvertIbcTransferredAmount<
-            hermes_cosmos_relayer::contexts::chain::CosmosChain,
-        >>::ibc_transfer_amount_from(&a_to_b_amount, channel_id_b, port_id_b)?;
+    chain_b
+        .assert_eventual_amount(&wallet_b.address, &expected_balance_b)
+        .await?;
+
+    // Wait for clear interval to trigger
+    tokio::time::sleep(Duration::from_secs(20)).await;
+
+    info!("Assert clear interval does not trigger");
 
     chain_b
         .assert_eventual_amount(&wallet_b.address, &expected_balance_b)
         .await?;
 
-    info!(
-        "successfully performed IBC transfer from chain {} to chain {}",
-        chain_a.chain_id, chain_b.chain_id,
-    );
-
-    let balance_c = chain_a.query_balance(&wallet_c.address, denom_a).await?;
-
-    let b_to_a_amount = chain_driver_a
-        .random_amount(1000, &expected_balance_b)
-        .await;
-
-    let expected_balance_b2 = hermes_cosmos_relayer::contexts::chain::CosmosChain::subtract_amount(
-        &expected_balance_b,
-        &b_to_a_amount,
-    )?;
-
-    let b_to_a_amount_as_denom_a = Amount {
-        quantity: b_to_a_amount.quantity,
-        denom: denom_a.clone(),
-    };
-
-    let expected_balance_c = hermes_cosmos_relayer::contexts::chain::CosmosChain::add_amount(
-        &balance_c,
-        &b_to_a_amount_as_denom_a,
-    )?;
-
-    info!(
-        "Sending IBC transfer from chain {} to chain {} with amount of {}",
-        chain_b.chain_id, chain_a.chain_id, b_to_a_amount,
-    );
-
     <hermes_cosmos_relayer::contexts::chain::CosmosChain as CanIbcTransferToken<
         hermes_cosmos_relayer::contexts::chain::CosmosChain,
     >>::ibc_transfer_token(
-        chain_b,
-        channel_id_b,
-        port_id_b,
-        wallet_b,
-        &wallet_c.address,
-        &b_to_a_amount,
+        chain_a,
+        channel_id_a,
+        port_id_a,
+        wallet_a,
+        &wallet_b.address,
+        &a_to_b_amount,
     )
     .await?;
+
+    info!("Assert clear interval correctly triggers");
+
+    let expected_balance_a2 = hermes_cosmos_relayer::contexts::chain::CosmosChain::subtract_amount(
+        &expected_balance_a,
+        &a_to_b_amount,
+    )?;
+
+    let expected_balance_b2_as_coin =
+        hermes_cosmos_relayer::contexts::chain::CosmosChain::subtract_amount(
+            &a_to_b_amount,
+            &a_to_b_amount,
+        )?;
+
+    let expected_balance_b2 =
+        <hermes_cosmos_relayer::contexts::chain::CosmosChain as CanConvertIbcTransferredAmount<
+            hermes_cosmos_relayer::contexts::chain::CosmosChain,
+        >>::ibc_transfer_amount_from(&expected_balance_b2_as_coin, channel_id_b, port_id_b)?;
+
+    chain_a
+        .assert_eventual_amount(&wallet_a.address, &expected_balance_a2)
+        .await?;
+
+    info!("Assert clear interval does not trigger");
 
     chain_b
         .assert_eventual_amount(&wallet_b.address, &expected_balance_b2)
         .await?;
-
-    chain_a
-        .assert_eventual_amount(&wallet_c.address, &expected_balance_c)
-        .await?;
-
-    info!(
-        "successfully performed reverse IBC transfer from chain {} back to chain {}",
-        chain_b.chain_id, chain_a.chain_id,
-    );
 
     Ok(())
 }
--- tools/sdk-integration-test/src/tests/mod.rs
+++ tools/sdk-integration-test/src/tests/mod.rs
@@ -1,1 +1,3 @@
+pub mod bootstrap;
+pub mod clear_packet;
 pub mod transfer;
