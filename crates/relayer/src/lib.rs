5e456c8a1404dcd8a5854f91ddbc6f168a33a7d3
Luca Joss <luca@informal.systems>
2025-02-11 10:32:28 +0100

    Bump versions

--- crates/relayer/src/lib.rs
+++ crates/relayer/src/lib.rs
@@ -14,7 +14,7 @@
 //!
 //! For the IBC relayer binary, please see [Hermes] (`ibc-relayer-cli` crate).
 //!
-//! [Hermes]: https://docs.rs/ibc-relayer-cli/1.10.5/
+//! [Hermes]: https://docs.rs/ibc-relayer-cli/1.11.0/
 
 extern crate alloc;
 
@@ -48,4 +48,4 @@ pub mod upgrade_chain;
 pub mod util;
 pub mod worker;
 
-pub const HERMES_VERSION: &str = "1.10.5";
+pub const HERMES_VERSION: &str = "1.11.0";
